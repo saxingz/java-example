69dbc333e9ad4378f6adb5930a2d02feb06d6c50
saxing <saxlh@foxmail.com>
2018-08-14 23:54:18 +0800

    saxing: active object.

---
+++ src/main/java/org/saxing/study/a20180814_handler/activeobject_concurrent/ActiveObject.java
@@ -0,0 +1,11 @@
+package org.saxing.study.a20180814_handler.activeobject_concurrent;
+
+import java.util.concurrent.Future;
+
+public interface ActiveObject {
+
+    public abstract Future<String> makeString(int count, char fillchar);
+    public abstract void displayString(String string);
+    public abstract void shutdown();
+
+}
---
+++ src/main/java/org/saxing/study/a20180814_handler/activeobject_concurrent/DisplayClientThread.java
@@ -0,0 +1,31 @@
+package org.saxing.study.a20180814_handler.activeobject_concurrent;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.RejectedExecutionException;
+
+public class DisplayClientThread extends Thread {
+
+    private final ActiveObject activeObject;
+
+    public DisplayClientThread(String name, ActiveObject activeObject) {
+        super(name);
+        this.activeObject = activeObject;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; true; i++) {
+                String string = Thread.currentThread().getName() + "  " + i;
+                activeObject.displayString(string);
+                Thread.sleep(200);
+            }
+        } catch (RejectedExecutionException e){
+            System.out.println(Thread.currentThread().getName() + ": " + e);
+        } catch (CancellationException e){
+            System.out.println(Thread.currentThread().getName() + ": " + e);
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + ": " + e);
+        }
+    }
+}
