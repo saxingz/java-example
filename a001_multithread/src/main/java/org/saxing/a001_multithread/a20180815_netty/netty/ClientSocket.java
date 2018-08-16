75fe3c0a49edc698fcf5702149eeae57097dd2f8
liuhan <liuhan@lsrobot.vip>
2018-08-16 09:42:23 +0800

    netty test

--- a001_multithread/src/main/java/org/saxing/a001_multithread/a20180815_netty/netty/ClientSocket.java
+++ a001_multithread/src/main/java/org/saxing/a001_multithread/a20180815_netty/netty/ClientSocket.java
@@ -13,7 +13,7 @@ import java.nio.ByteBuffer;
 public class ClientSocket {
 //    private static final Logger logger = Logger.getLogger(ClientSocket.class);
     private static final String HOST = "127.0.0.1";
-    private static final int PORT = 9999;
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
         String str = "abcdefg";
