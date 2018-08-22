23db48cc341e16ae2b27107be189aead389e4e7f
saxing <liuhan@lsrobot.vip>
2018-08-22 16:21:36 +0800

    音频转码 8k 8bit aLaw(g711a) -> 8k pcm 立体声

--- a0005_avdio/src/main/java/org/saxing/audio_video/G711/TestA2P.java
+++ a0005_avdio/src/main/java/org/saxing/audio_video/G711/TestA2P.java
@@ -2,6 +2,9 @@ package org.saxing.audio_video.G711;
 
 import java.io.*;
 
+/**
+ * 这里测试的是： 音频转码 8k 8bit aLaw(g711a) -> 8k pcm 立体声
+ */
 public class TestA2P {
 
     public static void main(String[] args) throws IOException {
