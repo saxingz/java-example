9bb68700f3da0b4dc3556f3b4261140524097cde
saxing <saxlh@foxmail.com>
2018-08-28 10:50:17 +0800

    bootapplication.

---
+++ a0014_dianzan/src/main/java/org/saxing/BocApplication.java
@@ -0,0 +1,50 @@
+package org.saxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@SpringBootApplication
+public class BocApplication extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(BocApplication.class, args);
+	}
+
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("org.saxing.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("RESTful APIs")
+				.description("apis")
+				.termsOfServiceUrl("")
+				.version("1.0")
+				.build();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("classpath:/static/");
+	}
+}
