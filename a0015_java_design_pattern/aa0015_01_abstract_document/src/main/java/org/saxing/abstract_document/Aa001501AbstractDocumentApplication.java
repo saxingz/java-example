069c9dc8a4e9b7f9590707ef764999ce03afb8d2
saxing <saxlh@foxmail.com>
2018-08-30 18:58:53 +0800

    abstract document.

---
+++ a0015_java_design_pattern/aa0015_01_abstract_document/src/main/java/org/saxing/abstract_document/Aa001501AbstractDocumentApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.abstract_document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001501AbstractDocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001501AbstractDocumentApplication.class, args);
+    }
+}
