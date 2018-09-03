93889c7531ed656679cbd3ade3cfadc2852dbc22
saxing <saxlh@foxmail.com>
2018-09-03 13:40:30 +0800

    apollo test

---
+++ a0016_apollo/src/main/java/org/saxing/apollo/A0016ApolloApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0016ApolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A0016ApolloApplication.class, args);
+	}
+}
