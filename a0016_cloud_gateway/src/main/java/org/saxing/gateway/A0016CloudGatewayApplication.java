73477d45c43afcc13a97d8297330d4c8b8536352
saxing <saxlh@foxmail.com>
2018-09-05 20:01:03 +0800

    cloud gateway demo

---
+++ a0016_cloud_gateway/src/main/java/org/saxing/gateway/A0016CloudGatewayApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0016CloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0016CloudGatewayApplication.class, args);
+    }
+}
---
+++ a0016_cloud_gateway/src/test/java/org/saxing/gateway/A0016CloudGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0016CloudGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
