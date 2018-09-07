6e4680f198df38fc9d021d6e16cc453dbf21de6c
saxing <saxlh@foxmail.com>
2018-09-07 09:57:55 +0800

    mybatis code helper

---
+++ a0017_mybatis_code_helper/src/main/java/org/saxing/mybatis_code_helper/A0017MybatisCodeHelperApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.mybatis_code_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0017MybatisCodeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0017MybatisCodeHelperApplication.class, args);
+    }
+}
---
+++ a0017_mybatis_code_helper/src/test/java/org/saxing/mybatis_code_helper/A0017MybatisCodeHelperApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.mybatis_code_helper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0017MybatisCodeHelperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
