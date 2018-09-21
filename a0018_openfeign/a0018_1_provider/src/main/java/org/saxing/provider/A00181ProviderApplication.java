f790de31fa6883f4129e229a33f23a1d4c9de468
saxing <saxlh@foxmail.com>
2018-09-21 15:23:30 +0800

    gitignore

---
+++ a0018_openfeign/a0018_1_provider/src/main/java/org/saxing/provider/A00181ProviderApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A00181ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A00181ProviderApplication.class, args);
+    }
+}
