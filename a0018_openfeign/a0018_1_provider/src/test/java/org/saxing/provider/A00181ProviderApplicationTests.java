587d08c8a3b51958e1732dd008191d2a1df7e745
saxing <saxlh@foxmail.com>
2018-09-21 15:23:39 +0800

    gitignore

---
+++ a0018_openfeign/a0018_1_provider/src/test/java/org/saxing/provider/A00181ProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A00181ProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
