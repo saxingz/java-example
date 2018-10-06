66851b1041d9a660c0978525d3e1f8f8d8bbec72
saxing <saxlh@foxmail.com>
2018-10-06 09:45:28 +0800

    test.

---
+++ a0019_syspro/src/main/java/org/saxing/syspro/Test.java
@@ -0,0 +1,16 @@
+package org.saxing.syspro;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        for (int i = 0; i < 1; i++) {
+//            System.out.println(i);
+//
+//        }
+
+        for (int i = 0; i < 20; i++){
+            System.out.println(i);
+        }
+    }
+
+}
