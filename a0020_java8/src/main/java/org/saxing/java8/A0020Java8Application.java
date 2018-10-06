293f05229a28492efa3b28332d511ce7e24b0115
saxing <saxlh@foxmail.com>
2018-10-06 11:27:58 +0800

    JAVA 8 test.

---
+++ a0020_java8/src/main/java/org/saxing/java8/A0020Java8Application.java
@@ -0,0 +1,12 @@
+package org.saxing.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0020Java8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0020Java8Application.class, args);
+    }
+}
