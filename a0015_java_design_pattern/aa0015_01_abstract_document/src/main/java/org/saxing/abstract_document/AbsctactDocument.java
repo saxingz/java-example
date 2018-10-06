9580860f7d0717bb2b6684f60c0909a145438e7d
saxing <saxlh@foxmail.com>
2018-10-06 11:04:30 +0800

    abstractDocument

---
+++ a0015_java_design_pattern/aa0015_01_abstract_document/src/main/java/org/saxing/abstract_document/AbsctactDocument.java
@@ -0,0 +1,51 @@
+package org.saxing.abstract_document;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ * Abstraction implemention of Document
+ *
+ * @author saxing  2018/10/6 10:49
+ */
+public abstract class AbsctactDocument implements Document {
+
+    private final Map<String, Object> properties;
+
+    protected AbsctactDocument(Map<String, Object> properties) {
+        Objects.requireNonNull(properties, "properties map is required");
+        this.properties = properties;
+    }
+
+    @Override
+    public Void put(String key, Object value) {
+        properties.put(key, value);
+        return null;
+    }
+
+    @Override
+    public Object get(String key) {
+        return properties.get(key);
+    }
+
+    @Override
+    public <T> Stream<T> ChildRen(String key, Function<Map<String, Object>, T> constructor) {
+        Optional<List<Map<String, Object>>> any = Stream.of(get(key)).filter(el -> el != null)
+                .map(el -> (List<Map<String, Object>>)el).findAny();
+        return any.isPresent() ? any.get().stream().map(constructor) : Stream.empty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(getClass().getName()).append("[");
+        properties.entrySet()
+                .forEach(e -> builder.append("[").append(e.getKey()).append(" : ").append(e.getValue()).append("]"));
+        builder.append("]");
+        return builder.toString();
+    }
+}
