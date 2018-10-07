4059addbbf6046cd1a80bb9ce57f5e51773f4205
saxing <saxlh@foxmail.com>
2018-10-07 14:52:56 +0800

    abstract factory

---
+++ a0015_java_design_pattern/aa0015_02_abstract_factory/src/main/java/org/saxing/abstract_factory/Aa001502AbstractFactoryApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.abstract_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001502AbstractFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001502AbstractFactoryApplication.class, args);
+    }
+}
