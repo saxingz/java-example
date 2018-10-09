3e03874061c00798211228e25eb8544c8dc9677f
saxing <saxlh@foxmail.com>
2018-10-09 22:12:08 +0800

    design pattern: adapter

--- a0015_java_design_pattern/aa0015_04_adapter/src/main/java/org/saxing/adapter/App.java
+++ a0015_java_design_pattern/aa0015_04_adapter/src/main/java/org/saxing/adapter/App.java
@@ -3,10 +3,16 @@ package org.saxing.adapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing  2018/10/9 22:11
+ */
+//@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        Captain captain = new Captain(new FishingBoatAdapter());
+        captain.row();
     }
 }
