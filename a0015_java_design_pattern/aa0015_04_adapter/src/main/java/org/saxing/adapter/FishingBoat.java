19310294f9e43127a0b9ec1c95685b661c48e486
saxing <saxlh@foxmail.com>
2018-10-09 22:11:56 +0800

    design pattern: adapter

---
+++ a0015_java_design_pattern/aa0015_04_adapter/src/main/java/org/saxing/adapter/FishingBoat.java
@@ -0,0 +1,20 @@
+package org.saxing.adapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * fish boat
+ *
+ * @author saxing  2018/10/9 22:06
+ */
+public class FishingBoat {
+
+    private static final Logger logger = LoggerFactory.getLogger(FishingBoat.class);
+
+    public void sail(){
+        logger.info("The fishing boat is sailing");
+    }
+
+}
