18600453dd4d3d773ecc75e91c51883594515a6c
saxing <saxlh@foxmail.com>
2018-10-09 22:02:15 +0800

    design pattern: adapter

---
+++ a0015_java_design_pattern/aa0015_04_adapter/src/main/java/org/saxing/adapter/Aa001504AdapterApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001504AdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001504AdapterApplication.class, args);
+    }
+}
---
+++ a0015_java_design_pattern/aa0015_04_adapter/src/test/java/org/saxing/adapter/Aa001504AdapterApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.adapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001504AdapterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
