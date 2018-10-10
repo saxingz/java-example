2aac3c2839d29548f59bfbe4523b3c617f2c9932
saxing <saxlh@foxmail.com>
2018-10-10 20:35:00 +0800

    design pattern: aggregator service

---
+++ a0015_java_design_pattern/aa0015_05_aggregator-microservice/src/main/java/org/saxing/aggregatormicroservice/Aa001505AggregatorMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.aggregatormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001505AggregatorMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001505AggregatorMicroserviceApplication.class, args);
+    }
+}
