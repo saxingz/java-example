b7bf72af5668d5c165d75b18e5970a2876dd426f
saxing <saxlh@foxmail.com>
2018-10-10 20:37:00 +0800

    design pattern: aggregator service

---
+++ a0015_java_design_pattern/aa0015_05_aggregator-microservice/aa0015_05_01_aggregator-service/src/main/java/org/saxing/aggregatorservice/Aa00150501AggregatorServiceApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.aggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa00150501AggregatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa00150501AggregatorServiceApplication.class, args);
+    }
+}
---
+++ a0015_java_design_pattern/aa0015_05_aggregator-microservice/aa0015_05_01_aggregator-service/src/test/java/org/saxing/aggregatorservice/Aa00150501AggregatorServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.aggregatorservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa00150501AggregatorServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
