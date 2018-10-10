501969c91fdeb2a83a354869353f153279288d7c
saxing <saxlh@foxmail.com>
2018-10-10 21:07:04 +0800

    design pattern: aggregator service

--- a0015_java_design_pattern/aa0015_05_aggregator-microservice/aa0015_05_01_information_microservice/src/main/java/org/saxing/information_microservice/InformationController.java
+++ a0015_java_design_pattern/aa0015_05_aggregator-microservice/aa0015_05_01_information_microservice/src/main/java/org/saxing/information_microservice/InformationController.java
@@ -2,12 +2,14 @@ package org.saxing.information_microservice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * information controller
  *
  * @author saxing  2018/10/10 20:47
  */
+@RestController
 public class InformationController {
 
     @RequestMapping(value = "/information", method = RequestMethod.GET)
