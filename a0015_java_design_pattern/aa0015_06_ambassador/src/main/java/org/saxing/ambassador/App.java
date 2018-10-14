bb4d85811db1ce212ee5c65c58a22583e53176c4
saxing <saxlh@foxmail.com>
2018-10-14 14:15:01 +0800

    design pattern: ambassador

--- a0015_java_design_pattern/aa0015_06_ambassador/src/main/java/org/saxing/ambassador/App.java
+++ a0015_java_design_pattern/aa0015_06_ambassador/src/main/java/org/saxing/ambassador/App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Aa001506AmbassadorApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001506AmbassadorApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
--- a0015_java_design_pattern/aa0015_06_ambassador/src/test/java/org/saxing/ambassador/AppTests.java
+++ a0015_java_design_pattern/aa0015_06_ambassador/src/test/java/org/saxing/ambassador/AppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Aa001506AmbassadorApplicationTests {
+public class AppTests {
 
     @Test
     public void contextLoads() {
