0dd5217b02590d6ab2d4b2fb11b2da4716749ebb
saxing <saxlh@foxmail.com>
2018-10-14 15:18:15 +0800

    design pattern: ambassador

---
+++ a0015_java_design_pattern/aa0015_06_ambassador/src/main/java/org/saxing/ambassador/RemoteServiceInterface.java
@@ -0,0 +1,13 @@
+package org.saxing.ambassador;
+
+/**
+ *
+ * remoteservice interface
+ *
+ * @author saxing  2018/10/14 14:23
+ */
+public interface RemoteServiceInterface {
+
+    long doRemoteFunction(int value) throws Exception;
+
+}
