8f48b2199f9677a02b54b257478e7c5546354850
saxing <saxlh@foxmail.com>
2018-10-14 15:18:56 +0800

    design pattern: ambassador

--- a0015_java_design_pattern/aa0015_06_ambassador/src/main/java/org/saxing/ambassador/App.java
+++ a0015_java_design_pattern/aa0015_06_ambassador/src/main/java/org/saxing/ambassador/App.java
@@ -3,10 +3,18 @@ package org.saxing.ambassador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing  2018/10/14 15:03
+ */
+//@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        Client host1 = new Client();
+        Client host2 = new Client();
+        host1.useService(12);
+        host2.useService(73);
     }
 }
