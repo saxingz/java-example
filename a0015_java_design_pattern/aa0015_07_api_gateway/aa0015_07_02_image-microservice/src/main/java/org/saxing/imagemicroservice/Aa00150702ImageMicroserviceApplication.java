92a175a6ac04ad27920cea4b8758ec0b403ed97c
saxing <saxlh@foxmail.com>
2018-10-16 23:14:25 +0800

    design pattern: api-gateway

---
+++ a0015_java_design_pattern/aa0015_07_api_gateway/aa0015_07_02_image-microservice/src/main/java/org/saxing/imagemicroservice/Aa00150702ImageMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.imagemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa00150702ImageMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa00150702ImageMicroserviceApplication.class, args);
+    }
+}
