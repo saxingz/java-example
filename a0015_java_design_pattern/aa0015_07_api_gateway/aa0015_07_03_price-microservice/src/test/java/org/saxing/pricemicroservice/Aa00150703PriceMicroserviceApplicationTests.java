1415485342663df7201e0fd5137507c05bf8472f
saxing <saxlh@foxmail.com>
2018-10-16 23:15:35 +0800

    design pattern: api-gateway

---
+++ a0015_java_design_pattern/aa0015_07_api_gateway/aa0015_07_03_price-microservice/src/test/java/org/saxing/pricemicroservice/Aa00150703PriceMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.pricemicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa00150703PriceMicroserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
