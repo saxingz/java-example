8c81497084999335b9c3bcd1e1e631ea1526ca28
saxing <saxlh@foxmail.com>
2018-10-16 22:52:15 +0800

    design pattern: api-gateway

---
+++ a0015_java_design_pattern/aa0015_07_api_gateway/aa0015_07_01_api-gateway-service/src/main/java/org/saxing/apigatewayservice/Aa00150701ApiGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa00150701ApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa00150701ApiGatewayServiceApplication.class, args);
+    }
+}
