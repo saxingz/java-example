97b8e06734dbb76cb63e288555894a98dcc96aca
saxing <saxlh@foxmail.com>
2018-10-30 09:15:04 +0800

    design pattern: async method invocation

---
+++ a0015_java_design_pattern/aa0015_08_async-method-invocation/src/main/java/org/saxing/aa0015_08_asyncmethodinvocation/AsyncMethodInvocationApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.aa0015_08_asyncmethodinvocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMethodInvocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncMethodInvocationApplication.class, args);
+    }
+}
---
+++ a0015_java_design_pattern/aa0015_08_async-method-invocation/src/test/java/org/saxing/aa0015_08_asyncmethodinvocation/AsyncMethodInvocationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.aa0015_08_asyncmethodinvocation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsyncMethodInvocationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
