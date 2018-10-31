2dff941d12aeadf676b3d3d20695a43612a05d4f
saxing <saxlh@foxmail.com>
2018-10-31 23:27:39 +0800

    design pattern: balking

---
+++ a0015_java_design_pattern/aa0015_09_balking/src/main/java/org/saxing/balking/Aa001509BalkingApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.balking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001509BalkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001509BalkingApplication.class, args);
+    }
+}
