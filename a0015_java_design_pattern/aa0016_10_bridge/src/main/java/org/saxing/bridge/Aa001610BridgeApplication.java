1a5d361769376ae8a9613ded360cffaa2792beda
saxing <saxlh@foxmail.com>
2018-11-03 17:26:29 +0800

    design pattern: bridge

---
+++ a0015_java_design_pattern/aa0016_10_bridge/src/main/java/org/saxing/bridge/Aa001610BridgeApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001610BridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001610BridgeApplication.class, args);
+    }
+}
