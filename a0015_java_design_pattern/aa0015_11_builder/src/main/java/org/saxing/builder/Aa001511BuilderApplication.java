1e8952c05d17007f9fafb076d081612fe5fe61b8
saxing <saxlh@foxmail.com>
2018-11-04 10:52:21 +0800

    design pattern: builder

---
+++ a0015_java_design_pattern/aa0015_11_builder/src/main/java/org/saxing/builder/Aa001511BuilderApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001511BuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001511BuilderApplication.class, args);
+    }
+}
