6b3cf0907391105e6b4692453c65638f7b13513f
saxing <saxlh@foxmail.com>
2018-11-05 21:35:09 +0800

    design pattern: business-delegate

---
+++ a0015_java_design_pattern/aa0015_12_business-delegate/src/main/java/org/saxing/businessdelegate/Aa001512BusinessDelegateApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.businessdelegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001512BusinessDelegateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001512BusinessDelegateApplication.class, args);
+    }
+}
---
+++ a0015_java_design_pattern/aa0015_12_business-delegate/src/test/java/org/saxing/businessdelegate/Aa001512BusinessDelegateApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.businessdelegate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001512BusinessDelegateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
