7b43d59408af9ace524befaddf7ca3c8bb429039
saxing <saxlh@foxmail.com>
2018-11-05 22:13:52 +0800

    design pattern: business-delegate

--- a0015_java_design_pattern/aa0015_12_business-delegate/src/main/java/org/saxing/businessdelegate/Aa001512BusinessDelegateApplication.java
+++ a0015_java_design_pattern/aa0015_12_business-delegate/src/main/java/org/saxing/businessdelegate/Aa001512BusinessDelegateApplication.java
@@ -3,10 +3,33 @@ package org.saxing.businessdelegate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+
+/**
+ * main
+ *
+ * @author saxing  2018/11/5 22:05
+ */
+//@SpringBootApplication
 public class Aa001512BusinessDelegateApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(Aa001512BusinessDelegateApplication.class, args);
+//    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001512BusinessDelegateApplication.class, args);
+        BusinessDelegate businessDelegate = new BusinessDelegate();
+        BusinessLookup businessLookup = new BusinessLookup();
+        businessLookup.setEjbService(new EjbService());
+        businessLookup.setJmsService(new JmsService());
+
+        businessDelegate.setBusinessLookup(businessLookup);
+        businessDelegate.setServiceType(ServiceType.EJB);
+
+        Client client = new Client(businessDelegate);
+        client.doTask();
+
+        businessDelegate.setServiceType(ServiceType.JMS);
+        client.doTask();
     }
 }
