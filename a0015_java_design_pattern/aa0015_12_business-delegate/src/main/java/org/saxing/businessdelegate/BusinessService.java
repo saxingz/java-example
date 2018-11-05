158922169199d628f03fc88894725534deefdf31
saxing <saxlh@foxmail.com>
2018-11-05 21:38:30 +0800

    design pattern: business-delegate

---
+++ a0015_java_design_pattern/aa0015_12_business-delegate/src/main/java/org/saxing/businessdelegate/BusinessService.java
@@ -0,0 +1,12 @@
+package org.saxing.businessdelegate;
+
+/**
+ * Interface for service implementations
+ *
+ * @author saxing  2018/11/5 21:35
+ */
+public interface BusinessService {
+
+    void doProcessing();
+
+}
