bf6bfd5a0476b76fa0f809e329a7314979bd2912
saxing <saxlh@foxmail.com>
2018-11-06 17:41:45 +0800

    design pattern: caching

---
+++ a0015_java_design_pattern/aa0015_13_caching/src/main/java/org/saxing/caching/Aa001513CachingApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001513CachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001513CachingApplication.class, args);
+    }
+}
