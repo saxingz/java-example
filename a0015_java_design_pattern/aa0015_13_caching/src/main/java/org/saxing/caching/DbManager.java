7ead496fc054855544ef043b5f12c7982557cf12
saxing <saxlh@foxmail.com>
2018-11-14 09:27:47 +0800

    design pattern: caching

--- a0015_java_design_pattern/aa0015_13_caching/src/main/java/org/saxing/caching/DbManager.java
+++ a0015_java_design_pattern/aa0015_13_caching/src/main/java/org/saxing/caching/DbManager.java
@@ -1,7 +1,13 @@
 package org.saxing.caching;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * DbManager
@@ -19,9 +25,77 @@ import com.mongodb.client.MongoDatabase;
 public final class DbManager {
 
     private static MongoClient mongoClient;
-    private static MongoDatabase mongoDatabase;
+    private static MongoDatabase db;
     private static boolean userMongoDB;
 
+    private static Map<String, UserAccount> virtualDB;
 
+    public DbManager() {
+    }
+
+    /**
+     * Create DB
+     */
+    public static void createVirtualDb(){
+        userMongoDB = false;
+        virtualDB = new HashMap<>();
+    }
+
+    /**
+     * Connect to DB
+     *
+     * @throws ParseException
+     */
+    public static void connect() throws ParseException{
+        userMongoDB = false;
+        mongoClient = new MongoClient();
+        db = mongoClient.getDatabase("test");
+    }
+
+    /**
+     * Read user account from DB
+     *
+     * @param userId
+     * @return
+     */
+    public static UserAccount readFromDb(String userId){
+        if (!userMongoDB){
+            if (virtualDB.containsKey(userId)){
+                return virtualDB.get(userId);
+            }
+            return null;
+        }
+        if (db == null){
+            try {
+                connect();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        FindIterable<Document> iterable =
+                db.getCollection("user_accounts").find(new Document("userId", userId));
+        if (iterable == null){
+            return null;
+        }
+        Document doc = iterable.first();
+        return new UserAccount(userId, doc.getString("userName"), doc.getString("additionalInfo"));
+    }
+
+    public static void writeToDb(UserAccount userAccount){
+        if (!userMongoDB){
+            virtualDB.put(userAccount.getUserId(), userAccount);
+            return;
+        }
+        if (db == null){
+            try {
+                connect();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        db.getCollection("user_accounts").insertOne(
+                new Document("userID", userAccount.getUserId()).append("userName", userAccount.getUserName())
+                .append("additionalInfo", userAccount.getAdditionalInfo()));
+    }
 
 }
