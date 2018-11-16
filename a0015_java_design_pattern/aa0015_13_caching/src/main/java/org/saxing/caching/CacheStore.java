c92b1e44f506c42de18ff705e23c5894194b2a04
saxing <saxlh@foxmail.com>
2018-11-16 13:55:55 +0800

    design pattern: caching

---
+++ a0015_java_design_pattern/aa0015_13_caching/src/main/java/org/saxing/caching/CacheStore.java
@@ -0,0 +1,30 @@
+package org.saxing.caching;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The caching strategies are implemented in this class.
+ */
+public class CacheStore {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CacheStore.class);
+
+    static LruCache cache;
+
+    public CacheStore() {
+    }
+
+    /**
+     * Init cache capacity
+     *
+     * @param capacity
+     */
+    public static void initCapacity(int capacity){
+        if (cache == null){
+            cache = new LruCache(capacity);
+        } else {
+            cache.setCapacity(capacity);
+        }
+    }
+}
