d74a38c759c9dfa32f07521ff5adb6c20533b481
saxing <saxlh@foxmail.com>
2018-11-17 11:42:54 +0800

    design pattern: callback

--- a0015_java_design_pattern/aa0015_14_callback/src/main/java/org/saxing/callback/Aa001514CallbackApplication.java
+++ a0015_java_design_pattern/aa0015_14_callback/src/main/java/org/saxing/callback/Aa001514CallbackApplication.java
@@ -1,12 +1,30 @@
 package org.saxing.callback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ * 
+ * @author saxing  2018/11/17 11:41
+ */
+//@SpringBootApplication
 public class Aa001514CallbackApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(Aa001514CallbackApplication.class, args);
+//    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001514CallbackApplication.class);
+
+    /**
+     * Program entry point
+     */
     public static void main(String[] args) {
-        SpringApplication.run(Aa001514CallbackApplication.class, args);
+        Task task = new SimpleTask();
+        Callback callback = () -> LOGGER.info("I'm done now.");
+        task.executeWith(callback);
     }
 }
