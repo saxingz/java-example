608777d60ff1f1784ac270bc23ff47385a1297fc
saxing <saxlh@foxmail.com>
2018-11-17 11:22:16 +0800

    design pattern: callback

---
+++ a0015_java_design_pattern/aa0015_14_callback/src/main/java/org/saxing/callback/Aa001514CallbackApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.callback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001514CallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001514CallbackApplication.class, args);
+    }
+}
