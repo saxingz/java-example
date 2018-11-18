8c737c313058cf45bad9a26cdb3435a4d017a042
saxing <saxlh@foxmail.com>
2018-11-18 11:11:11 +0800

    design pattern: chain

---
+++ a0015_java_design_pattern/aa0015_15_chain/src/main/java/org/saxing/chain/Aa001515ChainApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001515ChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001515ChainApplication.class, args);
+    }
+}
