303cf6d764482a0e7e153a1d96b82f9a6eac425c
saxing <saxlh@foxmail.com>
2018-11-18 15:04:06 +0800

    design pattern: chain

---
+++ a0015_java_design_pattern/aa0015_15_chain/src/main/java/org/saxing/chain/RequestType.java
@@ -0,0 +1,12 @@
+package org.saxing.chain;
+
+/**
+ * RequestType enumeration
+ * 
+ * @author saxing  2018/11/18 14:55
+ */
+public enum RequestType {
+
+    DEFEND_CASTLE, TORTURE_PRISONER, COLLECT_TAX
+
+}
