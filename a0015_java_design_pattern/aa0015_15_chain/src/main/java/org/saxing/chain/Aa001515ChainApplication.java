52d3aa182021121c8b77ea40f7723f5ce80006fe
saxing <saxlh@foxmail.com>
2018-11-18 15:24:53 +0800

    design pattern: chain

--- a0015_java_design_pattern/aa0015_15_chain/src/main/java/org/saxing/chain/Aa001515ChainApplication.java
+++ a0015_java_design_pattern/aa0015_15_chain/src/main/java/org/saxing/chain/Aa001515ChainApplication.java
@@ -3,10 +3,19 @@ package org.saxing.chain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Aa001515ChainApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(Aa001515ChainApplication.class, args);
+//    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001515ChainApplication.class, args);
+        OrcKing orcKing = new OrcKing();
+        orcKing.makeRequest(new Request(RequestType.COLLECT_TAX, "COLLECT_TAX"));
+        orcKing.makeRequest(new Request(RequestType.DEFEND_CASTLE, "DEFEND_CASTLE"));
+        orcKing.makeRequest(new Request(RequestType.TORTURE_PRISONER, "TORTURE_PRISONER"));
+
     }
+
 }
