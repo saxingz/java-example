6fc99c670e82fa9b246b80b932762a367fdf072d
saxing <saxlh@foxmail.com>
2018-11-23 14:20:21 +0800

    design pattern: command

---
+++ a0015_java_design_pattern/aa0015_17_command/src/main/java/org/saxing/command/Aa001517CommandApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001517CommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001517CommandApplication.class, args);
+    }
+}
