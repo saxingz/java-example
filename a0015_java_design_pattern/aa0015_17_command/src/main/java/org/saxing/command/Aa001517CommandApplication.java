d03ded1e55d59b986708d887f88fd61a3a6b5dcd
saxing <saxlh@foxmail.com>
2018-11-23 15:17:30 +0800

    design pattern: command

--- a0015_java_design_pattern/aa0015_17_command/src/main/java/org/saxing/command/Aa001517CommandApplication.java
+++ a0015_java_design_pattern/aa0015_17_command/src/main/java/org/saxing/command/Aa001517CommandApplication.java
@@ -3,10 +3,42 @@ package org.saxing.command;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing  2018/11/23 15:07
+ */
+//@SpringBootApplication
 public class Aa001517CommandApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Aa001517CommandApplication.class, args);
+//    }
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001517CommandApplication.class, args);
+        Wizard wizard = new Wizard();
+        Goblin goblin = new Goblin();
+
+        goblin.printStatus();
+
+        wizard.castSpell(new ShrinkSpell(), goblin);
+        goblin.printStatus();
+
+        wizard.castSpell(new InvisibilitySpell(), goblin);
+        goblin.printStatus();
+
+        wizard.undoLastSpell();
+        goblin.printStatus();
+
+        wizard.undoLastSpell();
+        goblin.printStatus();
+
+        wizard.redoLastSpell();
+        goblin.printStatus();
+
+        wizard.redoLastSpell();
+        goblin.printStatus();
+
     }
+
 }
