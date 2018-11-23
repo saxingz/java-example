dfd3c62ed852fe7dd1e10c204ad1d64ed030aa63
saxing <saxlh@foxmail.com>
2018-11-23 15:16:40 +0800

    design pattern: command

---
+++ a0015_java_design_pattern/aa0015_17_command/src/main/java/org/saxing/command/ShrinkSpell.java
@@ -0,0 +1,38 @@
+package org.saxing.command;
+
+/**
+ * ShrinkSpell is a concrete command
+ *
+ * @author saxing  2018/11/23 14:42
+ */
+public class ShrinkSpell extends Command {
+
+    private Size oldSize;
+    private Target target;
+
+    @Override
+    public void execute(Target target) {
+        oldSize = target.getSize();
+        target.setSize(Size.SMALL);
+        this.target = target;
+    }
+
+    @Override
+    public void undo() {
+        if (oldSize != null && target != null){
+            Size temp = target.getSize();
+            target.setSize(oldSize);
+            oldSize = temp;
+        }
+    }
+
+    @Override
+    public void redo() {
+        undo();
+    }
+
+    @Override
+    public String toString() {
+        return "Shrink spell";
+    }
+}
