b1d5fe6a918cfcb65bafcc7079fa821a984e545d
saxing <saxlh@foxmail.com>
2018-11-23 15:16:54 +0800

    design pattern: command

---
+++ a0015_java_design_pattern/aa0015_17_command/src/main/java/org/saxing/command/InvisibilitySpell.java
@@ -0,0 +1,36 @@
+package org.saxing.command;
+
+/**
+ * InvisibilitySpell is a concrete command
+ *
+ * @author saxing  2018/11/23 14:49
+ */
+public class InvisibilitySpell extends Command {
+
+    private Target target;
+
+    @Override
+    public void execute(Target target) {
+        target.setVisibility(Visibility.INVISIBLE);
+        this.target = target;
+    }
+
+    @Override
+    public void undo() {
+        if (target != null) {
+            target.setVisibility(Visibility.VISIBLE);
+        }
+    }
+
+    @Override
+    public void redo() {
+        if (target != null) {
+            target.setVisibility(Visibility.INVISIBLE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Invisibility spell";
+    }
+}
