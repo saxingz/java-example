9c2b650ffdb63f55b18414a250fd6720cb85388c
saxing <saxlh@foxmail.com>
2018-11-24 13:08:13 +0800

    design pattern: composite

---
+++ a0015_java_design_pattern/aa0015_18_composite/src/main/java/org/saxing/composite/Letter.java
@@ -0,0 +1,20 @@
+package org.saxing.composite;
+
+/**
+ * Letter
+ *
+ * @author saxing  2018/11/24 12:58
+ */
+public class Letter extends LetterComposite {
+
+    private char c;
+
+    public Letter(char c) {
+        this.c = c;
+    }
+
+    @Override
+    protected void printThisBefore() {
+        System.out.println(c);
+    }
+}
