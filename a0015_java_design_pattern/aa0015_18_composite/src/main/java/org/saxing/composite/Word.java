5f7dcf02c5e3ced0cc37e1cf2ef4b75e529fa282
saxing <saxlh@foxmail.com>
2018-11-24 13:08:34 +0800

    design pattern: composite

---
+++ a0015_java_design_pattern/aa0015_18_composite/src/main/java/org/saxing/composite/Word.java
@@ -0,0 +1,22 @@
+package org.saxing.composite;
+
+import java.util.List;
+
+/**
+ * Word
+ *
+ * @author saxing  2018/11/24 13:00
+ */
+public class Word extends LetterComposite{
+
+    public Word(List<Letter> letters) {
+        for (Letter l : letters){
+            this.add(l);
+        }
+    }
+
+    @Override
+    protected void printThisBefore() {
+        System.out.println(" ");
+    }
+}
