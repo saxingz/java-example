dcd4a2c72bc777009b50317b1c8e7e7bd25237c7
saxing <saxlh@foxmail.com>
2018-11-24 13:14:50 +0800

    design pattern: composite

--- a0015_java_design_pattern/aa0015_18_composite/src/main/java/org/saxing/composite/Aa001518CompositeApplication.java
+++ a0015_java_design_pattern/aa0015_18_composite/src/main/java/org/saxing/composite/Aa001518CompositeApplication.java
@@ -1,12 +1,34 @@
 package org.saxing.composite;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing  2018/11/24 13:12
+ */
+//@SpringBootApplication
 public class Aa001518CompositeApplication {
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(Aa001518CompositeApplication.class, args);
+//    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001518CompositeApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001518CompositeApplication.class, args);
+        LOGGER.info("Message from the orcs: ");
+
+        LetterComposite orcMessage = new Messenger().messageFromOrcs();
+        orcMessage.print();
+
+        LOGGER.info("\nMessage from the elves: ");
+
+        LetterComposite elfMessage = new Messenger().messageFromElves();
+        elfMessage.print();
     }
+
 }
