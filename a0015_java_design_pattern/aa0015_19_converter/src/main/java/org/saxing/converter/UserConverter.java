66f5bd3e91b65b4e56fb8766c1b206bc3140c45a
saxing <saxlh@foxmail.com>
2018-11-26 23:47:13 +0800

    design pattern: converter

--- a0015_java_design_pattern/aa0015_19_converter/src/main/java/org/saxing/converter/UserConverter.java
+++ a0015_java_design_pattern/aa0015_19_converter/src/main/java/org/saxing/converter/UserConverter.java
@@ -5,10 +5,10 @@ import java.util.function.Function;
 /**
  * user converter
  * 
- * @author saxing  2018/11/26 23:42 
+ * @author saxing  2018/11/26 23:42
  */
 public class UserConverter extends Converter<UserDto, User> {
-    public UserConverter(Function<UserDto, User> fromDto, Function<User, UserDto> fromEntity) {
+    public UserConverter() {
         super(userDto -> new User(userDto.getFirstName(), userDto.getLastName(), userDto.isActive(), userDto.getEmail()),
                 user -> new UserDto(user.getFirstName(), user.getLastName(), user.isActive(), user.getUserId()));
     }
