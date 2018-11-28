33a6d063e4be696a4d639937993b1c45f5bd5cae
saxing <saxlh@foxmail.com>
2018-11-28 23:02:38 +0800

    design pattern: cqrs

---
+++ a0015_java_design_pattern/aa0015_20_cqrs/src/main/java/org/saxing/cqrs/Aa001520CqrsApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001520CqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001520CqrsApplication.class, args);
+    }
+}
