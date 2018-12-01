17cdd79c978ec4f8ab5e1d8e900282d0a1981653
saxing <saxlh@foxmail.com>
2018-12-01 21:49:10 +0800

    design pattern: cqrs

--- a0015_java_design_pattern/aa0015_20_cqrs/src/main/java/org/saxing/cqrs/commands/CommandServiceImpl.java
+++ a0015_java_design_pattern/aa0015_20_cqrs/src/main/java/org/saxing/cqrs/commands/CommandServiceImpl.java
@@ -1,20 +1,68 @@
 package org.saxing.cqrs.commands;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.saxing.cqrs.domain.model.Author;
+import org.saxing.cqrs.domain.model.Book;
+import org.saxing.cqrs.util.HibernateUtil;
+
 /**
+ * This class is an implementation of {@link ICommandService} interface. It uses Hibernate as an api for persistence.
  *
  * @author saxing 2018/12/1 21:24
  */
 public class CommandServiceImpl implements ICommandService  {
 
+    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    private Author getAuthorByUsername(String username) {
+        Author author = null;
+        try (Session session = sessionFactory.openSession()) {
+            Query query = session.createQuery("from Author where username=:username");
+            query.setParameter("username", username);
+            author = (Author) query.uniqueResult();
+        }
+        if (author == null) {
+            HibernateUtil.getSessionFactory().close();
+            throw new NullPointerException("Author " + username + " doesn't exist!");
+        }
+        return author;
+    }
+
+    private Book getBookByTitle(String title) {
+        Book book = null;
+        try (Session session = sessionFactory.openSession()) {
+            Query query = session.createQuery("from Book where title=:title");
+            query.setParameter("title", title);
+            book = (Book) query.uniqueResult();
+        }
+        if (book == null) {
+            HibernateUtil.getSessionFactory().close();
+            throw new NullPointerException("Book " + title + " doesn't exist!");
+        }
+        return book;
+    }
 
     @Override
     public void authorCreated(String username, String name, String email) {
-
+        Author author = new Author(username, name, email);
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(author);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void bookAddedToAuthor(String title, double price, String username) {
-
+        Author author = getAuthorByUsername(username);
+        Book book = new Book(title, price, author);
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(book);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
--- a0015_java_design_pattern/aa0015_20_cqrs/src/main/java/org/saxing/cqrs/domain/model/Book.java
+++ a0015_java_design_pattern/aa0015_20_cqrs/src/main/java/org/saxing/cqrs/domain/model/Book.java
@@ -1,6 +1,6 @@
 package org.saxing.cqrs.domain.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 /**
  * book
@@ -11,6 +11,68 @@ import javax.persistence.Entity;
 @Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private double price;
+    @ManyToOne
+    private Author author;
+
+    /**
+     *
+     * @param title
+     *          title of the book
+     * @param price
+     *          price of the book
+     * @param author
+     *          author of the book
+     */
+    public Book(String title, double price, Author author) {
+        this.title = title;
+        this.price = price;
+        this.author = author;
+    }
+
+    protected Book() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", price=" + price + ", author=" + author + "]";
+    }
 
 
 }
