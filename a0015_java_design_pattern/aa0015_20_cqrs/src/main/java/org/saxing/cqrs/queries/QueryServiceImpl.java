5f8f4d9c6bfa2b754591a269becdb56737e5a422
saxing <saxlh@foxmail.com>
2018-12-01 21:54:57 +0800

    design pattern: cqrs

--- a0015_java_design_pattern/aa0015_20_cqrs/src/main/java/org/saxing/cqrs/queries/QueryServiceImpl.java
+++ a0015_java_design_pattern/aa0015_20_cqrs/src/main/java/org/saxing/cqrs/queries/QueryServiceImpl.java
@@ -1,7 +1,12 @@
 package org.saxing.cqrs.queries;
 
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.transform.Transformers;
 import org.saxing.cqrs.dto.Author;
 import org.saxing.cqrs.dto.Book;
+import org.saxing.cqrs.util.HibernateUtil;
 
 import java.math.BigInteger;
 import java.util.List;
@@ -13,28 +18,65 @@ import java.util.List;
  * @author saxing 2018/12/1 21:51
  */
 public class QueryServiceImpl implements IQueryService {
+
+    private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
     @Override
     public Author getAuthorByUsername(String username) {
-        return null;
+        Author authorDTo = null;
+        try (Session session = sessionFactory.openSession()) {
+            SQLQuery sqlQuery = session
+                    .createSQLQuery("SELECT a.username as \"username\", a.name as \"name\", a.email as \"email\""
+                            + "FROM Author a where a.username=:username");
+            sqlQuery.setParameter("username", username);
+            authorDTo = (Author) sqlQuery.setResultTransformer(Transformers.aliasToBean(Author.class)).uniqueResult();
+        }
+        return authorDTo;
     }
 
     @Override
     public Book getBook(String title) {
-        return null;
+        Book bookDTo = null;
+        try (Session session = sessionFactory.openSession()) {
+            SQLQuery sqlQuery = session
+                    .createSQLQuery("SELECT b.title as \"title\", b.price as \"price\"" + " FROM Book b where b.title=:title");
+            sqlQuery.setParameter("title", title);
+            bookDTo = (Book) sqlQuery.setResultTransformer(Transformers.aliasToBean(Book.class)).uniqueResult();
+        }
+        return bookDTo;
     }
 
     @Override
     public List<Book> getAuthorBooks(String username) {
-        return null;
+        List<Book> bookDTos = null;
+        try (Session session = sessionFactory.openSession()) {
+            SQLQuery sqlQuery = session.createSQLQuery("SELECT b.title as \"title\", b.price as \"price\""
+                    + " FROM Author a , Book b where b.author_id = a.id and a.username=:username");
+            sqlQuery.setParameter("username", username);
+            bookDTos = sqlQuery.setResultTransformer(Transformers.aliasToBean(Book.class)).list();
+        }
+        return bookDTos;
     }
 
     @Override
     public BigInteger getAuthorBooksCount(String username) {
-        return null;
+        BigInteger bookcount = null;
+        try (Session session = sessionFactory.openSession()) {
+            SQLQuery sqlQuery = session.createSQLQuery(
+                    "SELECT count(b.title)" + " FROM  Book b, Author a where b.author_id = a.id and a.username=:username");
+            sqlQuery.setParameter("username", username);
+            bookcount = (BigInteger) sqlQuery.uniqueResult();
+        }
+        return bookcount;
     }
 
     @Override
     public BigInteger getAuthorsCount() {
-        return null;
+        BigInteger authorcount = null;
+        try (Session session = sessionFactory.openSession()) {
+            SQLQuery sqlQuery = session.createSQLQuery("SELECT count(id) from Author");
+            authorcount = (BigInteger) sqlQuery.uniqueResult();
+        }
+        return authorcount;
     }
 }
