ddd4d76a951574b98ebf6b5b3c4229f974521bce
saxing <saxlh@foxmail.com>
2018-12-03 13:54:02 +0800

    design pattern: data-bus

---
+++ a0015_java_design_pattern/aa0015_22_data-bus/src/main/java/org/saxing/databus/Aa001522DataBusApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.databus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001522DataBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001522DataBusApplication.class, args);
+    }
+}
