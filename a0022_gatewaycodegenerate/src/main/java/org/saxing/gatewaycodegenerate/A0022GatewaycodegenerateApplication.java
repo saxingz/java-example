25df6988fc9f1789071bd3ce341b2b2df5420bae
saxing <saxlh@foxmail.com>
2018-12-03 16:27:00 +0800

    gateway code generate

---
+++ a0022_gatewaycodegenerate/src/main/java/org/saxing/gatewaycodegenerate/A0022GatewaycodegenerateApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.gatewaycodegenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0022GatewaycodegenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0022GatewaycodegenerateApplication.class, args);
+    }
+}
---
+++ a0022_gatewaycodegenerate/src/test/java/org/saxing/gatewaycodegenerate/A0022GatewaycodegenerateApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.gatewaycodegenerate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0022GatewaycodegenerateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
