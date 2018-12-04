e4aaeb7f00d5d1638b365a091bf803a7d8f1b9c0
saxing <saxlh@foxmail.com>
2018-12-04 10:31:43 +0800

    design pattern: data-bus

---
+++ a0015_java_design_pattern/aa0015_22_data-bus/src/main/java/org/saxing/databus/data/MessageData.java
@@ -0,0 +1,27 @@
+package org.saxing.databus.data;
+
+import org.saxing.databus.AbstactDataType;
+import org.saxing.databus.DataType;
+
+/**
+ * An event raised when a string message is sent.
+ *
+ * @author saxing 2018/12/4 10:27
+ */
+public class MessageData extends AbstactDataType {
+
+    private final String message;
+
+
+    public MessageData(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static DataType of(final String message){
+        return new MessageData(message);
+    }
+}
