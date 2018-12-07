c130cef067bb0c50c042340ae9b9295413cf5ee8
saxing <saxlh@foxmail.com>
2018-12-07 23:09:13 +0800

    design pattern: data transfer object

---
+++ a0015_java_design_pattern/aa0015_24_data-transfer-object/src/main/java/org/saxing/datatransferobject/CustomerResource.java
@@ -0,0 +1,30 @@
+package org.saxing.datatransferobject;
+
+import java.util.List;
+
+/**
+ * CustomerResource
+ *
+ * @author saxing 2018/12/7 22:46
+ */
+public class CustomerResource {
+
+    private List<CustomerDto> customerDtoList;
+
+    public CustomerResource(List<CustomerDto> customerDtoList) {
+        this.customerDtoList = customerDtoList;
+    }
+
+    public List<CustomerDto> getAll(){
+        return customerDtoList;
+    }
+
+    public void save(CustomerDto customerDto){
+        customerDtoList.add(customerDto);
+    }
+
+    public void delete(CustomerDto customerDto){
+        customerDtoList.removeIf(customer -> customer.getId().equals(customerDto.getId()));
+    }
+
+}
