f2d2d2bb02caff3e3ed02aea503476e48b8530ea
saxing <saxlh@foxmail.com>
2018-12-07 22:43:16 +0800

    design pattern: data transfer object

---
+++ a0015_java_design_pattern/aa0015_24_data-transfer-object/src/main/java/org/saxing/datatransferobject/Aa001524DataTransferObjectApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.datatransferobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001524DataTransferObjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001524DataTransferObjectApplication.class, args);
+    }
+}
