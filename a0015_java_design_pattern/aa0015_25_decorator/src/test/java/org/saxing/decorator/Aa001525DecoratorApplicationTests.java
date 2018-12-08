8d41026c50556701adb6fa7c59962e674f7a8470
saxing <saxlh@foxmail.com>
2018-12-08 11:34:39 +0800

    design pattern: decorator

---
+++ a0015_java_design_pattern/aa0015_25_decorator/src/test/java/org/saxing/decorator/Aa001525DecoratorApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.decorator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001525DecoratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
