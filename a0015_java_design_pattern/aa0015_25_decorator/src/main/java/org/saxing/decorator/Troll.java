1edd2da9ad280ec31e70767d98f25ffa660cd41e
saxing <saxlh@foxmail.com>
2018-12-08 11:36:00 +0800

    design pattern: decorator

---
+++ a0015_java_design_pattern/aa0015_25_decorator/src/main/java/org/saxing/decorator/Troll.java
@@ -0,0 +1,16 @@
+package org.saxing.decorator;
+
+/**
+ * Interface for trolls
+ *
+ * @author saxing 2018/12/8 11:35
+ */
+public interface Troll {
+
+    void attack();
+
+    int getAttackPower();
+
+    void fleeBattle();
+
+}
