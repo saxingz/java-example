b61e3dea4b3f6deb70f28abe6fc885a7f395dc05
saxing <saxlh@foxmail.com>
2018-12-08 11:40:30 +0800

    design pattern: decorator

--- a0015_java_design_pattern/aa0015_25_decorator/src/main/java/org/saxing/decorator/Aa001525DecoratorApplication.java
+++ a0015_java_design_pattern/aa0015_25_decorator/src/main/java/org/saxing/decorator/Aa001525DecoratorApplication.java
@@ -1,12 +1,34 @@
 package org.saxing.decorator;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SpringBootApplication
+//@SpringBootApplication
+
+/**
+ * main
+ *
+ * @author saxing 2018/12/8 11:38
+ *
+ */
 public class Aa001525DecoratorApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001525DecoratorApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001525DecoratorApplication.class, args);
+//        SpringApplication.run(Aa001525DecoratorApplication.class, args);
+
+        LOGGER.info("A simple looking troll approaches.");
+        Troll troll = new SimpleTroll();
+        troll.attack();
+        troll.fleeBattle();
+        LOGGER.info("Simple troll power {}.\n", troll.getAttackPower());
+
+        // change the behavior of the simple troll by adding a decorator
+        LOGGER.info("A troll with huge club surprises you.");
+        Troll clubbedTroll = new ClubbedTroll(troll);
+        clubbedTroll.attack();
+        clubbedTroll.fleeBattle();
+        LOGGER.info("Clubbed troll power {}.\n", clubbedTroll.getAttackPower());
     }
 }
