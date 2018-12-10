55855bfb092a182c4350442a0119cb4f7b678780
saxing <saxlh@foxmail.com>
2018-12-10 22:19:02 +0800

    design pattern: delegation

---
+++ a0015_java_design_pattern/aa0015_26_delegation/src/main/java/org/saxing/delegation/Aa001526DelegationApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.delegation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001526DelegationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001526DelegationApplication.class, args);
+    }
+}
---
+++ a0015_java_design_pattern/aa0015_26_delegation/src/test/java/org/saxing/delegation/Aa001526DelegationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.delegation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001526DelegationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
