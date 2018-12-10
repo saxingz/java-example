f6542eaf000bcd1260d7895c86baf92d4d939172
saxing <saxlh@foxmail.com>
2018-12-10 22:22:16 +0800

    design pattern: delegation

--- a0015_java_design_pattern/aa0015_26_delegation/src/main/java/org/saxing/delegation/PrinterController.java
+++ a0015_java_design_pattern/aa0015_26_delegation/src/main/java/org/saxing/delegation/PrinterController.java
@@ -10,10 +10,14 @@ package org.saxing.delegation;
  */
 public class PrinterController implements Printer {
 
+    private final Printer printer;
 
+    public PrinterController(Printer printer) {
+        this.printer = printer;
+    }
 
     @Override
     public void print(String message) {
-
+        printer.print(message);
     }
 }
