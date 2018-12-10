bd214f52cde3b41eaa08ed2ff210127a80dad6d7
saxing <saxlh@foxmail.com>
2018-12-10 22:24:03 +0800

    design pattern: delegation

---
+++ a0015_java_design_pattern/aa0015_26_delegation/src/main/java/org/saxing/delegation/printers/EpsonPrinter.java
@@ -0,0 +1,20 @@
+package org.saxing.delegation.printers;
+
+import org.saxing.delegation.Printer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * epson printer
+ *
+ * @author saxing 2018/12/10 22:23
+ */
+public class EpsonPrinter implements Printer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CanonPrinter.class);
+
+    @Override
+    public void print(String message) {
+        LOGGER.info("Epson Printer : {}", message);
+    }
+}
