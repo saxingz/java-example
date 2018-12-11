ce7328cec4b85d33bb0341033afe56168dcf2853
saxing <saxlh@foxmail.com>
2018-12-11 09:42:11 +0800

    design pattern: dependency injection

---
+++ a0015_java_design_pattern/aa0015_27_dependency-injectoin/src/main/java/org/saxing/dependencyinjectoin/Aa001527DependencyInjectoinApplication.java
@@ -0,0 +1,12 @@
+package org.saxing.dependencyinjectoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001527DependencyInjectoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001527DependencyInjectoinApplication.class, args);
+    }
+}
