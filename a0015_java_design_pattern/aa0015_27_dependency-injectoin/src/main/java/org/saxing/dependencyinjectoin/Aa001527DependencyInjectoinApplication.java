abe2c20ca28299ddadf10ceaaa9a68b9610f638e
saxing <saxlh@foxmail.com>
2018-12-12 23:13:35 +0800

    design pattern: dependency injection

--- a0015_java_design_pattern/aa0015_27_dependency-injectoin/src/main/java/org/saxing/dependencyinjectoin/Aa001527DependencyInjectoinApplication.java
+++ a0015_java_design_pattern/aa0015_27_dependency-injectoin/src/main/java/org/saxing/dependencyinjectoin/Aa001527DependencyInjectoinApplication.java
@@ -1,12 +1,36 @@
 package org.saxing.dependencyinjectoin;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+
+/**
+ * main
+ *
+ * @author saxing 2018/12/12 23:11
+ */
 public class Aa001527DependencyInjectoinApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001527DependencyInjectoinApplication.class, args);
+//        SpringApplication.run(Aa001527DependencyInjectoinApplication.class, args);
+
+        SimpleWizard simpleWizard = new SimpleWizard();
+        simpleWizard.smoke();
+
+        AdvancedWizard advancedWizard = new AdvancedWizard(new SecondBreakfastTobacco());
+        advancedWizard.smoke();
+
+        AdvancedSorceress advancedSorceress = new AdvancedSorceress();
+        advancedSorceress.setTobacco(new SecondBreakfastTobacco());
+        advancedSorceress.smoke();
+
+        Injector injector = Guice.createInjector(new TobaccoModule());
+        GuiceWizard guiceWizard = injector.getInstance(GuiceWizard.class);
+        guiceWizard.smoke();
+
+
     }
 }
--- a0015_java_design_pattern/aa0015_27_dependency-injectoin/src/main/java/org/saxing/dependencyinjectoin/AdvancedSorceress.java
+++ a0015_java_design_pattern/aa0015_27_dependency-injectoin/src/main/java/org/saxing/dependencyinjectoin/AdvancedSorceress.java
@@ -10,7 +10,7 @@ public class AdvancedSorceress implements Wizard {
 
     private Tobacco tobacco;
 
-    public AdvancedSorceress(Tobacco tobacco) {
+    public void setTobacco(Tobacco tobacco) {
         this.tobacco = tobacco;
     }
 
