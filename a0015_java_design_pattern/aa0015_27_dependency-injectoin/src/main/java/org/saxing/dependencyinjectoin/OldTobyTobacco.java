86256c041ec0104abd567c6f07f7d4a608c890f3
saxing <saxlh@foxmail.com>
2018-12-12 23:09:23 +0800

    design pattern: dependency injection

---
+++ a0015_java_design_pattern/aa0015_27_dependency-injectoin/src/main/java/org/saxing/dependencyinjectoin/OldTobyTobacco.java
@@ -0,0 +1,9 @@
+package org.saxing.dependencyinjectoin;
+
+/**
+ * OldTobyTobacco concrete {@link Tobacco} implementation
+ * 
+ * @author saxing 2018/12/12 23:09
+ */
+public class OldTobyTobacco extends Tobacco {
+}
