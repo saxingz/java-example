ac3569a2a891df65b24d81b6986643c9c966e981
saxing <saxlh@foxmail.com>
2018-12-12 23:10:04 +0800

    design pattern: dependency injection

---
+++ a0015_java_design_pattern/aa0015_27_dependency-injectoin/src/main/java/org/saxing/dependencyinjectoin/SecondBreakfastTobacco.java
@@ -0,0 +1,9 @@
+package org.saxing.dependencyinjectoin;
+
+/**
+ * SecondBreakfastTobacco concrete {@link Tobacco} implementation
+ * 
+ * @author saxing 2018/12/12 23:09
+ */
+public class SecondBreakfastTobacco extends Tobacco {
+}
