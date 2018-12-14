d822bef544beaf2b5ffefda3c361c43af7db77f3
saxing <saxlh@foxmail.com>
2018-12-14 09:53:08 +0800

    design pattern: dirty_flag

---
+++ a0015_java_design_pattern/aa0015_28_dirty_flag/src/main/java/org/saxing/dirty_flag/Aa001528DirtyFlagApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.dirty_flag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001528DirtyFlagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001528DirtyFlagApplication.class, args);
+    }
+
+}
+
---
+++ a0015_java_design_pattern/aa0015_28_dirty_flag/src/test/java/org/saxing/dirty_flag/Aa001528DirtyFlagApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.dirty_flag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001528DirtyFlagApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
