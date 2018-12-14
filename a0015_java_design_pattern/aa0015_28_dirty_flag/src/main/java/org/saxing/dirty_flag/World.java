060641ac0f20b6e718ae6edd6db26b87ffff0991
saxing <saxlh@foxmail.com>
2018-12-14 16:26:59 +0800

    design pattern: dirty_flag

---
+++ a0015_java_design_pattern/aa0015_28_dirty_flag/src/main/java/org/saxing/dirty_flag/World.java
@@ -0,0 +1,28 @@
+package org.saxing.dirty_flag;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * World
+ *
+ * @author saxing 2018/12/14 14:31
+ */
+public class World {
+
+    private List<String> countries;
+    private DataFetcher dataFetcher;
+
+    public World() {
+        this.countries = new ArrayList<>();
+        this.dataFetcher = new DataFetcher();
+    }
+
+    public List<String> fetch(){
+        List<String> data = new ArrayList<>();
+
+        countries = data.isEmpty() ? countries : data;
+
+        return countries;
+    }
+}
