d4381c5aefa2a371f5009d2fb861ab3b9b44de3e
saxing <saxlh@foxmail.com>
2018-12-15 15:47:14 +0800

    design pattern: double-checked-blocked

--- a0015_java_design_pattern/aa0015_29_double-checked-blocked/src/main/java/org/saxing/doublecheckedblocked/Aa001529DoubleCheckedBlockedApplication.java
+++ a0015_java_design_pattern/aa0015_29_double-checked-blocked/src/main/java/org/saxing/doublecheckedblocked/Aa001529DoubleCheckedBlockedApplication.java
@@ -1,13 +1,33 @@
 package org.saxing.doublecheckedblocked;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+//@SpringBootApplication
 public class Aa001529DoubleCheckedBlockedApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001529DoubleCheckedBlockedApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001529DoubleCheckedBlockedApplication.class, args);
+        final Inventory inventory = new Inventory(1000);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 3; i++) {
+            executorService.execute(() -> {
+                while (inventory.addItem(new Item())) {};
+            });
+        }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        }catch (InterruptedException e){
+            LOGGER.error("Error waiting for ExecutorService shutdown");
+        }
     }
 
 }
