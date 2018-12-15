54164d239414715ab3dfeae3736b5c8689d93ddb
saxing <saxlh@foxmail.com>
2018-12-15 15:32:23 +0800

    design pattern: double-checked-blocked

---
+++ a0015_java_design_pattern/aa0015_29_double-checked-blocked/src/main/java/org/saxing/doublecheckedblocked/Inventory.java
@@ -0,0 +1,15 @@
+package org.saxing.doublecheckedblocked;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Inventory
+ */
+public class Inventory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Inventory.class);
+
+
+
+}
---
+++ a0015_java_design_pattern/aa0015_29_double-checked-blocked/src/main/java/org/saxing/doublecheckedblocked/Item.java
@@ -0,0 +1,9 @@
+package org.saxing.doublecheckedblocked;
+
+/**
+ * item
+ * 
+ * @author saxing 2018/12/15 15:32
+ */
+public class Item {
+}
