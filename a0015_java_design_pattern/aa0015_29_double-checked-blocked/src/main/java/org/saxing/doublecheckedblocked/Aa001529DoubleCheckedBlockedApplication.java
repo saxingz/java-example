d48f1f0a9c92df757d26af02111117f84c4076b6
saxing <saxlh@foxmail.com>
2018-12-15 15:29:58 +0800

    design pattern: double-checked-blocked

---
+++ a0015_java_design_pattern/aa0015_29_double-checked-blocked/src/main/java/org/saxing/doublecheckedblocked/Aa001529DoubleCheckedBlockedApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.doublecheckedblocked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001529DoubleCheckedBlockedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001529DoubleCheckedBlockedApplication.class, args);
+    }
+
+}
+
---
+++ a0015_java_design_pattern/aa0015_29_double-checked-blocked/src/test/java/org/saxing/doublecheckedblocked/Aa001529DoubleCheckedBlockedApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.doublecheckedblocked;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001529DoubleCheckedBlockedApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
