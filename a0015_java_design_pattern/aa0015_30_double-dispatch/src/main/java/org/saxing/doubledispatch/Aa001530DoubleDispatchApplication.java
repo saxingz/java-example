80991da4228f99a1bd981611cc613bf99728a60e
saxing <saxlh@foxmail.com>
2018-12-16 13:32:36 +0800

    design pattern: double-dispatch

--- a0015_java_design_pattern/aa0015_30_double-dispatch/src/main/java/org/saxing/doubledispatch/Aa001530DoubleDispatchApplication.java
+++ a0015_java_design_pattern/aa0015_30_double-dispatch/src/main/java/org/saxing/doubledispatch/Aa001530DoubleDispatchApplication.java
@@ -3,11 +3,10 @@ package org.saxing.doubledispatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Aa001530DoubleDispatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001530DoubleDispatchApplication.class, args);
+
     }
 
 }
---
+++ a0015_java_design_pattern/aa0015_30_double-dispatch/src/main/java/org/saxing/doubledispatch/Rectangle.java
@@ -0,0 +1,42 @@
+package org.saxing.doubledispatch;
+
+public class Rectangle {
+
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+
+    public Rectangle(int left, int top, int right, int bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    boolean intersectsWith(Rectangle r){
+        return !(r.getLeft() > getRight() || r.getRight() < getLeft() || r.getTop() > getBottom() || r
+                .getBottom() < getTop());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d,%d,%d,%d]", getLeft(), getTop(), getRight(), getBottom());
+    }
+}
