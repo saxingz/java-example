6fd836906a83b8bbdc25d3ea01dfde51538bbac0
saxing <saxlh@foxmail.com>
2018-12-16 13:51:00 +0800

    design pattern: double-dispatch

--- a0015_java_design_pattern/aa0015_30_double-dispatch/src/main/java/org/saxing/doubledispatch/FlamingAsteroid.java
+++ a0015_java_design_pattern/aa0015_30_double-dispatch/src/main/java/org/saxing/doubledispatch/FlamingAsteroid.java
@@ -9,6 +9,11 @@ public class FlamingAsteroid extends Meteoroid {
 
     public FlamingAsteroid(int left, int top, int right, int bottom) {
         super(left, top, right, bottom);
+        setOnFire(true);
     }
 
+    @Override
+    public void collision(GameObject gameObject) {
+        gameObject.collisionResolve(this);
+    }
 }
