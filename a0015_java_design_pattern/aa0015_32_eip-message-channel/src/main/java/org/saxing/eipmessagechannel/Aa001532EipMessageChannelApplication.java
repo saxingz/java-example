80f76e65a3e55d0f9cfdcd95611b563de9e2c8d6
saxing <saxlh@foxmail.com>
2018-12-18 22:19:16 +0800

    design pattern: eip-message-channel

--- a0015_java_design_pattern/aa0015_32_eip-message-channel/src/main/java/org/saxing/eipmessagechannel/Aa001532EipMessageChannelApplication.java
+++ a0015_java_design_pattern/aa0015_32_eip-message-channel/src/main/java/org/saxing/eipmessagechannel/Aa001532EipMessageChannelApplication.java
@@ -1,13 +1,36 @@
 package org.saxing.eipmessagechannel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2018/12/18 22:15
+ */
 public class Aa001532EipMessageChannelApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa001532EipMessageChannelApplication.class, args);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001532EipMessageChannelApplication.class);
+
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("stream:in").to("direct:greetings");
+                from("direct:greetings").to("stream:out");
+            }
+        });
+
+        context.start();
+        context.getRoutes().forEach(r -> LOGGER.info(r.toString()));
+        context.stop();
     }
 
 }
