c2b4b66694c6531cdf2424fad325d7250a15ec5e
saxing <saxlh@foxmail.com>
2018-12-18 22:09:54 +0800

    design pattern: eip-message-channel

---
+++ a0015_java_design_pattern/aa0015_32_eip-message-channel/src/main/java/org/saxing/eipmessagechannel/Aa001532EipMessageChannelApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.eipmessagechannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001532EipMessageChannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001532EipMessageChannelApplication.class, args);
+    }
+
+}
+
