8795d355a0f33cb8fdc4e87cabcaf71e623a0bfd
saxing <saxlh@foxmail.com>
2018-12-19 20:05:42 +0800

    design pattern: eip-publish-subscribe

--- a0015_java_design_pattern/aa0015_33_eip-publish-subscribe/src/main/java/org/saxing/eippublishsubscribe/Aa001533EipPublishSubscribeApplication.java
+++ a0015_java_design_pattern/aa0015_33_eip-publish-subscribe/src/main/java/org/saxing/eippublishsubscribe/Aa001533EipPublishSubscribeApplication.java
@@ -1,13 +1,37 @@
 package org.saxing.eippublishsubscribe;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ * 
+ * @author saxing 2018/12/19 20:03
+ */
 public class Aa001533EipPublishSubscribeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa001533EipPublishSubscribeApplication.class, args);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001533EipPublishSubscribeApplication.class);
+
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:origin").multicast().to("mock:foo", "mock:bar", "stream:out");
+            }
+        });
+
+        ProducerTemplate template = context.createProducerTemplate();
+        context.start();
+        context.getRoutes().forEach(r -> LOGGER.info(r.toString()));
+        template.sendBody("direct:origin", "Hello from origin");
+        context.stop();
     }
 
 }
