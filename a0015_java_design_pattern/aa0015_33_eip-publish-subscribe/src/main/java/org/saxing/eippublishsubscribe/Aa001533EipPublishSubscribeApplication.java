f7072f2ff968ae6ad6f4b30efb587c0f70a9e54d
saxing <saxlh@foxmail.com>
2018-12-19 20:01:29 +0800

    design pattern: eip-publish-subscribe

---
+++ a0015_java_design_pattern/aa0015_33_eip-publish-subscribe/src/main/java/org/saxing/eippublishsubscribe/Aa001533EipPublishSubscribeApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.eippublishsubscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001533EipPublishSubscribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001533EipPublishSubscribeApplication.class, args);
+    }
+
+}
+
