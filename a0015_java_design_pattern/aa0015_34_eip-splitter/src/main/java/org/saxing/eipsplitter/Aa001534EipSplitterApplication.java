915729cf542dc785d4f78716e299deea1e0a539b
saxing <saxlh@foxmail.com>
2018-12-20 23:36:32 +0800

    design pattern: eip-splitter

---
+++ a0015_java_design_pattern/aa0015_34_eip-splitter/src/main/java/org/saxing/eipsplitter/Aa001534EipSplitterApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.eipsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001534EipSplitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001534EipSplitterApplication.class, args);
+    }
+
+}
+
