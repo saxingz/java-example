651934b0858ac194f0b26d718c8f768995bd741f
saxing <saxlh@foxmail.com>
2018-12-20 23:37:52 +0800

    design pattern: eip-splitter

---
+++ a0015_java_design_pattern/aa0015_34_eip-splitter/src/main/java/org/saxing/eipsplitter/routes/SplitterRoute.java
@@ -0,0 +1,12 @@
+package org.saxing.eipsplitter.routes;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * SplitterRoute
+ * 
+ * @author saxing 2018/12/20 23:37
+ */
+@Component
+public class SplitterRoute {
+}
