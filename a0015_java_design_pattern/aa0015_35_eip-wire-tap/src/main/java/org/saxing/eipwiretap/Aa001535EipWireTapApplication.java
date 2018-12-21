faac453c0caf40276f3f78c1f87c7245015eee71
saxing <saxlh@foxmail.com>
2018-12-21 23:42:32 +0800

    design pattern: eip-wire-tap

--- a0015_java_design_pattern/aa0015_35_eip-wire-tap/src/main/java/org/saxing/eipwiretap/Aa001535EipWireTapApplication.java
+++ a0015_java_design_pattern/aa0015_35_eip-wire-tap/src/main/java/org/saxing/eipwiretap/Aa001535EipWireTapApplication.java
@@ -1,13 +1,40 @@
 package org.saxing.eipwiretap;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * main
+ * 
+ * @author saxing 2018/12/21 23:40
+ */
 @SpringBootApplication
 public class Aa001535EipWireTapApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa001535EipWireTapApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        // Run Spring Boot application and obtain ApplicationContext
+        ConfigurableApplicationContext context = SpringApplication.run(Aa001535EipWireTapApplication.class, args);
+
+        // Get CamelContext from ApplicationContext
+        CamelContext camelContext = (CamelContext) context.getBean("camelContext");
+
+        // Add a new routes that will handle endpoints form WireTapRoute class.
+        camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("{{endpoint}}").log("ENDPOINT: ${body}");
+                from("{{wireTapEndpoint}}").log("WIRETAPPED ENDPOINT: ${body}");
+            }
+        });
+
+        // Add producer that will send test message to an entry point in WireTapRoute
+        camelContext.createProducerTemplate().sendBody("{{entry}}", "Test message");
+
+        SpringApplication.exit(context);
+
     }
 
 }
