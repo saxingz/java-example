694362f7ef28b1a7f80f10fe22a9f5c90782eb56
saxing <saxlh@foxmail.com>
2018-12-21 23:33:42 +0800

    design pattern: eip-wire-tap

---
+++ a0015_java_design_pattern/aa0015_35_eip-wire-tap/src/main/java/org/saxing/eipwiretap/Aa001535EipWireTapApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.eipwiretap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001535EipWireTapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001535EipWireTapApplication.class, args);
+    }
+
+}
+
---
+++ a0015_java_design_pattern/aa0015_35_eip-wire-tap/src/test/java/org/saxing/eipwiretap/Aa001535EipWireTapApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.eipwiretap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001535EipWireTapApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
