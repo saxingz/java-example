4dfc19199fa0147537293b21f4e69225ea6693bc
saxing <saxlh@foxmail.com>
2018-12-23 17:46:05 +0800

    design pattern: event-aggregator

--- a0015_java_design_pattern/aa0015_36_event-aggregator/src/main/java/org/saxing/eventaggregator/Aa001536EventAggregatorApplication.java
+++ a0015_java_design_pattern/aa0015_36_event-aggregator/src/main/java/org/saxing/eventaggregator/Aa001536EventAggregatorApplication.java
@@ -3,11 +3,31 @@ package org.saxing.eventaggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * main
+ *
+ * @author saxing 2018/12/23 17:42
+ */
 public class Aa001536EventAggregatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001536EventAggregatorApplication.class, args);
+        KingJoffrey kingJoffrey = new KingJoffrey();
+        KingsHand kingsHand = new KingsHand(kingJoffrey);
+
+        List<EventEmitter> eventEmitters = new ArrayList<>();
+        eventEmitters.add(kingsHand);
+        eventEmitters.add(new LordBaelish(kingsHand));
+        eventEmitters.add(new LordVarys(kingsHand));
+        eventEmitters.add(new Scout(kingsHand));
+
+        for (Weekday weekday : Weekday.values()){
+            for (EventEmitter eventEmitter : eventEmitters){
+                eventEmitter.timePasses(weekday);
+            }
+        }
     }
 
 }
