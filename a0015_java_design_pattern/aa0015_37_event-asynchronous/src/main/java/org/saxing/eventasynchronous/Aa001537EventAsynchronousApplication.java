330010e98ec8e1e6ea4fda130e382a828044b878
saxing <saxlh@foxmail.com>
2018-12-24 23:08:04 +0800

    design pattern: event-asynchronous

---
+++ a0015_java_design_pattern/aa0015_37_event-asynchronous/src/main/java/org/saxing/eventasynchronous/Aa001537EventAsynchronousApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.eventasynchronous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001537EventAsynchronousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001537EventAsynchronousApplication.class, args);
+    }
+
+}
+
