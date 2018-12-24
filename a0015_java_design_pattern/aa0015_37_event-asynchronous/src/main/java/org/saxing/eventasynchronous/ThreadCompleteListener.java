70da8202e10362725a97b0b8e89ddf511f3af0a3
saxing <saxlh@foxmail.com>
2018-12-24 23:15:12 +0800

    design pattern: event-asynchronous

---
+++ a0015_java_design_pattern/aa0015_37_event-asynchronous/src/main/java/org/saxing/eventasynchronous/ThreadCompleteListener.java
@@ -0,0 +1,12 @@
+package org.saxing.eventasynchronous;
+
+/**
+ * ThreadCompleteListener
+ * 
+ * @author saxing 2018/12/24 23:14
+ */
+public interface ThreadCompleteListener {
+
+    void completedEventHandler(final int eventId);
+
+}
