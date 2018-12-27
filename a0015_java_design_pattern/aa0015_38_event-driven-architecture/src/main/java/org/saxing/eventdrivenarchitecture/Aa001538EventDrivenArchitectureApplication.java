927f335d0a270fcf4ba48c53199d8313b727cc56
saxing <saxlh@foxmail.com>
2018-12-27 22:36:39 +0800

    design pattern: event-driven-architecture

--- a0015_java_design_pattern/aa0015_38_event-driven-architecture/src/main/java/org/saxing/eventdrivenarchitecture/Aa001538EventDrivenArchitectureApplication.java
+++ a0015_java_design_pattern/aa0015_38_event-driven-architecture/src/main/java/org/saxing/eventdrivenarchitecture/Aa001538EventDrivenArchitectureApplication.java
@@ -1,13 +1,31 @@
 package org.saxing.eventdrivenarchitecture;
 
+import org.saxing.eventdrivenarchitecture.event.UserCreatedEvent;
+import org.saxing.eventdrivenarchitecture.event.UserUpdatedEvent;
+import org.saxing.eventdrivenarchitecture.framework.EventDispatcher;
+import org.saxing.eventdrivenarchitecture.handler.UserCreatedEventHandler;
+import org.saxing.eventdrivenarchitecture.handler.UserUpdateEventHandler;
+import org.saxing.eventdrivenarchitecture.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2018/12/27 22:34
+ */
 public class Aa001538EventDrivenArchitectureApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001538EventDrivenArchitectureApplication.class, args);
+
+        EventDispatcher eventDispatcher = new EventDispatcher();
+        eventDispatcher.registerHandler(UserCreatedEvent.class, new UserCreatedEventHandler());
+        eventDispatcher.registerHandler(UserUpdatedEvent.class, new UserUpdateEventHandler());
+
+        User user = new User("saxing");
+        eventDispatcher.dispatch(new UserCreatedEvent(user));
+        eventDispatcher.dispatch(new UserUpdatedEvent(user));
+
     }
 
 }
