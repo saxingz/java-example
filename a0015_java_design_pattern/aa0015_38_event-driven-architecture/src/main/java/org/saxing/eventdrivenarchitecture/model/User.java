a97b751fc085bc486a85e01f5f149a0e7fe7f691
saxing <saxlh@foxmail.com>
2018-12-27 22:19:39 +0800

    design pattern: event-driven-architecture

---
+++ a0015_java_design_pattern/aa0015_38_event-driven-architecture/src/main/java/org/saxing/eventdrivenarchitecture/model/User.java
@@ -0,0 +1,19 @@
+package org.saxing.eventdrivenarchitecture.model;
+
+/**
+ * user
+ *
+ * @author saxing 2018/12/27 22:19
+ */
+public class User {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
