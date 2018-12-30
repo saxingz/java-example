a588cff2f38a9ba06ebe7058b69959c99059c769
saxing <saxlh@foxmail.com>
2018-12-30 22:49:13 +0800

    design pattern: event-queue

--- a0015_java_design_pattern/aa0015_39_event-queue/src/main/java/org/saxing/eventqueue/Aa001539EventQueueApplication.java
+++ a0015_java_design_pattern/aa0015_39_event-queue/src/main/java/org/saxing/eventqueue/Aa001539EventQueueApplication.java
@@ -1,13 +1,28 @@
 package org.saxing.eventqueue;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-@SpringBootApplication
+/**
+ * main
+ * 
+ * @author saxing 2018/12/30 22:48
+ */
 public class Aa001539EventQueueApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa001539EventQueueApplication.class, args);
+    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, InterruptedException {
+        Audio audio = Audio.getInstance();
+        audio.playSound(audio.getAudioStream("./etc/Bass-Drum-1.wav"), -10.0f);
+        audio.playSound(audio.getAudioStream("./etc/Closed-Hi-Hat-1.wav"), -8.0f);
+
+        System.out.println("Press Enter key to stop the program...");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            br.read();
+        }
+        audio.stopService();
     }
 
 }
