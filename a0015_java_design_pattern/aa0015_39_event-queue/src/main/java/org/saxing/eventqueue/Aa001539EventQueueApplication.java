c2dae69f3ce585351747e6779a6ab1a574dd9b6b
saxing <saxlh@foxmail.com>
2018-12-31 11:14:37 +0800

    design pattern: event-queue

--- a0015_java_design_pattern/aa0015_39_event-queue/src/main/java/org/saxing/eventqueue/Aa001539EventQueueApplication.java
+++ a0015_java_design_pattern/aa0015_39_event-queue/src/main/java/org/saxing/eventqueue/Aa001539EventQueueApplication.java
@@ -14,8 +14,8 @@ public class Aa001539EventQueueApplication {
 
     public static void main(String[] args) throws IOException, UnsupportedAudioFileException, InterruptedException {
         Audio audio = Audio.getInstance();
-        audio.playSound(audio.getAudioStream("./etc/Bass-Drum-1.wav"), -10.0f);
-        audio.playSound(audio.getAudioStream("./etc/Closed-Hi-Hat-1.wav"), -8.0f);
+        audio.playSound(audio.getAudioStream("D:\\Code\\study\\java-example\\a0015_java_design_pattern\\aa0015_39_event-queue\\etc\\Bass-Drum-1.wav"), -10.0f);
+        audio.playSound(audio.getAudioStream("D:\\Code\\study\\java-example\\a0015_java_design_pattern\\aa0015_39_event-queue\\etc\\Closed-Hi-Hat-1.wav"), -8.0f);
 
         System.out.println("Press Enter key to stop the program...");
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
