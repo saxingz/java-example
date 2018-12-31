c8c44c8ecda1b9421c64433b3261eb62ec867263
saxing <saxlh@foxmail.com>
2018-12-31 11:33:01 +0800

    design pattern: event-sourcing

---
+++ a0015_java_design_pattern/aa0015_40_event_sourcing/src/main/java/org/saxing/event_sourcing/Aa001540EventSourcingApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.event_sourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001540EventSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001540EventSourcingApplication.class, args);
+    }
+
+}
+
