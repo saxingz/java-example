e1002d214d74d9ec62dacb544e2b7c0294a14b0a
saxing <saxlh@foxmail.com>
2019-01-03 23:19:08 +0800

    design pattern: event-sourcing

---
+++ a0015_java_design_pattern/aa0015_40_event_sourcing/src/main/java/org/saxing/state/AccountAggregate.java
@@ -0,0 +1,35 @@
+package org.saxing.state;
+
+import org.saxing.domain.Account;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * account aggregate
+ *
+ * @author saxing 2019/1/3 23:16
+ */
+public class AccountAggregate {
+
+    private static Map<Integer, Account> accounts = new HashMap<>();
+
+    private AccountAggregate() {
+    }
+
+    public static void putAccount(Account account){
+        accounts.put(account.getAccountNo(), account);
+    }
+
+    public static Account getAccount(int accountNo){
+        Account account = accounts.get(accountNo);
+        if (account == null){
+            return null;
+        }
+        return account.copy();
+    }
+
+    public static void resetState(){
+        accounts = new HashMap<>();
+    }
+}
