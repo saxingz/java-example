8ac989f8c8fa4e33a302a323d257fa2d66bc8a31
saxing <saxlh@foxmail.com>
2019-01-03 23:39:36 +0800

    design pattern: event-sourcing

--- a0015_java_design_pattern/aa0015_40_event_sourcing/src/main/java/org/saxing/event_sourcing/processor/JsonFileJournal.java
+++ a0015_java_design_pattern/aa0015_40_event_sourcing/src/main/java/org/saxing/event_sourcing/processor/JsonFileJournal.java
@@ -2,9 +2,11 @@ package org.saxing.event_sourcing.processor;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.saxing.event_sourcing.event.AccountCreateEvent;
 import org.saxing.event_sourcing.event.DomainEvent;
 import org.saxing.event_sourcing.event.MoneyDepositEvent;
+import org.saxing.event_sourcing.event.MoneyTransferEvent;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -55,6 +57,49 @@ public class JsonFileJournal {
             jsonElement = gson.toJsonTree(domainEvent, AccountCreateEvent.class);
         } else if (domainEvent instanceof MoneyDepositEvent){
             jsonElement = gson.toJsonTree(domainEvent, AccountCreateEvent.class);
+        } else if (domainEvent instanceof MoneyTransferEvent){
+            jsonElement = gson.toJsonTree(domainEvent, MoneyTransferEvent.class);
+        } else {
+            throw new RuntimeException("Journal Event not recegnized");
         }
+
+        try (Writer output = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(aFile, true), "UTF-8"))) {
+            String eventString = jsonElement.toString();
+            output.write(eventString + "\r\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Read next domain event.
+     *
+     * @return
+     */
+    public DomainEvent readNext(){
+        if (index >= events.size()){
+            return null;
+        }
+        String event = events.get(index);
+        index++;
+
+        JsonParser parser = new JsonParser();
+        JsonElement jsonElement = parser.parse(event);
+        String eventClassName = jsonElement.getAsJsonObject().get("eventClassName").getAsString();
+        Gson gson = new Gson();
+        DomainEvent domainEvent;
+        if (eventClassName.equals("AccountCreateEvent")) {
+            domainEvent = gson.fromJson(jsonElement, AccountCreateEvent.class);
+        } else if (eventClassName.equals("MoneyDepositEvent")) {
+            domainEvent = gson.fromJson(jsonElement, MoneyDepositEvent.class);
+        } else if (eventClassName.equals("MoneyTransferEvent")) {
+            domainEvent = gson.fromJson(jsonElement, MoneyTransferEvent.class);
+        }  else {
+            throw new RuntimeException("Journal Event not recegnized");
+        }
+
+        domainEvent.setRealTime(false);
+        return domainEvent;
     }
 }
