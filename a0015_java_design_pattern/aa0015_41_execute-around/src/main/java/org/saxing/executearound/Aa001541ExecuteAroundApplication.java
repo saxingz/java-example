8efd52181f72f09f12aaa14881b7f03a8a9276ed
saxing <saxlh@foxmail.com>
2019-01-05 14:17:16 +0800

    design pattern: execute-around

---
+++ a0015_java_design_pattern/aa0015_41_execute-around/src/main/java/org/saxing/executearound/Aa001541ExecuteAroundApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.executearound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001541ExecuteAroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001541ExecuteAroundApplication.class, args);
+    }
+
+}
+
