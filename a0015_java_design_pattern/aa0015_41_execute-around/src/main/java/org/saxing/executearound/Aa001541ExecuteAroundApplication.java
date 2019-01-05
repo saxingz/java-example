2ea84bc98967014c8a412098182e2b07ac322899
saxing <saxlh@foxmail.com>
2019-01-05 14:23:17 +0800

    design pattern: execute-around

--- a0015_java_design_pattern/aa0015_41_execute-around/src/main/java/org/saxing/executearound/Aa001541ExecuteAroundApplication.java
+++ a0015_java_design_pattern/aa0015_41_execute-around/src/main/java/org/saxing/executearound/Aa001541ExecuteAroundApplication.java
@@ -3,11 +3,22 @@ package org.saxing.executearound;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.io.IOException;
+
+/**
+ * main
+ *
+ * @author saxing 2019/1/5 14:21
+ */
 public class Aa001541ExecuteAroundApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa001541ExecuteAroundApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        FileWriterAction writeHello = writer -> {
+            writer.write("Hello");
+            writer.append(" ");
+            writer.append("there!");
+        };
+        new SimpleFileWriter("hello.txt", writeHello);
     }
 
 }
