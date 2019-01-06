dff5a4aaf76048e95ef29f56e3e283f4a3b52c63
saxing <saxlh@foxmail.com>
2019-01-06 11:28:55 +0800

    design pattern: extension-objects

---
+++ a0015_java_design_pattern/aa0015_42_extension-objects/src/main/java/org/saxing/extensionobjects/abstractextensions/SergeantExtension.java
@@ -0,0 +1,12 @@
+package org.saxing.extensionobjects.abstractextensions;
+
+/**
+ * Interface with their method
+ *
+ * @author saxing 2019/1/6 11:28
+ */
+public interface SergeantExtension extends UnitExtension {
+
+    void sergeantReady();
+
+}
