47788c91c9cc638481a98951e9ced43bb523219a
saxing <saxlh@foxmail.com>
2019-01-06 11:30:24 +0800

    design pattern: extension-objects

---
+++ a0015_java_design_pattern/aa0015_42_extension-objects/src/main/java/org/saxing/extensionobjects/abstractextensions/CommanderExtension.java
@@ -0,0 +1,12 @@
+package org.saxing.extensionobjects.abstractextensions;
+
+/**
+ * Interface with their method
+ *
+ * @author saxing 2019/1/6 11:29
+ */
+public interface CommanderExtension extends UnitExtension {
+
+    void commanderReady();
+
+}
