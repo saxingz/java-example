6e67740914210f6e0e45623da2de7ab535d784f0
saxing <saxlh@foxmail.com>
2019-01-06 11:28:08 +0800

    design pattern: extension-objects

---
+++ a0015_java_design_pattern/aa0015_42_extension-objects/src/main/java/org/saxing/extensionobjects/abstractextensions/SoldierExtension.java
@@ -0,0 +1,12 @@
+package org.saxing.extensionobjects.abstractextensions;
+
+/**
+ * Interface with their method
+ *
+ * @author saxing 2019/1/6 11:27
+ */
+public interface SoldierExtension extends UnitExtension {
+
+    void soldierReady();
+
+}
