d48793c4451f89856b298afc38edb61a854aab31
saxing <saxlh@foxmail.com>
2019-01-06 11:49:46 +0800

    design pattern: extension-objects

---
+++ a0015_java_design_pattern/aa0015_42_extension-objects/src/main/java/org/saxing/extensionobjects/units/SergeantUnit.java
@@ -0,0 +1,13 @@
+package org.saxing.extensionobjects.units;
+
+/**
+ * Class defining SergeantUnit
+ *
+ * @author saxing 2019/1/6 11:42
+ */
+public class SergeantUnit extends Unit {
+
+    public SergeantUnit(String name) {
+        super(name);
+    }
+}
