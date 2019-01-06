7f466a2ecd675962a03e35a97b009e9ac048db7c
saxing <saxlh@foxmail.com>
2019-01-06 11:27:25 +0800

    design pattern: extension-objects

---
+++ a0015_java_design_pattern/aa0015_42_extension-objects/src/main/java/org/saxing/extensionobjects/abstractextensions/UnitExtension.java
@@ -0,0 +1,10 @@
+package org.saxing.extensionobjects.abstractextensions;
+
+/**
+ * Other Extensions will extend this interface
+ *
+ * @author saxing 2019/1/6 11:27
+ */
+public interface UnitExtension {
+
+}
