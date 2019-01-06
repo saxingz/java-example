1d0f58883d15e076e874a835be4584d4b95d75f5
saxing <saxlh@foxmail.com>
2019-01-06 11:25:33 +0800

    design pattern: extension-objects

---
+++ a0015_java_design_pattern/aa0015_42_extension-objects/src/main/java/org/saxing/extensionobjects/Aa001542ExtensionObjectsApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.extensionobjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001542ExtensionObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001542ExtensionObjectsApplication.class, args);
+    }
+
+}
+
