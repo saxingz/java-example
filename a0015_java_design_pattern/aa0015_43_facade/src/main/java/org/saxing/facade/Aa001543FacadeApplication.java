57486eb1607e8fa27bb4b6d6eac009e75b97c304
saxing <saxlh@foxmail.com>
2019-01-07 09:12:12 +0800

    design pattern: facade

--- a0015_java_design_pattern/aa0015_43_facade/src/main/java/org/saxing/facade/Aa001543FacadeApplication.java
+++ a0015_java_design_pattern/aa0015_43_facade/src/main/java/org/saxing/facade/Aa001543FacadeApplication.java
@@ -3,11 +3,18 @@ package org.saxing.facade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/1/7 9:11
+ */
 public class Aa001543FacadeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001543FacadeApplication.class, args);
+        DwarvenGoldmineFacade facade = new DwarvenGoldmineFacade();
+        facade.startNewDay();
+        facade.digOutGold();
+        facade.endDay();
     }
 
 }
