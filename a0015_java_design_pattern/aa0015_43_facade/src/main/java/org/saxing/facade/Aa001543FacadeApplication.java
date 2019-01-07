0c334c9425ef13bf7083e256f127008886c850e5
saxing <saxlh@foxmail.com>
2019-01-07 09:00:30 +0800

    design pattern: facade

---
+++ a0015_java_design_pattern/aa0015_43_facade/src/main/java/org/saxing/facade/Aa001543FacadeApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001543FacadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001543FacadeApplication.class, args);
+    }
+
+}
+
