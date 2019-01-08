21d4bfea367bb2d950165a39257af62f75915143
saxing <saxlh@foxmail.com>
2019-01-08 21:22:20 +0800

    design pattern: factory-kit

---
+++ a0015_java_design_pattern/aa0015_44_factory-kit/src/main/java/org/saxing/factorykit/Spear.java
@@ -0,0 +1,14 @@
+package org.saxing.factorykit;
+
+/**
+ * Class representing Spear
+ *
+ * @author saxing 2019/1/8 21:22
+ */
+public class Spear implements Weapon {
+    @Override
+    public String toString() {
+        return "Spear";
+    }
+}
+
