239a2c84cd23a885712d519f38d64a23d36be67c
saxing <saxlh@foxmail.com>
2019-01-08 21:22:43 +0800

    design pattern: factory-kit

---
+++ a0015_java_design_pattern/aa0015_44_factory-kit/src/main/java/org/saxing/factorykit/Sword.java
@@ -0,0 +1,14 @@
+package org.saxing.factorykit;
+
+/**
+ * Class representing Swords
+ *
+ * @author saxing 2019/1/8 21:22
+ */
+public class Sword implements Weapon {
+    @Override
+    public String toString() {
+        return "Sword";
+    }
+}
+
