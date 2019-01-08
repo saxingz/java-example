3e386fe853e7cd4b8af8539cefb8c7b0b48d7b55
saxing <saxlh@foxmail.com>
2019-01-08 21:24:39 +0800

    design pattern: factory-kit

--- a0015_java_design_pattern/aa0015_44_factory-kit/src/main/java/org/saxing/factorykit/Aa001544FactoryKitApplication.java
+++ a0015_java_design_pattern/aa0015_44_factory-kit/src/main/java/org/saxing/factorykit/Aa001544FactoryKitApplication.java
@@ -1,13 +1,29 @@
 package org.saxing.factorykit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/1/8 21:23
+ */
 public class Aa001544FactoryKitApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001544FactoryKitApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001544FactoryKitApplication.class, args);
+        WeaponFactory weaponFactory = WeaponFactory.factory(builder -> {
+            builder.add(WeaponType.SWORD, Sword::new);
+            builder.add(WeaponType.AXE, Axe::new);
+            builder.add(WeaponType.SPEAR, Spear::new);
+            builder.add(WeaponType.BOW, Bow::new);
+        });
+
+        Weapon axe = weaponFactory.create(WeaponType.AXE);
+        LOGGER.info(axe.toString());
     }
 
 }
