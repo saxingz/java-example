9cd554eaa087944e1dfaa7aeb3ad5717ecf7bae9
saxing <saxlh@foxmail.com>
2019-01-08 21:21:21 +0800

    design pattern: factory-kit

---
+++ a0015_java_design_pattern/aa0015_44_factory-kit/src/main/java/org/saxing/factorykit/Axe.java
@@ -0,0 +1,15 @@
+package org.saxing.factorykit;
+
+/**
+ * Class representing Axe
+ *
+ * @author saxing 2019/1/8 21:21
+ */
+public class Axe implements Weapon {
+
+    @Override
+    public String toString() {
+        return "Axe";
+    }
+
+}
