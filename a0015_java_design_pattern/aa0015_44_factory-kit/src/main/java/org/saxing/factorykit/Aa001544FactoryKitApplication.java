1751db22734b1ab755722b0527b97a458348b759
saxing <saxlh@foxmail.com>
2019-01-08 21:14:55 +0800

    design pattern: factory-kit

---
+++ a0015_java_design_pattern/aa0015_44_factory-kit/src/main/java/org/saxing/factorykit/Aa001544FactoryKitApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.factorykit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001544FactoryKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001544FactoryKitApplication.class, args);
+    }
+
+}
+
---
+++ a0015_java_design_pattern/aa0015_44_factory-kit/src/test/java/org/saxing/factorykit/Aa001544FactoryKitApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.factorykit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001544FactoryKitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
