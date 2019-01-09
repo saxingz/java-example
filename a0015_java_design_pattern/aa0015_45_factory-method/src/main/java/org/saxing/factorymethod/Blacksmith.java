3bfc69bac9bd869765cec04a5ce82fa9819f4c4c
saxing <saxlh@foxmail.com>
2019-01-09 23:30:04 +0800

    design pattern: factory-method

---
+++ a0015_java_design_pattern/aa0015_45_factory-method/src/main/java/org/saxing/factorymethod/Blacksmith.java
@@ -0,0 +1,12 @@
+package org.saxing.factorymethod;
+
+/**
+ * The interface containing method for producing objects.
+ *
+ * @author saxing 2019/1/9 23:29
+ */
+public interface Blacksmith {
+
+
+
+}
