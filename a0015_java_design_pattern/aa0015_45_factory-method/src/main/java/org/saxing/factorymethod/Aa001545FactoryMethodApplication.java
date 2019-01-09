c6242dfcbbcc667997631c900c2f49761ac9d3a0
saxing <saxlh@foxmail.com>
2019-01-09 23:28:07 +0800

    design pattern: factory-method

---
+++ a0015_java_design_pattern/aa0015_45_factory-method/src/main/java/org/saxing/factorymethod/Aa001545FactoryMethodApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.factorymethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001545FactoryMethodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001545FactoryMethodApplication.class, args);
+    }
+
+}
+
---
+++ a0015_java_design_pattern/aa0015_45_factory-method/src/test/java/org/saxing/factorymethod/Aa001545FactoryMethodApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.factorymethod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001545FactoryMethodApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
