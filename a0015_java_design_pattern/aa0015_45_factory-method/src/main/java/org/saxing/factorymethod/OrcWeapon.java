32560ddf5a12ac72537283f677e722f0e577805a
saxing <saxlh@foxmail.com>
2019-01-09 23:34:47 +0800

    design pattern: factory-method

--- a0015_java_design_pattern/aa0015_45_factory-method/src/main/java/org/saxing/factorymethod/OrcWeapon.java
+++ a0015_java_design_pattern/aa0015_45_factory-method/src/main/java/org/saxing/factorymethod/OrcWeapon.java
@@ -1,8 +1,25 @@
 package org.saxing.factorymethod;
 
-
+/**
+ * OrcWeapon.
+ *
+ * @author saxing 2019/1/9 23:34
+ */
 public class OrcWeapon implements Weapon {
 
+    private WeaponType weaponType;
 
+    public OrcWeapon(WeaponType weaponType) {
+        this.weaponType = weaponType;
+    }
 
+    @Override
+    public String toString() {
+        return "Orcish " + weaponType;
+    }
+
+    @Override
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
 }
