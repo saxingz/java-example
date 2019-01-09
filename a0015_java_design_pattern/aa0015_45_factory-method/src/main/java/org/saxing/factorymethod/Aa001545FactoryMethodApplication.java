a1a80d13134b817da6edcfa9638f8b2f3d45d302
saxing <saxlh@foxmail.com>
2019-01-09 23:51:01 +0800

    design pattern: factory-method

--- a0015_java_design_pattern/aa0015_45_factory-method/src/main/java/org/saxing/factorymethod/Aa001545FactoryMethodApplication.java
+++ a0015_java_design_pattern/aa0015_45_factory-method/src/main/java/org/saxing/factorymethod/Aa001545FactoryMethodApplication.java
@@ -1,13 +1,37 @@
 package org.saxing.factorymethod;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/1/9 23:47
+ */
 public class Aa001545FactoryMethodApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001545FactoryMethodApplication.class);
+
+    private final Blacksmith blacksmith;
+
+    public Aa001545FactoryMethodApplication(Blacksmith blacksmith) {
+        this.blacksmith = blacksmith;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001545FactoryMethodApplication.class, args);
+        Aa001545FactoryMethodApplication app = new Aa001545FactoryMethodApplication(new OrcBlacksmith());
+        app.manufactureWeapons();
+
+        app = new Aa001545FactoryMethodApplication(new ElfBlacksmith());
+        app.manufactureWeapons();
+    }
+
+    private void manufactureWeapons(){
+        Weapon weapon;
+        weapon = blacksmith.manufactureWeapon(WeaponType.SPEAR);
+        LOGGER.info(weapon.toString());
+        weapon = blacksmith.manufactureWeapon(WeaponType.AXE);
+        LOGGER.info(weapon.toString());
     }
 
 }
