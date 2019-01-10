68229bd3c09e7f869eecf4a462629fe035896647
saxing <saxlh@foxmail.com>
2019-01-10 09:26:11 +0800

    design pattern: feature-toggle

---
+++ a0015_java_design_pattern/aa0015_46_feature-toggle/src/main/java/org/saxing/featuretoggle/pattern/Service.java
@@ -0,0 +1,18 @@
+package org.saxing.featuretoggle.pattern;
+
+/**
+ * Simple interfaces to allow the calling of the method to generate the welcome message for a given user. While there is
+ * a helper method to gather the the status of the feature toggle. In some cases there is no need for the
+ * {@link Service#isEnhanced()} in {@link TieredFeatureToggleVersion}
+ * where the toggle is determined by the actual {@link User}.
+ *
+ *
+ * @author saxing 2019/1/10 9:25
+ */
+public interface Service {
+
+    String getWelcomeMessage(User user);
+
+    boolean isEnhanced();
+
+}
