b127b14dc7be78a1ea59ffaf75adc73e9ba93a91
saxing <saxlh@foxmail.com>
2019-01-10 09:23:25 +0800

    design pattern: feature-toggle

---
+++ a0015_java_design_pattern/aa0015_46_feature-toggle/src/main/java/org/saxing/featuretoggle/Aa001546FeatureToggleApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.featuretoggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001546FeatureToggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001546FeatureToggleApplication.class, args);
+    }
+
+}
+
