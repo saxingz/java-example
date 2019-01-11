bf82af4c5c46153dc2d51c26add5088a5393eaba
saxing <saxlh@foxmail.com>
2019-01-11 23:46:27 +0800

    design pattern: feature-toggle

---
+++ a0015_java_design_pattern/aa0015_47_fluentinterface/src/test/java/org/saxing/fluentinterface/Aa001547FluentinterfaceApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.fluentinterface;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001547FluentinterfaceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
