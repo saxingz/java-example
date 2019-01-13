592ef1601af8c7d34900349131255d31f24bc4ce
saxing <saxlh@foxmail.com>
2019-01-13 12:06:59 +0800

    design pattern: flux

---
+++ a0015_java_design_pattern/aa0015_48_flux/src/test/java/org/saxing/flux/Aa001548FluxApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.flux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001548FluxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
