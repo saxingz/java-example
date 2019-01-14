054c9c3d4d94f5d468f1a37d3c507749e9066cc2
saxing <saxlh@foxmail.com>
2019-01-14 23:09:16 +0800

    test

--- a0020_java8/src/main/java/org/saxing/java8/optional/ListTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/optional/ListTest.java
@@ -14,6 +14,16 @@ public class ListTest {
         list.add("A5");
         list.add("A6");
 
+        List<String> list2 = new ArrayList<>();
+        list2.add("A3");
+        list2.add("A4");
+        list2.add("A5");
+        list2.add("A6");
+
+        list.retainAll(list2);
+
+        System.out.println(list);
+
         System.out.println(list.size() / 4);
 
         List<String> strings = list.subList(0, 3);
