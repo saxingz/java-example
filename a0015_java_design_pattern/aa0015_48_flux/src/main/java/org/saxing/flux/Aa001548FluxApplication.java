9cc92328a4182ba3c780238dbad43841beda7c9a
saxing <saxlh@foxmail.com>
2019-01-15 23:17:41 +0800

    design pattern: flux

--- a0015_java_design_pattern/aa0015_48_flux/src/main/java/org/saxing/flux/Aa001548FluxApplication.java
+++ a0015_java_design_pattern/aa0015_48_flux/src/main/java/org/saxing/flux/Aa001548FluxApplication.java
@@ -1,13 +1,40 @@
 package org.saxing.flux;
 
+import org.saxing.flux.action.MenuItem;
+import org.saxing.flux.dispatcher.Dispatcher;
+import org.saxing.flux.store.ContentStore;
+import org.saxing.flux.store.MenuStore;
+import org.saxing.flux.view.ContentView;
+import org.saxing.flux.view.MenuView;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/1/15 23:15
+ */
 public class Aa001548FluxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001548FluxApplication.class, args);
+
+        MenuStore menuStore = new MenuStore();
+        Dispatcher.getInstance().registerStore(menuStore);
+        ContentStore contentStore = new ContentStore();
+        Dispatcher.getInstance().registerStore(contentStore);
+        MenuView menuView = new MenuView();
+        menuStore.registView(menuView);
+        ContentView contentView = new ContentView();
+        contentStore.registView(contentView);
+
+        // render initial view
+        menuView.render();
+        contentView.render();
+
+        // user clicks another menu item
+        // this triggers action dispatching and eventually causes views to render with new content
+        menuView.itemClicked(MenuItem.COMPANY);
+
     }
 
 }
