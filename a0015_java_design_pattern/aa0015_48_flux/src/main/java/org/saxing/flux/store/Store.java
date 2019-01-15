1ef1857cc994456afa0e960f6996e3535f927f13
saxing <saxlh@foxmail.com>
2019-01-15 23:05:35 +0800

    design pattern: flux

--- a0015_java_design_pattern/aa0015_48_flux/src/main/java/org/saxing/flux/store/Store.java
+++ a0015_java_design_pattern/aa0015_48_flux/src/main/java/org/saxing/flux/store/Store.java
@@ -1,5 +1,11 @@
 package org.saxing.flux.store;
 
+import org.saxing.flux.action.Action;
+import org.saxing.flux.view.View;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Store is a data model.
  *
@@ -7,6 +13,16 @@ package org.saxing.flux.store;
  */
 public abstract class Store {
 
+    private List<View> views = new LinkedList<>();
 
+    public abstract void onAction(Action action);
+
+    public void registView(View view){
+        views.add(view);
+    }
+
+    protected void notifyChange(){
+        views.stream().forEach(view -> view.storeChanged(this));
+    }
 
 }
