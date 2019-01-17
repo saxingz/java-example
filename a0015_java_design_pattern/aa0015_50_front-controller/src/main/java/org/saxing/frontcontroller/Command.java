779a1d111f9aa4a2aad71aedbb4bb8e301d27f03
saxing <saxlh@foxmail.com>
2019-01-17 22:50:40 +0800

    design pattern: front-controller

---
+++ a0015_java_design_pattern/aa0015_50_front-controller/src/main/java/org/saxing/frontcontroller/Command.java
@@ -0,0 +1,12 @@
+package org.saxing.frontcontroller;
+
+/**
+ * Commands are the intermediary between requests and views.
+ *
+ * @author saxing 2019/1/17 22:50
+ */
+public interface Command {
+
+    void process();
+
+}
