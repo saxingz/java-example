3005834992f3717df66c48eb061593c0bb2218b3
saxing <saxlh@foxmail.com>
2019-01-17 22:51:12 +0800

    design pattern: front-controller

---
+++ a0015_java_design_pattern/aa0015_50_front-controller/src/main/java/org/saxing/frontcontroller/View.java
@@ -0,0 +1,12 @@
+package org.saxing.frontcontroller;
+
+/**
+ * Views are the representations rendered for the user.
+ *
+ * @author saxing 2019/1/17 22:51
+ */
+public interface View {
+
+    void display();
+
+}
