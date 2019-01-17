009c65f36b9f67ebd0f1d1d05e9d8c8b955a10c9
saxing <saxlh@foxmail.com>
2019-01-17 22:48:05 +0800

    design pattern: front-controller

---
+++ a0015_java_design_pattern/aa0015_50_front-controller/src/main/java/org/saxing/frontcontroller/Aa001550FrontControllerApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.frontcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001550FrontControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001550FrontControllerApplication.class, args);
+    }
+
+}
+
