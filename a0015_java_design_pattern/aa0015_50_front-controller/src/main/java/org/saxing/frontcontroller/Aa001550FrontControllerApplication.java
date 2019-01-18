dc96a54af31b2cc302507ce8696da69df16e2c9c
saxing <saxlh@foxmail.com>
2019-01-18 22:55:28 +0800

    design pattern: front-controller

--- a0015_java_design_pattern/aa0015_50_front-controller/src/main/java/org/saxing/frontcontroller/Aa001550FrontControllerApplication.java
+++ a0015_java_design_pattern/aa0015_50_front-controller/src/main/java/org/saxing/frontcontroller/Aa001550FrontControllerApplication.java
@@ -3,6 +3,10 @@ package org.saxing.frontcontroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * main
  *
@@ -11,10 +15,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Aa001550FrontControllerApplication {
 
     public static void main(String[] args) {
-        FrontController controller = new FrontController();
-        controller.handleRequest("Archer");
-        controller.handleRequest("Catapult");
-        controller.handleRequest("foobar");
+//        FrontController controller = new FrontController();
+//        controller.handleRequest("Archer");
+//        controller.handleRequest("Catapult");
+//        controller.handleRequest("foobar");
+
+        Byte a = 1;
+        Byte b = 1;
+        List list = Arrays.asList(a, b);
+        System.out.println(list);
+        List xxx =  list;
+        System.out.println(xxx);
     }
 
 }
