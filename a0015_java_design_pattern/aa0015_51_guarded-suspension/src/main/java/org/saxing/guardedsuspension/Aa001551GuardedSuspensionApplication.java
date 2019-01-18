2893877b800bbf7912cf548c855546e700af28ba
saxing <saxlh@foxmail.com>
2019-01-18 22:59:47 +0800

    design pattern: guarded-suspension

---
+++ a0015_java_design_pattern/aa0015_51_guarded-suspension/src/main/java/org/saxing/guardedsuspension/Aa001551GuardedSuspensionApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.guardedsuspension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001551GuardedSuspensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001551GuardedSuspensionApplication.class, args);
+    }
+
+}
+
---
+++ a0015_java_design_pattern/aa0015_51_guarded-suspension/src/test/java/org/saxing/guardedsuspension/Aa001551GuardedSuspensionApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.guardedsuspension;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001551GuardedSuspensionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
