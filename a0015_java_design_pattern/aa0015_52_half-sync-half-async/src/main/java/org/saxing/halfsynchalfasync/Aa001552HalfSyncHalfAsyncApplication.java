d0face76237b141ffe9754ce70b08873d352cf29
saxing <saxlh@foxmail.com>
2019-01-19 09:42:33 +0800

    design pattern: half-sync-half-async

---
+++ a0015_java_design_pattern/aa0015_52_half-sync-half-async/src/main/java/org/saxing/halfsynchalfasync/Aa001552HalfSyncHalfAsyncApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.halfsynchalfasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001552HalfSyncHalfAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001552HalfSyncHalfAsyncApplication.class, args);
+    }
+
+}
+
