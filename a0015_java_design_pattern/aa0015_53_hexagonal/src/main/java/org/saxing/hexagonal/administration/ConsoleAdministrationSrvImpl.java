e4c2c1a18fca593e0441111a5f8154dc1962efc1
saxing <saxlh@foxmail.com>
2019-01-20 11:01:16 +0800

    design pattern: hexagonal

---
+++ a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/administration/ConsoleAdministrationSrvImpl.java
@@ -0,0 +1,27 @@
+package org.saxing.hexagonal.administration;
+
+import org.slf4j.Logger;
+
+/**
+ * Console implementation for lottery administration
+ *
+ * @author saxing 2019/1/20 11:00
+ */
+public class ConsoleAdministrationSrvImpl implements ConsoleAdministrationSrv {
+
+
+    @Override
+    public void getAllSubmittedTickets() {
+
+    }
+
+    @Override
+    public void performLottery() {
+
+    }
+
+    @Override
+    public void resetLottery() {
+
+    }
+}
