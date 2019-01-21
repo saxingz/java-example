b61ddd14cd1df8b10a9447de5e33d2175923e129
saxing <saxlh@foxmail.com>
2019-01-21 23:50:26 +0800

    design pattern: hexagonal

---
+++ a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/domain/LotteryUtils.java
@@ -0,0 +1,15 @@
+package org.saxing.hexagonal.domain;
+
+/**
+ * Lottery utilities
+ *
+ * @author saxing 2019/1/21 23:48
+ */
+public class LotteryUtils {
+
+    private LotteryUtils() {
+    }
+
+
+
+}
