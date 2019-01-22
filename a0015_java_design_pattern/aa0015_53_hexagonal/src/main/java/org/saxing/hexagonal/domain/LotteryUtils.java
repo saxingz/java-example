9953357aed277d3ebcd2c19f38b9fe3180d1454b
saxing <saxlh@foxmail.com>
2019-01-22 09:34:58 +0800

    design pattern: hexagonal

--- a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/domain/LotteryUtils.java
+++ a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/domain/LotteryUtils.java
@@ -16,7 +16,16 @@ public class LotteryUtils {
 
     public static LotteryTicketCheckResult checkTicketForPrize(LotteryTicketRepository repository, LotteryTicketId id,
                                                                LotteryNumbers winningNumbers){
-        return null;
+        Optional<LotteryTicket> optional = repository.findById(id);
+        if (optional.isPresent()){
+            if (optional.get().getNumbers().equals(winningNumbers)){
+                return new LotteryTicketCheckResult(LotteryTicketCheckResult.CheckResult.WIN_PRIZE, 1000);
+            } else {
+                return new LotteryTicketCheckResult(LotteryTicketCheckResult.CheckResult.NO_PRIZE);
+            }
+        } else {
+            return new LotteryTicketCheckResult(LotteryTicketCheckResult.CheckResult.TICKET_NOT_SUBMITTED);
+        }
     }
 
 }
