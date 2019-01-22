d8f5d8dc414f5068f511af139d3a452c4a643a30
saxing <saxlh@foxmail.com>
2019-01-22 09:28:26 +0800

    search

---
+++ a0023_search/src/main/java/org/saxing/a0023_search/A0023SearchApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.a0023_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0023SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0023SearchApplication.class, args);
+    }
+
+}
+
---
+++ a0023_search/src/test/java/org/saxing/a0023_search/A0023SearchApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.a0023_search;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0023SearchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
