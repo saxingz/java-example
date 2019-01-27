8c633f91996b7483325e3edd609bb66e1d50d840
saxing <saxlh@foxmail.com>
2019-01-27 22:51:20 +0800

    search

---
+++ a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/eventlog/MongoEventLog.java
@@ -0,0 +1,47 @@
+package org.saxing.hexagonal.eventlog;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.saxing.hexagonal.domain.PlayerDetails;
+
+/**
+ * Mongo based event log
+ *
+ * @author saxing 2019/1/27 22:50
+ */
+public class MongoEventLog implements LotteryEventLog  {
+
+    private static final String DEFAULT_DB = "lotteryDB";
+    private static final String DEFAULT_EVENTS_COLLECTION = "events";
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> eventsCollection;
+
+    @Override
+    public void ticketSubmitted(PlayerDetails details) {
+
+    }
+
+    @Override
+    public void ticketSubmitError(PlayerDetails details) {
+
+    }
+
+    @Override
+    public void ticketDidNotWin(PlayerDetails details) {
+
+    }
+
+    @Override
+    public void ticketWon(PlayerDetails details, int prizeAmount) {
+
+    }
+
+    @Override
+    public void prizeError(PlayerDetails details, int prizeAmount) {
+
+    }
+}
