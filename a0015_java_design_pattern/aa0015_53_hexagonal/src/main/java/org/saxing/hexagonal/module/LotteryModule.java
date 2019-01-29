41409b56b1574554882c8fd8a6ad88c99ced39bb
saxing <saxlh@foxmail.com>
2019-01-29 23:05:38 +0800

    design pattern: hexagonal

---
+++ a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/module/LotteryModule.java
@@ -0,0 +1,18 @@
+package org.saxing.hexagonal.module;
+
+import com.google.inject.AbstractModule;
+import org.saxing.hexagonal.database.LotteryTicketRepository;
+
+/**
+ * Guice module for binding production dependencies
+ *
+ * @author saxing 2019/1/29 23:05
+ */
+public class LotteryModule extends AbstractModule {
+
+
+    @Override
+    protected void configure() {
+
+    }
+}
