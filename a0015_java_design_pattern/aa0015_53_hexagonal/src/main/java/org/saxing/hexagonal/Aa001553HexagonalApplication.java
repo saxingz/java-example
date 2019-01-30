735f015b59cb52de06d65dbe4b005ca3d7e7232e
saxing <saxlh@foxmail.com>
2019-01-30 17:03:36 +0800

    design pattern: hexagonal

--- a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/Aa001553HexagonalApplication.java
+++ a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/Aa001553HexagonalApplication.java
@@ -1,13 +1,34 @@
 package org.saxing.hexagonal;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.saxing.hexagonal.domain.LotteryAdministration;
+import org.saxing.hexagonal.domain.LotteryService;
+import org.saxing.hexagonal.module.LotteryTestingModule;
+import org.saxing.hexagonal.sampledata.SampleData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/1/30 17:03
+ */
 public class Aa001553HexagonalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001553HexagonalApplication.class, args);
+        Injector injector = Guice.createInjector(new LotteryTestingModule());
+
+        // start new lottery round
+        LotteryAdministration administration = injector.getInstance(LotteryAdministration.class);
+        administration.resetLottery();
+
+        // submit some lottery tickets
+        LotteryService service = injector.getInstance(LotteryService.class);
+        SampleData.submitTickets(service, 20);
+
+        // perform lottery
+        administration.performLottery();
     }
 
 }
