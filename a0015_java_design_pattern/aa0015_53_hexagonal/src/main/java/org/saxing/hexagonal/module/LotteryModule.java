ea95b749bcfb4cbdea7f31965657d19c7db48072
saxing <saxlh@foxmail.com>
2019-01-30 14:16:48 +0800

    design pattern: hexagonal

--- a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/module/LotteryModule.java
+++ a0015_java_design_pattern/aa0015_53_hexagonal/src/main/java/org/saxing/hexagonal/module/LotteryModule.java
@@ -1,7 +1,12 @@
 package org.saxing.hexagonal.module;
 
 import com.google.inject.AbstractModule;
+import org.saxing.hexagonal.banking.MongoBank;
+import org.saxing.hexagonal.banking.WireTransfers;
 import org.saxing.hexagonal.database.LotteryTicketRepository;
+import org.saxing.hexagonal.database.MongoTicketRepository;
+import org.saxing.hexagonal.eventlog.LotteryEventLog;
+import org.saxing.hexagonal.eventlog.MongoEventLog;
 
 /**
  * Guice module for binding production dependencies
@@ -12,6 +17,8 @@ public class LotteryModule extends AbstractModule {
 
     @Override
     protected void configure() {
-
+        bind(LotteryTicketRepository.class).to(MongoTicketRepository.class);
+        bind(LotteryEventLog.class).to(MongoEventLog.class);
+        bind(WireTransfers.class).to(MongoBank.class);
     }
 }
