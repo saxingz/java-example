099290ec45ec4f98980b49f5e1c7298be6cdf380
saxing <saxlh@foxmail.com>
2019-02-12 15:25:44 +0800

    design pattern: intercepting_filter

--- a0015_java_design_pattern/aa0015_54_intercepting_filter/src/main/java/org/saxing/intercepting_filter/Filter.java
+++ a0015_java_design_pattern/aa0015_54_intercepting_filter/src/main/java/org/saxing/intercepting_filter/Filter.java
@@ -9,5 +9,24 @@ package org.saxing.intercepting_filter;
 public interface Filter {
 
 
+    /**
+     * Execute order processing filter.
+     */
+    String execute(Order order);
+
+    /**
+     * Set next filter in chain after this.
+     */
+    void setNext(Filter filter);
+
+    /**
+     * Get next filter in chain after this.
+     */
+    Filter getNext();
+
+    /**
+     * Get last filter in the chain.
+     */
+    Filter getLast();
 
 }
