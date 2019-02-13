cbcd589dcbeea76ed0101b62ce14512b7c188283
saxing <saxlh@foxmail.com>
2019-02-13 21:29:32 +0800

    design pattern: intercepting

---
+++ a0015_java_design_pattern/aa0015_55_interpreter/src/main/java/org/saxing/interpreter/Aa001555InterpreterApplication.java
@@ -0,0 +1,14 @@
+package org.saxing.interpreter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001555InterpreterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001555InterpreterApplication.class, args);
+    }
+
+}
+
