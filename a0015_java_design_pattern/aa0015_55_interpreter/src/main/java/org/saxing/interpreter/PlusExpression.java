af7b574473384f2cbe8b65e401e52e49987b05fa
saxing <saxlh@foxmail.com>
2019-02-13 21:35:19 +0800

    design pattern: intercepter

---
+++ a0015_java_design_pattern/aa0015_55_interpreter/src/main/java/org/saxing/interpreter/PlusExpression.java
@@ -0,0 +1,27 @@
+package org.saxing.interpreter;
+
+/**
+ * PlusExpression
+ *
+ * @author saxing 2019/2/13 21:34
+ */
+public class PlusExpression extends Expression {
+
+    private Expression leftExpression;
+    private Expression rightExpression;
+
+    public PlusExpression(Expression leftExpression, Expression rightExpression) {
+        this.leftExpression = leftExpression;
+        this.rightExpression = rightExpression;
+    }
+
+    @Override
+    public int interpret() {
+        return leftExpression.interpret() + rightExpression.interpret();
+    }
+
+    @Override
+    public String toString() {
+        return "+";
+    }
+}
