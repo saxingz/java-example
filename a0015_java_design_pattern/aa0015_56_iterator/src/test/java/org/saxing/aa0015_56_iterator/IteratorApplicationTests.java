aa38b13136ee405569dcb6d93bb73e0cf9424d84
saxing <saxlh@foxmail.com>
2019-02-16 23:36:26 +0800

    design pattern: iterator

---
+++ a0015_java_design_pattern/aa0015_56_iterator/src/test/java/org/saxing/aa0015_56_iterator/IteratorApplicationTests.java
@@ -0,0 +1,17 @@
+package org.saxing.aa0015_56_iterator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IteratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
