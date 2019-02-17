832105942467cddb0fe25321b562c2d1e7b4eb0c
saxing <saxlh@foxmail.com>
2019-02-17 20:46:22 +0800

    design pattern: iterator

---
+++ a0015_java_design_pattern/aa0015_56_iterator/src/main/java/org/saxing/aa0015_56_iterator/list/Item.java
@@ -0,0 +1,4 @@
+package org.saxing.aa0015_56_iterator.list;
+
+public class Item {
+}
---
+++ a0015_java_design_pattern/aa0015_56_iterator/src/main/java/org/saxing/aa0015_56_iterator/list/ItemType.java
@@ -0,0 +1,12 @@
+package org.saxing.aa0015_56_iterator.list;
+
+/**
+ * ItemType enumeration
+ *
+ * @author saxing 2019/2/17 17:10
+ */
+public enum ItemType {
+
+    ANY, WEAPON, RING, POTION
+
+}
