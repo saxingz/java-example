1a6d15ec45af289bb58ac75557cdaec9873296a6
saxing <saxlh@foxmail.com>
2019-02-17 20:47:10 +0800

    design pattern: iterator

--- a0015_java_design_pattern/aa0015_56_iterator/src/main/java/org/saxing/aa0015_56_iterator/list/Item.java
+++ a0015_java_design_pattern/aa0015_56_iterator/src/main/java/org/saxing/aa0015_56_iterator/list/Item.java
@@ -1,4 +1,31 @@
 package org.saxing.aa0015_56_iterator.list;
 
+/**
+ * item
+ *
+ * @author saxing 2019/2/17 20:46
+ */
 public class Item {
+
+    private ItemType type;
+    private String name;
+
+    public Item(ItemType type, String name) {
+        this.setType(type);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+
+    public final void setType(ItemType type) {
+        this.type = type;
+    }
+
 }
