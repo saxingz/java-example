e0de3f56d980117d26bb80f2d7f37d376a15f4a8
saxing <saxlh@foxmail.com>
2019-02-26 14:59:02 +0800

    rec xml

---
+++ a0024_rec_xml/src/main/java/org/saxing/rec_xml/A0024RecXmlApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.rec_xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0024RecXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0024RecXmlApplication.class, args);
+    }
+
+}
---
+++ a0024_rec_xml/src/test/java/org/saxing/rec_xml/A0024RecXmlApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.rec_xml;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0024RecXmlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
