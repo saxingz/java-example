193e01671a839997280542aeb2e1b2a1fec652f8
saxing <saxlh@foxmail.com>
2019-02-28 21:50:16 +0800

    design pattern: layers

---
+++ a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/Aa001557LayersApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.layers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001557LayersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001557LayersApplication.class, args);
+    }
+
+}
