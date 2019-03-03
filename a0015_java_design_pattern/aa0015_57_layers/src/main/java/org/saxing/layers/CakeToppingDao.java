630dff910df9d597be94fbdd6be206f4b501e084
saxing <saxlh@foxmail.com>
2019-03-03 22:20:41 +0800

    design pattern: layers

---
+++ a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/CakeToppingDao.java
@@ -0,0 +1,11 @@
+package org.saxing.layers;
+
+
+/**
+ *
+ * cake topping
+ *
+ * @author saxing 2019/3/3 22:20
+ */
+public interface CakeToppingDao {
+}
--- a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/View.java
+++ a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/View.java
@@ -2,6 +2,8 @@ package org.saxing.layers;
 
 /**
  * View interface
+ *
+ * @author saxing 2019/3/3 22:18
  */
 public interface View {
 
