141f2eb874d5ac8b69e4d4dee5060bf7d25e0d99
saxing <saxlh@foxmail.com>
2019-03-10 23:24:48 +0800

    list split

--- a0020_java8/src/main/java/org/saxing/java8/optional/ListTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/optional/ListTest.java
@@ -54,7 +54,7 @@ public class ListTest {
         list.add("1");list.add("2");list.add("3");list.add("4");list.add("5");list.add("6");list.add("7");list.add("8");
         list.add("9");list.add("10");list.add("11");list.add("12");list.add("13");list.add("14");list.add("15");list.add("16");
 
-        int page = 3;
+        int page = 2;
         int pageSize = 8;
 
         // 手动分页
