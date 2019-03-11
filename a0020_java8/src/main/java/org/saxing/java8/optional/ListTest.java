23df2d283017502c864cb02b2482b6bb3cd46e43
saxing <saxlh@foxmail.com>
2019-03-11 23:58:45 +0800

    list split

--- a0020_java8/src/main/java/org/saxing/java8/optional/ListTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/optional/ListTest.java
@@ -51,8 +51,10 @@ public class ListTest {
 
     public static void listTest2(){
         List<String> list = new ArrayList<>();
-        list.add("1");list.add("2");list.add("3");list.add("4");list.add("5");list.add("6");list.add("7");list.add("8");
-        list.add("9");list.add("10");list.add("11");list.add("12");list.add("13");list.add("14");list.add("15");list.add("16");
+        list.add("1");list.add("2");list.add("3");list.add("4");
+        list.add("5");list.add("6");list.add("7");list.add("8");
+        list.add("9");list.add("10");list.add("11");list.add("12");
+        list.add("13");list.add("14");list.add("15");list.add("16");
 
         int page = 2;
         int pageSize = 8;
