4605ea768352834df5373a8a1526ef558769056e
saxing <saxlh@foxmail.com>
2019-03-15 23:19:23 +0800

    design pattern: layers

---
+++ a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/CakeToppingInfo.java
@@ -0,0 +1,39 @@
+package org.saxing.layers;
+
+import java.util.Optional;
+
+/**
+ * DTO for cake toppings
+ *
+ * @author saxing 2019/3/15 23:18
+ */
+public class CakeToppingInfo {
+
+    public final Optional<Long> id;
+    public final String name;
+    public final int calories;
+
+    /**
+     * Constructor
+     */
+    public CakeToppingInfo(Long id, String name, int calories) {
+        this.id = Optional.of(id);
+        this.name = name;
+        this.calories = calories;
+    }
+
+    /**
+     * Constructor
+     */
+    public CakeToppingInfo(String name, int calories) {
+        this.id = Optional.empty();
+        this.name = name;
+        this.calories = calories;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("CakeToppingInfo id=%d name=%s calories=%d", id.orElse(-1L), name, calories);
+    }
+
+}
