59de372ae22481ee90c7c37300840347425c9a89
saxing <saxlh@foxmail.com>
2019-03-17 13:05:40 +0800

    wx

--- a0024_rec_xml/src/main/java/org/saxing/rec_xml/TestXmlController.java
+++ a0024_rec_xml/src/main/java/org/saxing/rec_xml/TestXmlController.java
@@ -1,5 +1,6 @@
 package org.saxing.rec_xml;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class TestXmlController {
     @PostMapping(value = "/tx", consumes = "application/xml")
     public String test(@RequestBody TestBean testBean){
         return testBean.getFromUserName() + " " + testBean.getToUserName();
+    }
+
+    @GetMapping(value = "/test2")
+    public String test2(){
+        return "hello test2";
     }
 
 }
