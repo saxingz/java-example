f3dfea1c563c13797ca29eb2be1d33b9000400fb
saxing <saxlh@foxmail.com>
2019-03-26 23:52:25 +0800

    测试类

--- a0024_rec_xml/src/main/java/org/saxing/rec_xml/TestXmlController.java
+++ a0024_rec_xml/src/main/java/org/saxing/rec_xml/TestXmlController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * testxml
  *
@@ -19,9 +22,19 @@ public class TestXmlController {
         return testBean.getFromUserName() + " " + testBean.getToUserName();
     }
 
+    @GetMapping(value = "/**")
+    public String testAll(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+        return "*";
+    }
+
     @GetMapping(value = "/test2")
     public String test2(){
         return "hello test2";
+    }
+
+    @GetMapping(value = "/t*")
+    public String t(){
+        return "t*";
     }
 
 }
