8df3c0687868f7a614078b1193fbc7a9e705ab18
saxing <saxlh@foxmail.com>
2019-04-02 08:23:34 +0800

    修改支行

---
+++ a0020_java8/src/main/java/org/saxing/java8/CreateTest.java
@@ -0,0 +1,25 @@
+package org.saxing.java8;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CreateTest {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100000; i++) {
+            String random = UUID.randomUUID().toString();
+            new Thread(){
+                @Override
+                public void run() {
+                    InitTest initTest = InitTest.createInitTest(random);
+                    if (!Objects.equals(initTest.getName(), random)){
+                        System.out.println("error. name = " + initTest.getName() + ", random = " + random);
+                    }
+                }
+            }.start();
+        }
+
+        System.out.println("end ...");
+    }
+
+}
