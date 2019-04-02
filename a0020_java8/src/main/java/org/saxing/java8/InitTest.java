13a536c2557a581b4fb6a39ea6c4b3682662dc7c
saxing <saxlh@foxmail.com>
2019-04-02 08:23:20 +0800

    类创建测试

---
+++ a0020_java8/src/main/java/org/saxing/java8/InitTest.java
@@ -0,0 +1,19 @@
+package org.saxing.java8;
+
+public final class InitTest {
+
+    public static InitTest createInitTest(String param){
+        return new InitTest(param);
+    }
+
+    final String name;
+
+    InitTest(String param){
+        this.name = param;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
