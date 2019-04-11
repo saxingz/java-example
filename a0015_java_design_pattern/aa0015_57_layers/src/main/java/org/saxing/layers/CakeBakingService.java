d9c218ef47dcf9a9fbcbf18897a661c4c56306ab
saxing <saxlh@foxmail.com>
2019-04-11 23:07:18 +0800

    design pattern: layers

--- a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/CakeBakingService.java
+++ a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/CakeBakingService.java
@@ -1,10 +1,43 @@
 package org.saxing.layers;
 
+import java.util.List;
+
 /**
  * cake baking service
  *
  * @author saxing 2019/4/10 23:48
  */
 public interface CakeBakingService {
+
+
+    /**
+     * Bakes new cake according to parameters
+     */
+    void bakeNewCake(CakeInfo cakeInfo) throws CakeBakingException;
+
+    /**
+     * Get all cakes
+     */
+    List<CakeInfo> getAllCakes();
+
+    /**
+     * Store new cake topping
+     */
+    void saveNewTopping(CakeToppingInfo toppingInfo);
+
+    /**
+     * Get available cake toppings
+     */
+    List<CakeToppingInfo> getAvailableToppings();
+
+    /**
+     * Add new cake layer
+     */
+    void saveNewLayer(CakeLayerInfo layerInfo);
+
+    /**
+     * Get available cake layers
+     */
+    List<CakeLayerInfo> getAvailableLayers();
 
 }
