bb9a45f31a8a85195bf2969db0a18031447ae522
saxing <saxlh@foxmail.com>
2019-04-12 22:15:27 +0800

    design pattern: layers

--- a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/Aa001557LayersApplication.java
+++ a0015_java_design_pattern/aa0015_57_layers/src/main/java/org/saxing/layers/Aa001557LayersApplication.java
@@ -1,13 +1,58 @@
 package org.saxing.layers;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.Arrays;
+
+/**
+ * main
+ *
+ * @author saxing 2019/4/12 21:42
+ */
 public class Aa001557LayersApplication {
 
+    private static CakeBakingService cakeBakingService = new CakeBakingServiceImpl();
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001557LayersApplication.class, args);
+        // initialize example data
+        initializeData(cakeBakingService);
+
+        // create view and render it
+        CakeViewImpl cakeView = new CakeViewImpl(cakeBakingService);
+        cakeView.render();
+    }
+
+    /**
+     * Initializes the example data
+     */
+    private static void initializeData(CakeBakingService cakeBakingService) {
+        cakeBakingService.saveNewLayer(new CakeLayerInfo("chocolate", 1200));
+        cakeBakingService.saveNewLayer(new CakeLayerInfo("banana", 900));
+        cakeBakingService.saveNewLayer(new CakeLayerInfo("strawberry", 950));
+        cakeBakingService.saveNewLayer(new CakeLayerInfo("lemon", 950));
+        cakeBakingService.saveNewLayer(new CakeLayerInfo("vanilla", 950));
+        cakeBakingService.saveNewLayer(new CakeLayerInfo("strawberry", 950));
+
+        cakeBakingService.saveNewTopping(new CakeToppingInfo("candies", 350));
+        cakeBakingService.saveNewTopping(new CakeToppingInfo("cherry", 350));
+
+        CakeInfo cake1 =
+                new CakeInfo(
+                        new CakeToppingInfo("candies", 0), Arrays.asList(new CakeLayerInfo("chocolate", 0),
+                        new CakeLayerInfo("banana", 0), new CakeLayerInfo("strawberry", 0)));
+        try {
+            cakeBakingService.bakeNewCake(cake1);
+        } catch (CakeBakingException e) {
+            e.printStackTrace();
+        }
+        CakeInfo cake2 =
+                new CakeInfo(new CakeToppingInfo("cherry", 0), Arrays.asList(
+                        new CakeLayerInfo("vanilla", 0), new CakeLayerInfo("lemon", 0), new CakeLayerInfo(
+                                "strawberry", 0)));
+        try {
+            cakeBakingService.bakeNewCake(cake2);
+        } catch (CakeBakingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
