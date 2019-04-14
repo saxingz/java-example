da09e11daa16822fa3ff0e54f54e15d517a46fd8
saxing <saxlh@foxmail.com>
2019-04-14 13:55:08 +0800

    design pattern: lazy-loading

---
+++ a0015_java_design_pattern/aa0015_58_lazy-loading/src/main/java/org/saxing/lazyloading/Aa001558LazyLoadingApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.lazyloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001558LazyLoadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001558LazyLoadingApplication.class, args);
+    }
+
+}
---
+++ a0015_java_design_pattern/aa0015_58_lazy-loading/src/test/java/org/saxing/lazyloading/Aa001558LazyLoadingApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.lazyloading;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001558LazyLoadingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
