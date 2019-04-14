a4f67cce1d91cafee4ad5d4db9cca326ba72b4e1
saxing <saxlh@foxmail.com>
2019-04-14 13:56:50 +0800

    design pattern: lazy-loading

--- a0015_java_design_pattern/aa0015_58_lazy-loading/src/main/java/org/saxing/lazyloading/Aa001558LazyLoadingApplication.java
+++ a0015_java_design_pattern/aa0015_58_lazy-loading/src/main/java/org/saxing/lazyloading/Aa001558LazyLoadingApplication.java
@@ -3,6 +3,11 @@ package org.saxing.lazyloading;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * lazy loading
+ *
+ * @author saxing 2019/4/14 13:55
+ */
 @SpringBootApplication
 public class Aa001558LazyLoadingApplication {
 
