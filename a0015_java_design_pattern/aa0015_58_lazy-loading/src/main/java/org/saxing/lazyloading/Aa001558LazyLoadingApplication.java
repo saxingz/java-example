c95d63c1171195207281ba07bc66dac55dda107e
saxing <saxlh@foxmail.com>
2019-04-14 21:35:09 +0800

    design pattern: lazy-loading

--- a0015_java_design_pattern/aa0015_58_lazy-loading/src/main/java/org/saxing/lazyloading/Aa001558LazyLoadingApplication.java
+++ a0015_java_design_pattern/aa0015_58_lazy-loading/src/main/java/org/saxing/lazyloading/Aa001558LazyLoadingApplication.java
@@ -1,5 +1,7 @@
 package org.saxing.lazyloading;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +10,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author saxing 2019/4/14 13:55
  */
-@SpringBootApplication
 public class Aa001558LazyLoadingApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001558LazyLoadingApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001558LazyLoadingApplication.class, args);
+        // Simple lazy loader - not thread safe
+        HolderNaive holderNaive = new HolderNaive();
+        Heavy heavy = holderNaive.getHeavy();
+        LOGGER.info("heavy={}", heavy);
+
+        // Thread safe lazy loader, but with heavy synchronization on each access
+        HolderThreadSafe holderThreadSafe = new HolderThreadSafe();
+        Heavy another = holderThreadSafe.getHeavy();
+        LOGGER.info("another={}", another);
+
+        // The most efficient lazy loader utilizing Java 8 features
+        Java8Holder java8Holder = new Java8Holder();
+        Heavy next = java8Holder.getHeavy();
+        LOGGER.info("next={}", next);
     }
 
 }
