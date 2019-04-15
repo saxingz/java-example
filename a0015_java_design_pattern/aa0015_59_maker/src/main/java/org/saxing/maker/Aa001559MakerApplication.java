4126f55a558496d9b8994065d89b95efb678d1b0
saxing <saxlh@foxmail.com>
2019-04-15 20:16:44 +0800

    design pattern: maker

---
+++ a0015_java_design_pattern/aa0015_59_maker/src/main/java/org/saxing/maker/Aa001559MakerApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001559MakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001559MakerApplication.class, args);
+    }
+
+}
