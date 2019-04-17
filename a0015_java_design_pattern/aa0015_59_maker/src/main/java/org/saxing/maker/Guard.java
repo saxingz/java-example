1bf9ef10337f2cffae56793fda732a74a5282ae4
saxing <saxlh@foxmail.com>
2019-04-17 20:58:58 +0800

    design pattern: maker

---
+++ a0015_java_design_pattern/aa0015_59_maker/src/main/java/org/saxing/maker/Guard.java
@@ -0,0 +1,20 @@
+package org.saxing.maker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class defining Guard
+ *
+ * @author saxing 2019/4/17 20:58
+ */
+public class Guard implements Permission  {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Guard.class);
+
+    protected static void enter() {
+
+        LOGGER.info("You can enter");
+    }
+
+}
