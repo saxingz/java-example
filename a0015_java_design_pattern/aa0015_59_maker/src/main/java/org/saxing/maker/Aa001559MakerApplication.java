62b2346759b92500d47ae9f7c0367963b1405fcf
saxing <saxlh@foxmail.com>
2019-04-17 20:59:58 +0800

    design pattern: maker

--- a0015_java_design_pattern/aa0015_59_maker/src/main/java/org/saxing/maker/Aa001559MakerApplication.java
+++ a0015_java_design_pattern/aa0015_59_maker/src/main/java/org/saxing/maker/Aa001559MakerApplication.java
@@ -1,13 +1,33 @@
 package org.saxing.maker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/4/17 20:59
+ */
 public class Aa001559MakerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001559MakerApplication.class, args);
+        final Logger logger = LoggerFactory.getLogger(Aa001559MakerApplication.class);
+        Guard guard = new Guard();
+        Thief thief = new Thief();
+
+        if (guard instanceof Permission) {
+            guard.enter();
+        } else {
+            logger.info("You have no permission to enter, please leave this area");
+        }
+
+        if (thief instanceof Permission) {
+            thief.steal();
+        } else {
+            thief.doNothing();
+        }
     }
 
 }
