b80b94158683c7fe109e155a7806884bb5557944
saxing <saxlh@foxmail.com>
2019-04-18 13:11:04 +0800

    design pattern: mediator

---
+++ a0015_java_design_pattern/aa0015_60_mediator/src/main/java/org/saxing/mediator/Hunter.java
@@ -0,0 +1,15 @@
+package org.saxing.mediator;
+
+/**
+ * Hunter
+ *
+ * @author saxing 2019/4/18 13:10
+ */
+public class Hunter extends PartyMemberBase {
+
+    @Override
+    public String toString() {
+        return "Hunter";
+    }
+
+}
