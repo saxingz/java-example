c88a5020a8cb27d55f6f9928294682fa102ca8f1
saxing <saxlh@foxmail.com>
2019-04-18 13:05:04 +0800

    design pattern: mediator

---
+++ a0015_java_design_pattern/aa0015_60_mediator/src/main/java/org/saxing/mediator/Aa001560MediatorApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.mediator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001560MediatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001560MediatorApplication.class, args);
+    }
+
+}
