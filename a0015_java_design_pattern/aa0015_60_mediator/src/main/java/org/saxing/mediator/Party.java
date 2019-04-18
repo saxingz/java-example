113a190673ba44df3af694ef52f058224a03e019
saxing <saxlh@foxmail.com>
2019-04-18 13:07:07 +0800

    design pattern: mediator

---
+++ a0015_java_design_pattern/aa0015_60_mediator/src/main/java/org/saxing/mediator/Party.java
@@ -0,0 +1,9 @@
+package org.saxing.mediator;
+
+/**
+ * party
+ *
+ * @author saxing 2019/4/18 13:07
+ */
+public interface Party {
+}
---
+++ a0015_java_design_pattern/aa0015_60_mediator/src/main/java/org/saxing/mediator/PartyMember.java
@@ -0,0 +1,12 @@
+package org.saxing.mediator;
+
+/**
+ * party member
+ *
+ * @author saxing 2019/4/18 13:06
+ */
+public interface PartyMember {
+
+
+
+}
