9bc98a597f2e63c59533af835370e945f4e06080
saxing <saxlh@foxmail.com>
2019-04-18 13:14:28 +0800

    design pattern: mediator

---
+++ a0015_java_design_pattern/aa0015_60_mediator/src/main/java/org/saxing/mediator/Wizard.java
@@ -0,0 +1,15 @@
+package org.saxing.mediator;
+
+/**
+ * Wizard party member.
+ *
+ * @author saxing 2019/4/18 13:14
+ */
+public class Wizard extends PartyMemberBase {
+
+    @Override
+    public String toString() {
+        return "Wizard";
+    }
+
+}
