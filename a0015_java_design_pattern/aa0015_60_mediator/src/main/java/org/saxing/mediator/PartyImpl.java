6641aede3bafe1488132f1ee91a1cc969f939337
saxing <saxlh@foxmail.com>
2019-04-18 13:13:13 +0800

    design pattern: mediator

---
+++ a0015_java_design_pattern/aa0015_60_mediator/src/main/java/org/saxing/mediator/PartyImpl.java
@@ -0,0 +1,33 @@
+package org.saxing.mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * party implementation
+ *
+ * @author saxing 2019/4/18 13:11
+ */
+public class PartyImpl implements Party {
+
+    private final List<PartyMember> members;
+
+    public PartyImpl(List<PartyMember> members) {
+        this.members = new ArrayList<>();
+    }
+
+    @Override
+    public void addMember(PartyMember member) {
+        members.add(member);
+        member.joinedParty(this);
+    }
+
+    @Override
+    public void act(PartyMember actor, Action action) {
+        for (PartyMember member : members) {
+            if (!member.equals(actor)) {
+                member.partyAction(action);
+            }
+        }
+    }
+}
