e76c0ebb556e24a2fec94cf2ca16659d9d7a6bb6
saxing <saxlh@foxmail.com>
2019-04-18 13:13:57 +0800

    design pattern: mediator

---
+++ a0015_java_design_pattern/aa0015_60_mediator/src/main/java/org/saxing/mediator/Rogue.java
@@ -0,0 +1,15 @@
+package org.saxing.mediator;
+
+/**
+ * rogue
+ *
+ * @author saxing 2019/4/18 13:13
+ */
+public class Rogue extends PartyMemberBase {
+
+    @Override
+    public String toString() {
+        return "Rogue";
+    }
+
+}
