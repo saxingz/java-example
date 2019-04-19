7c932d2c46166452156e537a073bfe28dcab5020
saxing <saxlh@foxmail.com>
2019-04-19 16:50:52 +0800

    design pattern: memento

--- a0015_java_design_pattern/aa0015_61_memento/src/main/java/org/saxing/memento/Aa001561MementoApplication.java
+++ a0015_java_design_pattern/aa0015_61_memento/src/main/java/org/saxing/memento/Aa001561MementoApplication.java
@@ -1,11 +1,43 @@
 package org.saxing.memento;
 
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Stack;
 
+/**
+ * main
+ *
+ * @author saxing 2019/4/19 16:50
+ */
 public class Aa001561MementoApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001561MementoApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001561MementoApplication.class, args);
+
+
+
+        Stack<StarMemento> states = new Stack<>();
+
+        Star star = new Star(StarType.SUN, 10000000, 500000);
+        LOGGER.info(star.toString());
+        states.add(star.getMemento());
+        star.timePasses();
+        LOGGER.info(star.toString());
+        states.add(star.getMemento());
+        star.timePasses();
+        LOGGER.info(star.toString());
+        states.add(star.getMemento());
+        star.timePasses();
+        LOGGER.info(star.toString());
+        states.add(star.getMemento());
+        star.timePasses();
+        LOGGER.info(star.toString());
+        while (states.size() > 0) {
+            star.setMemento(states.pop());
+            LOGGER.info(star.toString());
+        }
+
     }
 
 }
