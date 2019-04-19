2171a51f88e9900346119f63bf8ab86d5001e2d8
saxing <saxlh@foxmail.com>
2019-04-19 16:35:46 +0800

    design pattern: memento

---
+++ a0015_java_design_pattern/aa0015_61_memento/src/main/java/org/saxing/memento/Aa001561MementoApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.memento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001561MementoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001561MementoApplication.class, args);
+    }
+
+}
