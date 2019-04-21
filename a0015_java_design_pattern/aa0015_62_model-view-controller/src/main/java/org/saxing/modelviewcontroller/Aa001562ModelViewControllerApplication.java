043e737b0d56a02ca1f4e623bf035871ee36b410
saxing <saxlh@foxmail.com>
2019-04-21 14:23:03 +0800

    design pattern: model-view-controller

--- a0015_java_design_pattern/aa0015_62_model-view-controller/src/main/java/org/saxing/modelviewcontroller/Aa001562ModelViewControllerApplication.java
+++ a0015_java_design_pattern/aa0015_62_model-view-controller/src/main/java/org/saxing/modelviewcontroller/Aa001562ModelViewControllerApplication.java
@@ -1,13 +1,26 @@
 package org.saxing.modelviewcontroller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/4/21 14:22
+ */
 public class Aa001562ModelViewControllerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001562ModelViewControllerApplication.class, args);
+        // create model, view and controller
+        GiantModel giant = new GiantModel(Health.HEALTHY, Fatigue.ALERT, Nourishment.SATURATED);
+        GiantView view = new GiantView();
+        GiantController controller = new GiantController(giant, view);
+        // initial display
+        controller.updateView();
+        // controller receives some interactions that affect the giant
+        controller.setHealth(Health.WOUNDED);
+        controller.setNourishment(Nourishment.HUNGRY);
+        controller.setFatigue(Fatigue.TIRED);
+        // redisplay
+        controller.updateView();
     }
 
 }
