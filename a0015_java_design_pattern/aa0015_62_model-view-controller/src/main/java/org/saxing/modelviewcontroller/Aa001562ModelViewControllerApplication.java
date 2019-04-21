0c1cefcc7b5423054d9a914bdad39235bd016a2b
saxing <saxlh@foxmail.com>
2019-04-21 14:17:47 +0800

    design pattern: model-view-controller

---
+++ a0015_java_design_pattern/aa0015_62_model-view-controller/src/main/java/org/saxing/modelviewcontroller/Aa001562ModelViewControllerApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.modelviewcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001562ModelViewControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001562ModelViewControllerApplication.class, args);
+    }
+
+}
