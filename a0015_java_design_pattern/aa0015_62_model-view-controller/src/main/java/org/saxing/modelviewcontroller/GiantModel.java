ad00da28d3bff2491279c25914ba2e85d6f3958f
saxing <saxlh@foxmail.com>
2019-04-21 14:20:19 +0800

    design pattern: model-view-controller

---
+++ a0015_java_design_pattern/aa0015_62_model-view-controller/src/main/java/org/saxing/modelviewcontroller/GiantModel.java
@@ -0,0 +1,49 @@
+package org.saxing.modelviewcontroller;
+
+/**
+ * GiantModel contains the giant data
+ *
+ * @author saxing 2019/4/21 14:20
+ */
+public class GiantModel {
+
+    private Health health;
+    private Fatigue fatigue;
+    private Nourishment nourishment;
+
+    GiantModel(Health health, Fatigue fatigue, Nourishment nourishment) {
+        this.health = health;
+        this.fatigue = fatigue;
+        this.nourishment = nourishment;
+    }
+
+    public Health getHealth() {
+        return health;
+    }
+
+    public void setHealth(Health health) {
+        this.health = health;
+    }
+
+    public Fatigue getFatigue() {
+        return fatigue;
+    }
+
+    public void setFatigue(Fatigue fatigue) {
+        this.fatigue = fatigue;
+    }
+
+    public Nourishment getNourishment() {
+        return nourishment;
+    }
+
+    public void setNourishment(Nourishment nourishment) {
+        this.nourishment = nourishment;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The giant looks %s, %s and %s.", health, fatigue, nourishment);
+    }
+
+}
