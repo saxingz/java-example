d33a2a7169b4d920150521fb601a7277a31a26c1
saxing <saxlh@foxmail.com>
2019-04-22 10:01:39 +0800

    design pattern: model-view-presenter

---
+++ a0015_java_design_pattern/aa0015_63_model-view-presenter/src/main/java/org/saxing/modelviewpresenter/Aa001563ModelViewPresenterApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.modelviewpresenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001563ModelViewPresenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001563ModelViewPresenterApplication.class, args);
+    }
+
+}
