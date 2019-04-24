1702fca801d9629f2cfb44e46cb3bee96a48dfae
saxing <saxlh@foxmail.com>
2019-04-24 16:06:48 +0800

    design pattern: module

--- a0015_java_design_pattern/aa0015_64_module/src/main/java/org/saxing/module/Aa001564ModuleApplication.java
+++ a0015_java_design_pattern/aa0015_64_module/src/main/java/org/saxing/module/Aa001564ModuleApplication.java
@@ -1,13 +1,59 @@
 package org.saxing.module;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.FileNotFoundException;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/4/24 16:04
+ */
 public class Aa001564ModuleApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa001564ModuleApplication.class, args);
+    public static FileLoggerModule fileLoggerModule;
+    public static ConsoleLoggerModule consoleLoggerModule;
+
+    public static void prepare() throws FileNotFoundException {
+        fileLoggerModule = FileLoggerModule.getSingleton().prepare();
+        consoleLoggerModule = ConsoleLoggerModule.getSingleton().prepare();
+    }
+
+    /**
+     * Following method performs the finalization
+     */
+    public static void unprepare() {
+
+        /* Close all resources */
+        fileLoggerModule.unprepare();
+        consoleLoggerModule.unprepare();
+    }
+
+    /**
+     * Following method is main executor
+     *
+     * @param args for providing default program arguments
+     */
+    public static void execute(final String... args) {
+
+        /* Send logs on file system */
+        fileLoggerModule.printString("Message");
+        fileLoggerModule.printErrorString("Error");
+
+        /* Send logs on console */
+        consoleLoggerModule.printString("Message");
+        consoleLoggerModule.printErrorString("Error");
+    }
+
+    /**
+     * Program entry point.
+     *
+     * @param args command line args.
+     * @throws FileNotFoundException if program is not able to find log files (output.txt and
+     *         error.txt)
+     */
+    public static void main(final String... args) throws FileNotFoundException {
+        prepare();
+        execute(args);
+        unprepare();
     }
 
 }
