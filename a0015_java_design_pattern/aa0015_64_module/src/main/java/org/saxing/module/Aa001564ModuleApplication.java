cbb4c011080eaee0c3676b6a4c21cbfe21dcf72d
saxing <saxlh@foxmail.com>
2019-04-24 14:28:23 +0800

    design pattern: module

---
+++ a0015_java_design_pattern/aa0015_64_module/src/main/java/org/saxing/module/Aa001564ModuleApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001564ModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001564ModuleApplication.class, args);
+    }
+
+}
