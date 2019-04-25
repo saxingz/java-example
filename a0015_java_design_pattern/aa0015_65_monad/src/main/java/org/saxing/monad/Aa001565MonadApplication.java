cb817f8237a866ce68e69b62355830190749bb97
saxing <saxlh@foxmail.com>
2019-04-25 16:53:09 +0800

    design pattern: monad

--- a0015_java_design_pattern/aa0015_65_monad/src/main/java/org/saxing/monad/Aa001565MonadApplication.java
+++ a0015_java_design_pattern/aa0015_65_monad/src/main/java/org/saxing/monad/Aa001565MonadApplication.java
@@ -3,7 +3,6 @@ package org.saxing.monad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Aa001565MonadApplication {
 
     public static void main(String[] args) {
---
+++ a0015_java_design_pattern/aa0015_65_monad/src/main/java/org/saxing/monad/Sex.java
@@ -0,0 +1,12 @@
+package org.saxing.monad;
+
+/**
+ * sex
+ *
+ * @author saxing 2019/4/25 16:53
+ */
+public enum Sex {
+
+    MALE, FEMALE
+
+}
