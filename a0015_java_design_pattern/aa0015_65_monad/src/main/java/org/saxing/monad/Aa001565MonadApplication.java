0d4c7cf3721db55f422e1772803120cdeb9370bf
saxing <saxlh@foxmail.com>
2019-04-25 17:07:24 +0800

    design pattern: monad

--- a0015_java_design_pattern/aa0015_65_monad/src/main/java/org/saxing/monad/Aa001565MonadApplication.java
+++ a0015_java_design_pattern/aa0015_65_monad/src/main/java/org/saxing/monad/Aa001565MonadApplication.java
@@ -1,12 +1,21 @@
 package org.saxing.monad;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 public class Aa001565MonadApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001565MonadApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001565MonadApplication.class, args);
+        User user = new User("user", 24, Sex.FEMALE, "foobar.com");
+        LOGGER.info(Validator.of(user).validate(User::getName, Objects::nonNull, "name is null")
+                .validate(User::getName, name -> !name.isEmpty(), "name is empty")
+                .validate(User::getEmail, email -> !email.contains("@"), "email doesn't containt '@'")
+                .validate(User::getAge, age -> age > 20 && age < 30, "age isn't between...").get().toString());
     }
 
 }
