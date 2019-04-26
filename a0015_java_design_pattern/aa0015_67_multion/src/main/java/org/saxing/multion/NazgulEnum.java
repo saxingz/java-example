7c91fe1a31075a6714b31dd0b27bc0de958f88f4
saxing <saxlh@foxmail.com>
2019-04-26 16:05:19 +0800

    design pattern: multion

---
+++ a0015_java_design_pattern/aa0015_67_multion/src/main/java/org/saxing/multion/NazgulEnum.java
@@ -0,0 +1,12 @@
+package org.saxing.multion;
+
+/**
+ * NazgulEnum
+ *
+ * @author saxing 2019/4/26 16:05
+ */
+public enum NazgulEnum {
+
+    KHAMUL, MURAZOR, DWAR, JI_INDUR, AKHORAHIL, HOARMURATH, ADUNAPHEL, REN, UVATHA;
+
+}
