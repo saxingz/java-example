5fce83b1569e8da594320ca8ce01cd623c81288c
saxing <saxlh@foxmail.com>
2019-04-26 15:57:01 +0800

    design pattern: multion

---
+++ a0015_java_design_pattern/aa0015_67_multion/src/main/java/org/saxing/multion/Aa001567MultionApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.multion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001567MultionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001567MultionApplication.class, args);
+    }
+
+}
---
+++ a0015_java_design_pattern/aa0015_67_multion/src/test/java/org/saxing/multion/Aa001567MultionApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.multion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001567MultionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
