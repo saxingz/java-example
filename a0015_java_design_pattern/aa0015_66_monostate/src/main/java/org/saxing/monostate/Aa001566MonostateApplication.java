a45abf86b8c402072a0598fdcf5053350cb3c040
saxing <saxlh@foxmail.com>
2019-04-26 12:43:59 +0800

    design pattern: monostate

---
+++ a0015_java_design_pattern/aa0015_66_monostate/src/main/java/org/saxing/monostate/Aa001566MonostateApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.monostate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001566MonostateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001566MonostateApplication.class, args);
+    }
+
+}
---
+++ a0015_java_design_pattern/aa0015_66_monostate/src/test/java/org/saxing/monostate/Aa001566MonostateApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.monostate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001566MonostateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
