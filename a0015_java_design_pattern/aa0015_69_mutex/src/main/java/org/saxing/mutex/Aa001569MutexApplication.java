4a2543d3c3c1ad6cff358969b62afd42a2e18c8c
saxing <saxlh@foxmail.com>
2019-04-30 14:37:47 +0800

    design pattern: mutex

--- a0015_java_design_pattern/aa0015_69_mutex/src/main/java/org/saxing/mutex/Aa001569MutexApplication.java
+++ a0015_java_design_pattern/aa0015_69_mutex/src/main/java/org/saxing/mutex/Aa001569MutexApplication.java
@@ -3,11 +3,20 @@ package org.saxing.mutex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/4/30 14:36
+ */
 public class Aa001569MutexApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001569MutexApplication.class, args);
+        Mutex mutex = new Mutex();
+        Jar jar = new Jar(1000, mutex);
+        Thief peter = new Thief("Peter", jar);
+        Thief john = new Thief("John", jar);
+        peter.start();
+        john.start();
     }
 
 }
