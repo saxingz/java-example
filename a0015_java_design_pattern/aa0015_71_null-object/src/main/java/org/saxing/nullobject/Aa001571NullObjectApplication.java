bf03e0496b226fa5788e881d5817ea79f4d7683d
saxing <saxlh@foxmail.com>
2019-05-05 15:13:07 +0800

    design pattern: null-object

--- a0015_java_design_pattern/aa0015_71_null-object/src/main/java/org/saxing/nullobject/Aa001571NullObjectApplication.java
+++ a0015_java_design_pattern/aa0015_71_null-object/src/main/java/org/saxing/nullobject/Aa001571NullObjectApplication.java
@@ -3,11 +3,21 @@ package org.saxing.nullobject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/5/5 15:13
+ */
 public class Aa001571NullObjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001571NullObjectApplication.class, args);
+        Node root =
+                new NodeImpl("1", new NodeImpl("11", new NodeImpl("111", NullNode.getInstance(),
+                        NullNode.getInstance()), NullNode.getInstance()), new NodeImpl("12",
+                        NullNode.getInstance(), new NodeImpl("122", NullNode.getInstance(),
+                        NullNode.getInstance())));
+
+        root.walk();
     }
 
 }
