dfad5cf57e27d305f19336976ffac93bf45a4171
saxing <saxlh@foxmail.com>
2019-05-05 11:09:05 +0800

    design pattern: naked-object-dom

---
+++ a0015_java_design_pattern/aa0015_70_nacked-objects/integtest/src/main/java/org/saxing/nakedobjectsintegtests/IntegtestApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.nakedobjectsintegtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegtestApplication.class, args);
+    }
+
+}
