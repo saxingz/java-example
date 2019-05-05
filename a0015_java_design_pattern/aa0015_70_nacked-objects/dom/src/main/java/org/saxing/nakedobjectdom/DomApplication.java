65e828809c5df5439b20db2d19f72fa5d4e1d4d6
saxing <saxlh@foxmail.com>
2019-05-05 10:12:04 +0800

    design pattern: naked-object-dom

---
+++ a0015_java_design_pattern/aa0015_70_nacked-objects/dom/src/main/java/org/saxing/nakedobjectdom/DomApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.nakedobjectdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomApplication.class, args);
+    }
+
+}
