112def8130513b00705767fa8c7ebf863d09a0f8
saxing <saxlh@foxmail.com>
2019-05-05 10:56:03 +0800

    design pattern: naked-object-dom

---
+++ a0015_java_design_pattern/aa0015_70_nacked-objects/fixture/src/main/java/org/saxing/nakedobjectsfixture/FixtureApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.nakedobjectsfixture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixtureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixtureApplication.class, args);
+    }
+
+}
