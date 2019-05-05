c550ee55920082c7c4bbc227eaa5d3c04a50ee34
saxing <saxlh@foxmail.com>
2019-05-05 15:15:38 +0800

    design pattern: null-object

--- a0015_java_design_pattern/aa0015_71_null-object/src/main/java/org/saxing/nullobject/Aa001571NullObjectApplication.java
+++ a0015_java_design_pattern/aa0015_71_null-object/src/main/java/org/saxing/nullobject/Aa001571NullObjectApplication.java
@@ -12,10 +12,9 @@ public class Aa001571NullObjectApplication {
 
     public static void main(String[] args) {
         Node root =
-                new NodeImpl("1", new NodeImpl("11", new NodeImpl("111", NullNode.getInstance(),
-                        NullNode.getInstance()), NullNode.getInstance()), new NodeImpl("12",
-                        NullNode.getInstance(), new NodeImpl("122", NullNode.getInstance(),
-                        NullNode.getInstance())));
+                new NodeImpl("1",
+                        new NodeImpl("11", new NodeImpl("111", NullNode.getInstance(),NullNode.getInstance()), NullNode.getInstance()),
+                        new NodeImpl("12", NullNode.getInstance(), new NodeImpl("122", NullNode.getInstance(), NullNode.getInstance())));
 
         root.walk();
     }
