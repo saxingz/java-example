0d64333e4b73ca51228752ee7cae61c063306a8d
saxing <saxlh@foxmail.com>
2019-05-06 11:20:30 +0800

    design pattern: object-mother

---
+++ a0015_java_design_pattern/aa0015_72_object-mother/src/main/java/org/saxing/objectmother/Aa001572ObjectMotherApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.objectmother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001572ObjectMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001572ObjectMotherApplication.class, args);
+    }
+
+}
---
+++ a0015_java_design_pattern/aa0015_72_object-mother/src/test/java/org/saxing/objectmother/Aa001572ObjectMotherApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.objectmother;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001572ObjectMotherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
