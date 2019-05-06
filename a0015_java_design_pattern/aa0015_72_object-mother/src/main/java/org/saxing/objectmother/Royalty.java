ab9ceba3935c56694c3afc2836c2156daf071570
saxing <saxlh@foxmail.com>
2019-05-06 11:25:11 +0800

    design pattern: object-mother

---
+++ a0015_java_design_pattern/aa0015_72_object-mother/src/main/java/org/saxing/objectmother/Royalty.java
@@ -0,0 +1,18 @@
+package org.saxing.objectmother;
+
+/**
+ * Interface contracting Royalty Behaviour
+ *
+ * @author saxing 2019/5/6 11:21
+ */
+public interface Royalty {
+
+    void makeDrunk();
+
+    void makeSober();
+
+    void makeHappy();
+
+    void makeUnhappy();
+
+}
