b5411cc6741096a884176aaabd6d1dc0be44e938
saxing <saxlh@foxmail.com>
2019-05-08 14:28:19 +0800

    design pattern: object-pool

---
+++ a0015_java_design_pattern/aa0015_73_object-pool/src/main/java/org/saxing/objectpool/Aa001573ObjectPoolApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.objectpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001573ObjectPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001573ObjectPoolApplication.class, args);
+    }
+
+}
