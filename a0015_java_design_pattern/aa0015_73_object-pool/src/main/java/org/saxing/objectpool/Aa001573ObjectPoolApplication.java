077a440e9ea6ffa7d317cc144514f118029b2b8d
saxing <saxlh@foxmail.com>
2019-05-10 17:50:56 +0800

    design pattern: object-pool

--- a0015_java_design_pattern/aa0015_73_object-pool/src/main/java/org/saxing/objectpool/Aa001573ObjectPoolApplication.java
+++ a0015_java_design_pattern/aa0015_73_object-pool/src/main/java/org/saxing/objectpool/Aa001573ObjectPoolApplication.java
@@ -2,8 +2,6 @@ package org.saxing.objectpool;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * main
