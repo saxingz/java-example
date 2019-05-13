70e242fd862e766be70d61f147d08906f26fd1d2
saxing <saxlh@foxmail.com>
2019-05-13 22:02:23 +0800

    pdf edit

---
+++ aa0025_pdf/src/main/java/org/saxing/pdf/Aa0025PdfApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa0025PdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa0025PdfApplication.class, args);
+    }
+
+}
