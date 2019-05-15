5559907d33fa06aa4b28095499f3f9e25f7f6d61
saxing <saxlh@foxmail.com>
2019-05-15 22:23:38 +0800

    hls

---
+++ aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/Aa0026HlsdownloadApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.hlsdownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa0026HlsdownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa0026HlsdownloadApplication.class, args);
+    }
+
+}
