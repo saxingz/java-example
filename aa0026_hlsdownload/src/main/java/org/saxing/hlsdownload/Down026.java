cf70d6d14ff16ba9cc67bb0730493cd793ed338c
saxing <saxlh@foxmail.com>
2019-05-16 01:02:43 +0800

    hls

---
+++ aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/Down026.java
@@ -0,0 +1,20 @@
+package org.saxing.hlsdownload;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Down026 {
+
+    public static List<String> getList(){
+        return Arrays.asList(getResourceArray());
+    }
+
+    public static String[] getResourceArray(){
+        return new String[]{
+                "https://my.oschina.net/haitaohu/blog/1941179",
+                "wobushi",
+
+        };
+    }
+
+}
