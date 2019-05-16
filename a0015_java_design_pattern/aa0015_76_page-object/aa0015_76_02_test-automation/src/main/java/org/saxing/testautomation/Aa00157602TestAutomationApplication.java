5008ab4273f99c74718553aae94de4ab264198cd
saxing <saxlh@foxmail.com>
2019-05-16 16:35:49 +0800

    design pattern: page-object

---
+++ a0015_java_design_pattern/aa0015_76_page-object/aa0015_76_02_test-automation/src/main/java/org/saxing/testautomation/Aa00157602TestAutomationApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.testautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa00157602TestAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa00157602TestAutomationApplication.class, args);
+    }
+
+}
