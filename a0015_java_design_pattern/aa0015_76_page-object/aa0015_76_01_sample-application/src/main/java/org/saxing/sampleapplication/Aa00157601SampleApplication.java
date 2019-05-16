abc6b4582229cfc4e86acaa7b9d1182e47c152b5
saxing <saxlh@foxmail.com>
2019-05-16 16:34:33 +0800

    design pattern: page-object

---
+++ a0015_java_design_pattern/aa0015_76_page-object/aa0015_76_01_sample-application/src/main/java/org/saxing/sampleapplication/Aa00157601SampleApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.sampleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa00157601SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa00157601SampleApplication.class, args);
+    }
+
+}
