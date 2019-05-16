652a09a1f51c21b16bd3d8aff7e688c3e94ac0a3
saxing <saxlh@foxmail.com>
2019-05-16 14:23:45 +0800

    design pattern: observer

---
+++ a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/Aa001575ObserverApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001575ObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001575ObserverApplication.class, args);
+    }
+
+}
