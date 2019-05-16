f7afbd5c2d549addb845e348ac0449822adef18e
saxing <saxlh@foxmail.com>
2019-05-16 15:02:18 +0800

    design pattern: observer

---
+++ a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/generic/GWeather.java
@@ -0,0 +1,9 @@
+package org.saxing.observer.generic;
+
+import org.saxing.observer.WeatherType;
+
+/**
+ *
+ */
+public class GWeather extends Observable<GWeather, Race, WeatherType>   {
+}
---
+++ a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/generic/Race.java
@@ -0,0 +1,12 @@
+package org.saxing.observer.generic;
+
+import org.saxing.observer.WeatherType;
+
+/**
+ * Race
+ */
+public interface Race extends Observer<GWeather, Race, WeatherType>  {
+
+
+
+}
