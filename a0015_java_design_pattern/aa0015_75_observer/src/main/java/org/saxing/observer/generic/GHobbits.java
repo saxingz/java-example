ef516b9c1aa15fc479733cd04f89a38cc7307ac6
saxing <saxlh@foxmail.com>
2019-05-16 15:05:55 +0800

    design pattern: observer

--- a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/generic/GHobbits.java
+++ a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/generic/GHobbits.java
@@ -5,28 +5,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * GOrcs
+ * GHobbits
  *
- * @author saxing 2019/5/16 15:03
+ * @author saxing 2019/5/16 15:05
  */
-public class GOrcs implements Race  {
+public class GHobbits implements Race {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GOrcs.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(GHobbits.class);
 
     @Override
     public void update(GWeather subject, WeatherType weatherType) {
         switch (weatherType) {
             case COLD:
-                LOGGER.info("The orcs are freezing cold.");
+                LOGGER.info("The hobbits are shivering in the cold weather.");
                 break;
             case RAINY:
-                LOGGER.info("The orcs are dripping wet.");
+                LOGGER.info("The hobbits look for cover from the rain.");
                 break;
             case SUNNY:
-                LOGGER.info("The sun hurts the orcs' eyes.");
+                LOGGER.info("The happy hobbits bade in the warm sun.");
                 break;
             case WINDY:
-                LOGGER.info("The orc smell almost vanishes in the wind.");
+                LOGGER.info("The hobbits hold their hats tightly in the windy weather.");
                 break;
             default:
                 break;
