e6323cc2d2e8dff1560ff31dba37ca744ab980f2
saxing <saxlh@foxmail.com>
2019-05-16 16:52:43 +0800

    design pattern: page-object

--- a0015_java_design_pattern/aa0015_76_page-object/aa0015_76_01_sample-application/src/main/java/org/saxing/sampleapplication/Aa00157601SampleApplication.java
+++ a0015_java_design_pattern/aa0015_76_page-object/aa0015_76_01_sample-application/src/main/java/org/saxing/sampleapplication/Aa00157601SampleApplication.java
@@ -1,13 +1,41 @@
 package org.saxing.sampleapplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Aa00157601SampleApplication {
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * main
+ *
+ * @author saxing 2019/5/16 16:51
+ */
+public final class Aa00157601SampleApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa00157601SampleApplication.class);
+    private Aa00157601SampleApplication() {
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa00157601SampleApplication.class, args);
+        try {
+            File applicationFile = new File(Aa00157601SampleApplication.class.getClassLoader().getResource("sample-ui/login.html").getPath());
+
+            // should work for unix like OS (mac, unix etc...)
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().open(applicationFile);
+
+            } else {
+                // java Desktop not supported - above unlikely to work for Windows so try following instead...
+                Runtime.getRuntime().exec("cmd.exe start " + applicationFile);
+            }
+
+        } catch (IOException ex) {
+            LOGGER.error("An error occured.", ex);
+        }
     }
 
 }
