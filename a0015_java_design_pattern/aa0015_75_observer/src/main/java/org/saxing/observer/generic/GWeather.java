a9b382d153c1581d89973e11a56b776586b3b984
saxing <saxlh@foxmail.com>
2019-05-16 15:03:06 +0800

    design pattern: observer

--- a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/generic/GWeather.java
+++ a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/generic/GWeather.java
@@ -1,9 +1,31 @@
 package org.saxing.observer.generic;
 
 import org.saxing.observer.WeatherType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * GWeather
  *
+ * @author saxing 2019/5/16 15:02
  */
 public class GWeather extends Observable<GWeather, Race, WeatherType>   {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GWeather.class);
+
+    private WeatherType currentWeather;
+
+    public GWeather() {
+        currentWeather = WeatherType.SUNNY;
+    }
+
+    /**
+     * Makes time pass for weather
+     */
+    public void timePasses() {
+        WeatherType[] enumValues = WeatherType.values();
+        currentWeather = enumValues[(currentWeather.ordinal() + 1) % enumValues.length];
+        LOGGER.info("The weather changed to {}.", currentWeather);
+        notifyObservers(currentWeather);
+    }
 }
