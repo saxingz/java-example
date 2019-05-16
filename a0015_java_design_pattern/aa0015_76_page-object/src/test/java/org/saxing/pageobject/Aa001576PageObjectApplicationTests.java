80a3db44ca3b9bc786fadd8be617aa2e56ba6301
saxing <saxlh@foxmail.com>
2019-05-16 16:39:19 +0800

    design pattern: page-object

--- a0015_java_design_pattern/aa0015_76_page-object/src/test/java/org/saxing/pageobject/Aa001576PageObjectApplicationTests.java
+++ a0015_java_design_pattern/aa0015_76_page-object/src/test/java/org/saxing/pageobject/Aa001576PageObjectApplicationTests.java
@@ -1,15 +1,7 @@
 package org.saxing.pageobject;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class Aa001576PageObjectApplicationTests {
 
-    @Test
     public void contextLoads() {
     }
 
