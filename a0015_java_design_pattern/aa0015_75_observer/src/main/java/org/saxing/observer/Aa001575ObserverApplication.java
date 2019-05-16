3ba6cb1d859477b4aa8d55f5e94b9b74d0564765
saxing <saxlh@foxmail.com>
2019-05-16 15:08:26 +0800

    design pattern: observer

--- a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/Aa001575ObserverApplication.java
+++ a0015_java_design_pattern/aa0015_75_observer/src/main/java/org/saxing/observer/Aa001575ObserverApplication.java
@@ -1,13 +1,40 @@
 package org.saxing.observer;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.saxing.observer.generic.GHobbits;
+import org.saxing.observer.generic.GOrcs;
+import org.saxing.observer.generic.GWeather;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SpringBootApplication
+/**
+ * main
+ *
+ * @author saxing 2019/5/16 15:07
+ */
 public class Aa001575ObserverApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aa001575ObserverApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Aa001575ObserverApplication.class, args);
+        Weather weather = new Weather();
+        weather.addObserver(new Orcs());
+        weather.addObserver(new Hobbits());
+
+        weather.timePasses();
+        weather.timePasses();
+        weather.timePasses();
+        weather.timePasses();
+
+        // Generic observer inspired by Java Generics and Collection by Naftalin & Wadler
+        LOGGER.info("--Running generic version--");
+        GWeather gWeather = new GWeather();
+        gWeather.addObserver(new GOrcs());
+        gWeather.addObserver(new GHobbits());
+
+        gWeather.timePasses();
+        gWeather.timePasses();
+        gWeather.timePasses();
+        gWeather.timePasses();
     }
 
 }
