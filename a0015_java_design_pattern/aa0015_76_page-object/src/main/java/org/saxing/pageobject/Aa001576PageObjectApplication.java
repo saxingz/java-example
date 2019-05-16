aeb6f9fa11f21ce463f0a51ba30d6b11bff96dad
saxing <saxlh@foxmail.com>
2019-05-16 16:39:07 +0800

    design pattern: page-object

--- a0015_java_design_pattern/aa0015_76_page-object/src/main/java/org/saxing/pageobject/Aa001576PageObjectApplication.java
+++ a0015_java_design_pattern/aa0015_76_page-object/src/main/java/org/saxing/pageobject/Aa001576PageObjectApplication.java
@@ -1,13 +1,9 @@
 package org.saxing.pageobject;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Aa001576PageObjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aa001576PageObjectApplication.class, args);
+
     }
 
 }
