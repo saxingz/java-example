0cb9cea96cd49369cd84823457d7a57d0f0700d9
saxing <saxlh@foxmail.com>
2019-05-16 17:11:02 +0800

    design pattern: page-object

--- a0015_java_design_pattern/aa0015_76_page-object/aa0015_76_02_test-automation/src/main/java/org/saxing/testautomation/AlbumPage.java
+++ a0015_java_design_pattern/aa0015_76_page-object/aa0015_76_02_test-automation/src/main/java/org/saxing/testautomation/AlbumPage.java
@@ -1,6 +1,11 @@
 package org.saxing.testautomation;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * Page Object encapsulating the Album Page (album-page.html)
@@ -9,14 +14,36 @@ import com.gargoylesoftware.htmlunit.WebClient;
  */
 public class AlbumPage extends Page {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AlbumPage.class);
+    private static final String ALBUM_PAGE_HTML_FILE = "album-page.html";
+    private static final String PAGE_URL = "file:" + AUT_PATH + ALBUM_PAGE_HTML_FILE;
+
+    private HtmlPage page;
 
 
     public AlbumPage(WebClient webClient) {
         super(webClient);
     }
 
+    /**
+     * Navigates to the album page
+     *
+     * @return {@link AlbumPage}
+     */
+    public AlbumPage navigateToPage() {
+        try {
+            page = this.webClient.getPage(PAGE_URL);
+        } catch (IOException e) {
+            LOGGER.error("An error occured on navigateToPage.", e);
+        }
+        return this;
+    }
+
     @Override
     public boolean isAt() {
-        return false;
+        return "Album Page".equals(page.getTitleText());
     }
+
+
+
 }
