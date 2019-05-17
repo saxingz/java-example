4bac901b746cd7d6c5ff0a86aee9888215c5eda6
saxing <saxlh@foxmail.com>
2019-05-17 17:29:50 +0800

    design pattern: partial-response

---
+++ a0015_java_design_pattern/aa0015_74_partial-response/src/main/java/org/saxing/partialresponse/Aa001574PartialResponseApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.partialresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa001574PartialResponseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa001574PartialResponseApplication.class, args);
+    }
+
+}
---
+++ a0015_java_design_pattern/aa0015_74_partial-response/src/test/java/org/saxing/partialresponse/Aa001574PartialResponseApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.partialresponse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aa001574PartialResponseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
