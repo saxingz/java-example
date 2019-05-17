febe9940d1da244d97c412cfd4a01ee11b89f6c8
saxing <saxlh@foxmail.com>
2019-05-18 00:04:14 +0800

    hls

--- aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/HlsDownloader.java
+++ aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/HlsDownloader.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
