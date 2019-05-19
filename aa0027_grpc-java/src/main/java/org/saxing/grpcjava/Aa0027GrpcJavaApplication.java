4375c0e4f0c1d4fec5ebdabdb32ec8b31c24b46a
saxing <saxlh@foxmail.com>
2019-05-19 22:25:08 +0800

    grpc

---
+++ aa0027_grpc-java/src/main/java/org/saxing/grpcjava/Aa0027GrpcJavaApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.grpcjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa0027GrpcJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa0027GrpcJavaApplication.class, args);
+    }
+
+}
