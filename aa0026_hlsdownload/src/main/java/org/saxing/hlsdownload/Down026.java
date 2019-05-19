2cfc25f06521d4e13d0e439e5eb3b770eb4520fd
saxing <saxlh@foxmail.com>
2019-05-19 22:24:51 +0800

    news

--- aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/Down026.java
+++ aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/Down026.java
@@ -11,8 +11,8 @@ public class Down026 {
 
     public static String[] getResourceArray(){
         return new String[]{
-                "https://media001.geekbang.org/8ddb9b3ad2b745629d6a04d9c79d39a8/01cf7257583a4f6f841d980f23b8c902-dd0080d36e595db81a3b8d966f83883b-hd.m3u8",
-                "01 课程介绍",
+                "http://asp.cntv.kcdnvip.com/asp/hls/2000/0303000a/3/default/40b5cd1e6dae4a51914955a08a4c9b64/2000.m3u8",
+                "2019.05.19",
         };
     }
 
