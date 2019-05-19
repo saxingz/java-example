efd254c18c1192e770fd053cb685330d82873b1e
saxing <saxlh@foxmail.com>
2019-05-19 22:28:36 +0800

    grpc

---
+++ aa0027_grpc-java/aa0027_01_grpc-api/src/main/java/org/saxing/grpcapi/Aa002701GrpcApiApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.grpcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa002701GrpcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa002701GrpcApiApplication.class, args);
+    }
+
+}
