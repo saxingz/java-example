a912e7fe120c789ff9723d366a55aef619cf4fc3
saxing <saxlh@foxmail.com>
2019-05-20 23:47:10 +0800

    grpc

--- aa0027_grpc-java/aa0027_02_grpc_client/src/main/java/org/saxing/aa0027_02_grpc_client/Aa002702GrpcClientApplication.java
+++ aa0027_grpc-java/aa0027_02_grpc_client/src/main/java/org/saxing/aa0027_02_grpc_client/Aa002702GrpcClientApplication.java
@@ -1,13 +1,32 @@
 package org.saxing.aa0027_02_grpc_client;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.saxing.grpc.api.RPCDateRequest;
+import org.saxing.grpc.api.RPCDateResponse;
+import org.saxing.grpc.api.RPCDateServiceGrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Aa002702GrpcClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa002702GrpcClientApplication.class, args);
+    private static final String host = "localhost";
+    private static final int serverPort = 8180;
+
+    public static void main( String[] args ) throws Exception {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress( host, serverPort ).usePlaintext().build();
+        try {
+            RPCDateServiceGrpc.RPCDateServiceBlockingStub rpcDateService = RPCDateServiceGrpc.newBlockingStub( managedChannel );
+            RPCDateRequest rpcDateRequest = RPCDateRequest
+                    .newBuilder()
+                    .setUserName("saxing")
+                    .build();
+            RPCDateResponse rpcDateResponse = rpcDateService.getDate( rpcDateRequest );
+            System.out.println( rpcDateResponse.getServerDate() );
+        } finally {
+            managedChannel.shutdown();
+        }
     }
 
 }
