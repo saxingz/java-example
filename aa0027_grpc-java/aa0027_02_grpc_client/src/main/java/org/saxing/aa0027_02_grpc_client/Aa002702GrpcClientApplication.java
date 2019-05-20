5306bfa0a050a09a0f1ceafa2d703f08e29ea990
saxing <saxlh@foxmail.com>
2019-05-20 23:50:08 +0800

    grpc

--- aa0027_grpc-java/aa0027_02_grpc_client/src/main/java/org/saxing/aa0027_02_grpc_client/Aa002702GrpcClientApplication.java
+++ aa0027_grpc-java/aa0027_02_grpc_client/src/main/java/org/saxing/aa0027_02_grpc_client/Aa002702GrpcClientApplication.java
@@ -8,14 +8,13 @@ import org.saxing.grpc.api.RPCDateServiceGrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Aa002702GrpcClientApplication {
 
-    private static final String host = "localhost";
-    private static final int serverPort = 8180;
+    private static final String HOST = "localhost";
+    private static final int PORT = 8180;
 
     public static void main( String[] args ) throws Exception {
-        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress( host, serverPort ).usePlaintext().build();
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress(HOST, PORT).usePlaintext().build();
         try {
             RPCDateServiceGrpc.RPCDateServiceBlockingStub rpcDateService = RPCDateServiceGrpc.newBlockingStub( managedChannel );
             RPCDateRequest rpcDateRequest = RPCDateRequest
