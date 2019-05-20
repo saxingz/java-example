a454a0fcd494e196d3c979794a04ffad212ea738
saxing <saxlh@foxmail.com>
2019-05-20 23:41:13 +0800

    grpc

--- aa0027_grpc-java/aa0027_02_server/src/main/java/org/saxing/server/Aa002702ServerApplication.java
+++ aa0027_grpc-java/aa0027_02_server/src/main/java/org/saxing/server/Aa002702ServerApplication.java
@@ -1,13 +1,22 @@
 package org.saxing.server;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class Aa002702ServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Aa002702ServerApplication.class, args);
+    private static final int port = 8180;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.
+                forPort(port)
+                .addService( new RPCDateServiceImpl() )
+                .build().start();
+        System.out.println( "服务端启动成功, 端口=" + port );
+        server.awaitTermination();
     }
 
 }
