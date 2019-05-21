4ab5a73ca304bb46d662c5560ab9dced4145849b
saxing <saxlh@foxmail.com>
2019-05-21 14:40:03 +0800

    design pattern: partial-response

---
+++ a0015_java_design_pattern/aa0015_74_partial-response/src/main/java/org/saxing/partialresponse/Video.java
@@ -0,0 +1,49 @@
+package org.saxing.partialresponse;
+
+/**
+ * video
+ * 
+ * @author saxing 2019/5/21 14:39
+ */
+public class Video {
+
+    private final Integer id;
+    private final String title;
+    private final Integer length;
+    private final String description;
+    private final String director;
+    private final String language;
+
+    /**
+     * @param id          video unique id
+     * @param title       video title
+     * @param length      video length in minutes
+     * @param description video description by publisher
+     * @param director    video director name
+     * @param language    video language {private, public}
+     */
+    public Video(Integer id, String title, Integer length, String description, String director, String language) {
+        this.id = id;
+        this.title = title;
+        this.length = length;
+        this.description = description;
+        this.director = director;
+        this.language = language;
+    }
+
+    /**
+     * @return json representaion of video
+     */
+    @Override
+    public String toString() {
+        return "{"
+                + "\"id\": " + id + ","
+                + "\"title\": \"" + title + "\","
+                + "\"length\": " + length + ","
+                + "\"description\": \"" + description + "\","
+                + "\"director\": \"" + director + "\","
+                + "\"language\": \"" + language + "\","
+                + "}";
+    }
+    
+}
