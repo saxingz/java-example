d39d3e9a924f692aed3354c119fb2329507e6ec3
saxing <saxlh@foxmail.com>
2019-05-22 15:50:03 +0800

    system info

--- a0020_java8/src/main/java/org/saxing/encry/ThreeDES.java
+++ a0020_java8/src/main/java/org/saxing/encry/ThreeDES.java
@@ -185,7 +185,8 @@ public class ThreeDES {
      */
     public static String md5_2(String text, String key) {
         String k = "78cdabb3-5a8f-4899-9214-cf37b6e7caab";
-        return org.apache.commons.codec.digest.DigestUtils.md5Hex(text + key + k);
+//        return org.apache.commons.codec.digest.DigestUtils.md5Hex(text + key + k);
+        return null;
     }
 
     /**
