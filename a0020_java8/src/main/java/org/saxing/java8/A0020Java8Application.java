0b6f54dd66b4f39819a38807ed30888f8d7b5f5d
saxing <saxlh@foxmail.com>
2019-05-22 15:49:56 +0800

    system info

--- a0020_java8/src/main/java/org/saxing/java8/A0020Java8Application.java
+++ a0020_java8/src/main/java/org/saxing/java8/A0020Java8Application.java
@@ -1,12 +1,21 @@
 package org.saxing.java8;
 
+import org.saxing.systeminfo.RuntimeTest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class A0020Java8Application {
+public class A0020Java8Application
+        implements CommandLineRunner
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(A0020Java8Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RuntimeTest.systemInfo();
     }
 }
