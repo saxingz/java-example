1f4c925b61603f9f7fe3eee59821ef5ea19453aa
saxing <saxlh@foxmail.com>
2019-05-23 13:36:38 +0800

    down test

--- aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/Down026.java
+++ aa0026_hlsdownload/src/main/java/org/saxing/hlsdownload/Down026.java
@@ -11,8 +11,8 @@ public class Down026 {
 
     public static String[] getResourceArray(){
         return new String[]{
-                "http://asp.cntv.kcdnvip.com/asp/hls/2000/0303000a/3/default/40b5cd1e6dae4a51914955a08a4c9b64/2000.m3u8",
-                "2019.05.19",
+                "https://res001.geekbang.org/media/audio/fd/b2/fdc58ecbae845a0dcf6045fc7d4132b2/ld/ld.m3u8",
+                "003",
         };
     }
 
