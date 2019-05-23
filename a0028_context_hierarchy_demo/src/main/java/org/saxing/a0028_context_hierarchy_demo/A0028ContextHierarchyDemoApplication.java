5c6ba9eadfe1338ead30d7ed99a358f009a26ba6
saxing <saxlh@foxmail.com>
2019-05-23 22:13:07 +0800

    context hierarchy

---
+++ a0028_context_hierarchy_demo/src/main/java/org/saxing/a0028_context_hierarchy_demo/A0028ContextHierarchyDemoApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0028_context_hierarchy_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0028ContextHierarchyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0028ContextHierarchyDemoApplication.class, args);
+    }
+
+}
---
+++ a0028_context_hierarchy_demo/src/test/java/org/saxing/a0028_context_hierarchy_demo/A0028ContextHierarchyDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.a0028_context_hierarchy_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0028ContextHierarchyDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
