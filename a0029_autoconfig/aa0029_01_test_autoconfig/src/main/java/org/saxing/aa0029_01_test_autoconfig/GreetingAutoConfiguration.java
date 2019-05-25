933748bdeac35d40814c094147fbb88df32625a3
saxing <saxlh@foxmail.com>
2019-05-25 14:35:16 +0800

    autoconfig

--- a0029_autoconfig/aa0029_01_test_autoconfig/src/main/java/org/saxing/aa0029_01_test_autoconfig/GreetingAutoConfiguration.java
+++ a0029_autoconfig/aa0029_01_test_autoconfig/src/main/java/org/saxing/aa0029_01_test_autoconfig/GreetingAutoConfiguration.java
@@ -1,4 +1,21 @@
 package org.saxing.aa0029_01_test_autoconfig;
 
-public class GreetingAutoConfiguration {
+import org.saxing.aa0029_02_test_greet.GreetingApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(GreetingApplicationRunner.class)
+public class GreetingAutoConfiguration  {
+
+    @Bean
+    @ConditionalOnMissingBean(GreetingApplicationRunner.class)
+    @ConditionalOnProperty(name = "greeting.enabled", havingValue = "true", matchIfMissing = true)
+    public GreetingApplicationRunner greetingApplicationRunner() {
+        return new GreetingApplicationRunner();
+    }
+
 }
