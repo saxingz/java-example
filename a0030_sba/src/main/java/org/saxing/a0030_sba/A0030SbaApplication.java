81d8a5d37678b14366d5382f089b2e3064ea5255
saxing <saxlh@foxmail.com>
2019-05-25 16:41:01 +0800

    sba

---
+++ a0030_sba/src/main/java/org/saxing/a0030_sba/A0030SbaApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0030_sba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0030SbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0030SbaApplication.class, args);
+    }
+
+}
---
+++ a0030_sba/src/test/java/org/saxing/a0030_sba/A0030SbaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.a0030_sba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0030SbaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
