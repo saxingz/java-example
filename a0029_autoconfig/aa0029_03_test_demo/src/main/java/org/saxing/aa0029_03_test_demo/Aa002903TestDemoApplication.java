5da0fd9d5193e7d59b3333f15229dae2b793195b
saxing <saxlh@foxmail.com>
2019-05-25 15:25:21 +0800

    autoconfig

--- a0029_autoconfig/aa0029_03_test_demo/src/main/java/org/saxing/aa0029_03_test_demo/Aa002903TestDemoApplication.java
+++ a0029_autoconfig/aa0029_03_test_demo/src/main/java/org/saxing/aa0029_03_test_demo/Aa002903TestDemoApplication.java
@@ -1,13 +1,20 @@
 package org.saxing.aa0029_03_test_demo;
 
+import org.saxing.aa0029_02_test_greet.GreetingApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Aa002903TestDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Aa002903TestDemoApplication.class, args);
+    }
+
+    @Bean
+    public GreetingApplicationRunner greetingApplicationRunner(){
+        return new GreetingApplicationRunner("modify self");
     }
 
 }
