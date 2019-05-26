26fe18a0016bddd811824e62d6de1aeac52447cf
saxing <saxlh@foxmail.com>
2019-05-26 16:01:05 +0800

    sba

---
+++ a0020_java8/src/main/java/org/saxing/a/thread/ThreadTest.java
@@ -0,0 +1,17 @@
+package org.saxing.a.thread;
+
+import java.util.Arrays;
+
+public class ThreadTest {
+
+    public static void main(String[] args) {
+        // NEW
+        // RUNNABLE
+        // BLOCKED
+        // WAITTING
+        // TIMED_WAITING
+        // TERMINATED
+        System.out.println(Arrays.toString(Thread.State.values()));
+    }
+
+}
