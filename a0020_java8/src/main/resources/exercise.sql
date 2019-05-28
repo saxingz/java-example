d19d0693f276a5b82e526b0cd7258dddfa41b064
saxing <saxlh@foxmail.com>
2019-05-28 19:47:03 +0800

    sql

--- a0020_java8/src/main/resources/exercise.sql
+++ a0020_java8/src/main/resources/exercise.sql
@@ -4,9 +4,7 @@ create table T(c int) engine=InnoDB;
 -- 查看事务隔离级别
 show variables like 'transaction_isolation';
 
-
-
-
-
+-- 查找持续时间超过 60s 的事务
+select * from information_schema.innodb_trx where TIME_TO_SEC(timediff(now(),trx_started))>60
 
 
