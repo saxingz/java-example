11c367d6c8bea585b198657137937df5b6250dc9
saxing <saxlh@foxmail.com>
2019-05-30 19:19:23 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/ReverseTest.java
@@ -0,0 +1,71 @@
+package org.saxing.a.algorithm;
+
+public class ReverseTest {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+//        ListNode listNode5 = new ListNode(5);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+//        listNode4.next = listNode5;
+
+        System.out.println(listNode1);
+
+        ListNode listNode = swapPairs(listNode1);
+
+        System.out.println(listNode);
+    }
+
+//    public static ListNode swapPairs(ListNode head){
+//        ListNode id = head;
+//        while (head != null && head.next != null){
+//            ListNode temp1 = head.next;
+//            ListNode temp2 = temp1.next;
+//            head.next = temp2;
+//            temp1.next = head;
+//            head = temp2;
+//        }
+//        return id;
+//    }
+
+    public static ListNode swapPairs(ListNode head){
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+
+
+//    public static ListNode swapPairs(ListNode head) {
+//        if ((head == null)||(head.next == null))
+//            return head;
+//        ListNode n = head.next;
+//        head.next = swapPairs(head.next.next);
+//        n.next = head;
+//        return n;
+//    }
+
+//    public static ListNode swapPairs(ListNode head) {
+//        if (head == null || head.next == null) return head;
+//        ListNode cur = head;
+//        ListNode newHead = head.next;
+//        while (cur != null && cur.next != null) {
+//            ListNode tmp = cur;
+//            cur = cur.next;
+//            tmp.next = cur.next;
+//            cur.next = tmp;
+//            cur = tmp.next;
+//            if (cur != null && cur.next != null) tmp.next = cur.next;
+//        }
+//        return newHead;
+//    }
+
+}
---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/ReverseTest2.java
@@ -0,0 +1,64 @@
+package org.saxing.a.algorithm;
+
+public class ReverseTest2 {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        System.out.println(listNode1);
+
+        ListNode listNode = reverseList(listNode1);
+
+        System.out.println(listNode);
+    }
+
+    public static ListNode reverseList(ListNode head){
+        ListNode cur = head;
+        ListNode prev = null;
+        while (cur != null){
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static ListNode reverseList(ListNode head){
+//        ListNode cur = head;
+//        ListNode prev = null;
+//        while(cur != null){
+//            ListNode nextTemp = cur.next;
+//            cur.next = prev;
+//            prev = cur;
+//            cur = nextTemp;
+//        }
+//        return prev;
+//    }
+
+
+}
