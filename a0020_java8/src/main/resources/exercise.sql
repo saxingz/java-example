34f52891c350c0b56c81b26c4baef08ea8fc9e9a
saxing <saxlh@foxmail.com>
2019-05-30 14:19:08 +0800

    test

--- a0020_java8/src/main/resources/exercise.sql
+++ a0020_java8/src/main/resources/exercise.sql
@@ -7,4 +7,19 @@ show variables like 'transaction_isolation';
 -- 查找持续时间超过 60s 的事务
 select * from information_schema.innodb_trx where TIME_TO_SEC(timediff(now(),trx_started))>60
 
+-- 存储过程来插入数据
+delimiter ;;
+create procedure idata()
+begin
+  declare i int;
+  set i=1;
+  while(i<=100000)do
+    insert into t values(i, i, i);
+    set i=i+1;
+  end while;
+end;;
+delimiter ;
+call idata();
+
+
 
