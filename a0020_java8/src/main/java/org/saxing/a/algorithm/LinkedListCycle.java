83743d0ce1d15a8e60c6d52255ac8b741412770e
saxing <saxlh@foxmail.com>
2019-05-30 21:16:12 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm/LinkedListCycle.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/LinkedListCycle.java
@@ -18,5 +18,20 @@ public class LinkedListCycle {
         return false;
     }
 
+    public boolean hasCycle2(ListNode head){
+        if (head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast){
+            if (fast == null || fast.next == null){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
 
 }
