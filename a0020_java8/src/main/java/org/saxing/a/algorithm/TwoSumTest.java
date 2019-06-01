c22178e6b035ad622222c3ba3cd34e38bf559133
saxing <saxlh@foxmail.com>
2019-06-01 16:01:44 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/TwoSumTest.java
@@ -0,0 +1,80 @@
+package org.saxing.a.algorithm;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TwoSumTest {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+
+        int[] ints = twoSum(nums, target);
+        System.out.println(ints);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSum4(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSum3(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.clear();
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i){
+                    set.add(nums[j]);
+                }
+            }
+
+            int t = target - nums[i];
+            int index = 0;
+            if (set.contains(t)){
+                for (int j = 0; j < nums.length; j++) {
+                    if (i != j && t == nums[j]){
+                        index = j;
+                        break;
+                    }
+                }
+                return new int[]{i, index};
+            }
+        }
+        return new int[0];
+    }
+
+}
