2c3dbc6649784e3ab1b878ccc13b1f436c19563b
saxing <saxlh@foxmail.com>
2019-06-01 20:31:00 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm/ValidBSTTest.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/ValidBSTTest.java
@@ -1,7 +1,23 @@
 package org.saxing.a.algorithm;
 
+import org.saxing.a.algorithm.base.TreeNode;
+
 public class ValidBSTTest {
 
 
+
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
+    }
+
+
+    public boolean isValid(TreeNode root, Integer min, Integer max){
+        if (root == null) return true;
+        if (min != null && root.val <= min) return false;
+        if (max != null && root.val >= max) return false;
+
+        return isValid(root.left, min, root.val)
+                && isValid(root.right, root.val, max);
+    }
 
 }
