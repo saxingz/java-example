7e5ebb49393d1d3a18cb9351246ddecf368358e2
saxing <saxlh@foxmail.com>
2019-06-01 16:03:31 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm/TwoSumTest.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/TwoSumTest.java
@@ -22,6 +22,20 @@ public class TwoSumTest {
         }
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSum5(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
             if (map.containsKey(complement) && map.get(complement) != i) {
                 return new int[] { i, map.get(complement) };
             }
