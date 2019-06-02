736aee95015a0fb03575cd742e29a977cd247c35
saxing <saxlh@foxmail.com>
2019-06-02 15:27:54 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/NQueen2.java
@@ -0,0 +1,43 @@
+package org.saxing.a.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NQueen2 {
+
+
+    public static void main(String[] args) {
+        List<List<String>> lists = solveNQueens(9);
+        System.out.println(lists);
+    }
+
+    private static void helper(int r, boolean[] cols, boolean[] d1, boolean[] d2,
+                        String[] board, List<String[]> res) {
+        if (r == board.length) res.add(board.clone());
+        else {
+            for (int c = 0; c < board.length; c++) {
+                int id1 = r - c + board.length, id2 = 2*board.length - r - c - 1;
+                if (!cols[c] && !d1[id1] && !d2[id2]) {
+                    char[] row = new char[board.length];
+                    Arrays.fill(row, '.'); row[c] = 'Q';
+                    board[r] = new String(row);
+                    cols[c] = true; d1[id1] = true; d2[id2] = true;
+                    helper(r+1, cols, d1, d2, board, res);
+                    cols[c] = false; d1[id1] = false; d2[id2] = false;
+                }
+            }
+        }
+    }
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<String[]> res = new ArrayList<>();
+        helper(0, new boolean[n], new boolean[2*n], new boolean[2*n],
+                new String[n], res);
+
+        List<List<String>> real = new ArrayList<>();
+        res.forEach(r -> real.add(Arrays.asList(r)));
+        return real;
+    }
+}
