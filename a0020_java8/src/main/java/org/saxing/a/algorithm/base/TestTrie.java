9dab179fbd973e59364039032b13b4115b126fde
saxing <saxlh@foxmail.com>
2019-06-03 23:17:32 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/base/TestTrie.java
@@ -0,0 +1,18 @@
+package org.saxing.a.algorithm.base;
+
+public class TestTrie {
+
+//    public static final int ALPHABET_SIZE = 256;
+//
+//    public static class TrieNode{
+//        TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+//        boolean isEndOfWorld = false;
+//        TrieNode(){
+//            isEndOfWorld = false;
+//            for (int i = 0; i < ALPHABET_SIZE; i++) {
+//                children[i] = null;
+//            }
+//        }
+//    }
+
+}
