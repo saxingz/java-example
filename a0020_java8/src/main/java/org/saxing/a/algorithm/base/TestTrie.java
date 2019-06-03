d5e1197ccf4dff86becfc4e29ec8cc5bb80d84ae
saxing <saxlh@foxmail.com>
2019-06-03 23:59:35 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm/base/TestTrie.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm/base/TestTrie.java
@@ -4,10 +4,10 @@ public class TestTrie {
 
 //    public static final int ALPHABET_SIZE = 256;
 //
-//    public static class TrieNode{
-//        TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+//    public static class TrieNode2{
+//        TrieNode2[] children = new TrieNode2[ALPHABET_SIZE];
 //        boolean isEndOfWorld = false;
-//        TrieNode(){
+//        TrieNode2(){
 //            isEndOfWorld = false;
 //            for (int i = 0; i < ALPHABET_SIZE; i++) {
 //                children[i] = null;
