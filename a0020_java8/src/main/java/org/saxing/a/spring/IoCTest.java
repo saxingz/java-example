e810a9ec20920a9283fdf294115be76d59449f21
saxing <saxlh@foxmail.com>
2019-06-12 19:57:55 +0800

    ioc test

---
+++ a0020_java8/src/main/java/org/saxing/a/spring/IoCTest.java
@@ -0,0 +1,16 @@
+package org.saxing.a.spring;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+
+public class IoCTest {
+
+    public static void main(String[] args) {
+        ClassPathResource res = new ClassPathResource("bean.xml");
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(res);
+    }
+
+}
