89d3c5f3c55dc352ce59a035a8da207278676181
saxing <saxlh@foxmail.com>
2019-06-13 14:03:25 +0800

    consistent hash

--- a0020_java8/src/main/java/org/saxing/a/ConsistentHash/ConsistentHash.java
+++ a0020_java8/src/main/java/org/saxing/a/ConsistentHash/ConsistentHash.java
@@ -1,8 +1,34 @@
 package org.saxing.a.ConsistentHash;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class ConsistentHash<T> {
 
 
+
+    /**
+     * 使用MD5算法
+     * @param key
+     * @return
+     */
+    private static long md5HashingAlg(String key) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            md5.reset();
+            md5.update(key.getBytes());
+            byte[] bKey = md5.digest();
+            long res = ((long) (bKey[3] & 0xFF) << 24) | ((long) (bKey[2] & 0xFF) << 16) | ((long) (bKey[1] & 0xFF) << 8)| (long) (bKey[0] & 0xFF);
+            return res;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return 0l;
+    }
 
 
     /**
