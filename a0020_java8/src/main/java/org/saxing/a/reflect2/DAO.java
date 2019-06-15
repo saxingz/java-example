9d395d78f17d914be78e0d08ac26d2d1c0c7a8b0
saxing <saxlh@foxmail.com>
2019-06-15 21:52:12 +0800

    reflect

---
+++ a0020_java8/src/main/java/org/saxing/a/reflect2/DAO.java
@@ -0,0 +1,15 @@
+package org.saxing.a.reflect2;
+
+public class DAO<T> {
+
+    //根据id获取一个对象
+    T get(Integer id){
+        return null;
+    }
+
+    //保存一个对象
+    void save(T entity){
+
+    }
+
+}
