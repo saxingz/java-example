9419fd49980b73fa5a3180c56b840ad31c3f9419
saxing <saxlh@foxmail.com>
2019-06-15 22:01:48 +0800

    reflect

--- a0020_java8/src/main/java/org/saxing/a/reflect2/ReflectionTest.java
+++ a0020_java8/src/main/java/org/saxing/a/reflect2/ReflectionTest.java
@@ -9,7 +9,17 @@ import java.util.Date;
 public class ReflectionTest {
 
     public static void main(String[] args) throws Exception {
-        new ReflectionTest().testInvoke8();
+        new ReflectionTest().testAnnotation9();
+    }
+
+    public void testAnnotation9(){
+        PersonDAO personDAO = new PersonDAO();
+        Person entity = new Person();
+        //调用父类的save方法，同时也把Person这个“实参”传给了父类的T
+        personDAO.save(entity);
+        //这句的本意是要返回一个Person类型的对象
+        Person result = personDAO.get(1);
+        System.out.print(result);
     }
 
     // 使用系统方法（前提是此类有一个无参的构造器（查看API））
