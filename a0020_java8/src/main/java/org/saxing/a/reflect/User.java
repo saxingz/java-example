c8f22d1b27b7fb049061dbbb19d385ed13f36df2
saxing <saxlh@foxmail.com>
2019-06-15 20:47:25 +0800

    reflect

---
+++ a0020_java8/src/main/java/org/saxing/a/reflect/User.java
@@ -0,0 +1,59 @@
+package org.saxing.a.reflect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private Integer id;
+    private String userName;
+    private String password;
+    List<String> books=new ArrayList<String>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String userName, String password) {
+        super();
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public User() {
+        super();
+    }
+
+    public List<String> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<String> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString(){
+        return this.id+" "+this.userName+" "+this.password+" ";
+    }
+
+}
