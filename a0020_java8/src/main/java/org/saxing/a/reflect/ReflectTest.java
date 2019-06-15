bf32397511ce1ce72543fb558d3d7d84d4f32cc5
saxing <saxlh@foxmail.com>
2019-06-15 20:50:06 +0800

    reflect

--- a0020_java8/src/main/java/org/saxing/a/reflect/ReflectTest.java
+++ a0020_java8/src/main/java/org/saxing/a/reflect/ReflectTest.java
@@ -4,12 +4,18 @@ public class ReflectTest {
 
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         // method 1
-//        Class<?> clazz = Class.forName("org.saxing.a.reflect.User");
-//        User user = (User) clazz.newInstance();
+        Class<?> clazz = Class.forName("org.saxing.a.reflect.User");
+        User user = (User) clazz.newInstance();
+
 
         // method 2
-        User user = User.class.newInstance();
+//        User user = User.class.newInstance();
 
+
+        user.setId(10);
+        user.setUserName("小浩");
+        user.setPassword("123456");
+        System.out.println(user);
     }
 
 }
