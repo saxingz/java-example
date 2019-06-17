d7e4f0ac5eb24421411d03f6c672205da8d40ef5
saxing <saxlh@foxmail.com>
2019-06-18 07:52:01 +0800

    thread

--- a0020_java8/src/main/java/org/saxing/a/thread2/ForkJoinTest.java
+++ a0020_java8/src/main/java/org/saxing/a/thread2/ForkJoinTest.java
@@ -2,11 +2,31 @@ package org.saxing.a.thread2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class ForkJoinTest {
 
+    public static void main(String[] args) {
+        String[] fc = {"hello world",
+                "hello me",
+                "hello fork",
+                "hello join",
+                "fork join in world"};
 
+        // 创建 ForkJoin 线程池
+        ForkJoinPool fjp =
+                new ForkJoinPool(3);
+        // 创建任务
+        MR mr = new MR(
+                fc, 0, fc.length);
+        // 启动任务
+        Map<String, Long> result =
+                fjp.invoke(mr);
+        // 输出结果
+        result.forEach((k, v)->
+                System.out.println(k+":"+v));
+    }
 
     static class MR extends RecursiveTask<Map<String, Long>>{
 
