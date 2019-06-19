cd498d575c8b86267b7235f4c4aacf4fcc31bdb5
saxing <saxlh@foxmail.com>
2019-06-19 10:55:21 +0800

    thread

--- a0020_java8/src/main/java/org/saxing/a/thread2/ObjPool.java
+++ a0020_java8/src/main/java/org/saxing/a/thread2/ObjPool.java
@@ -3,22 +3,26 @@ package org.saxing.a.thread2;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.function.Function;
 
 class ObjPool<T, R> {
     final List<T> pool;
     // 用信号量实现限流器
     final Semaphore sem;
+
     // 构造函数
-    ObjPool(int size, T t){
-        pool = new Vector<T>(){};
-        for(int i=0; i<size; i++){
+    ObjPool(int size, T t) {
+        pool = new Vector<T>() {
+        };
+        for (int i = 0; i < size; i++) {
             pool.add(t);
         }
         sem = new Semaphore(size);
     }
+
     // 利用对象池的对象，调用 func
-    R exec(Function<T,R> func) throws InterruptedException {
+    R exec(Function<T, R> func) throws InterruptedException {
         T t = null;
         sem.acquire();
         try {
@@ -29,12 +33,27 @@ class ObjPool<T, R> {
             sem.release();
         }
     }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建对象池
+        ObjPool<Long, String> pool =
+                new ObjPool<Long, String>(2, 10L);
+        // 通过对象池获取 t，之后执行
+        for (int i = 0; i < 10; i++) {
+            pool.exec(t -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(t);
+                return t.toString();
+            });
+        }
+
+    }
 }
-//    // 创建对象池
-//    ObjPool<Long, String> pool =
-//            new ObjPool<Long, String>(10, 2);
-//// 通过对象池获取 t，之后执行
-//pool.exec(t -> {
-//        System.out.println(t);
-//        return t.toString();
-//        });
+
+
+
