86d32f248cad24180ae79c212130adfcc6eae9c6
saxing <saxlh@foxmail.com>
2019-06-22 22:50:20 +0800

    netty

---
+++ a0020_java8/src/main/java/org/saxing/b_Netty/c10/ToIntegerDecoder2.java
@@ -0,0 +1,15 @@
+package org.saxing.b_Netty.c10;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+
+import java.util.List;
+
+public class ToIntegerDecoder2 extends ReplayingDecoder<Void> {
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        out.add(in.readInt());
+    }
+}
