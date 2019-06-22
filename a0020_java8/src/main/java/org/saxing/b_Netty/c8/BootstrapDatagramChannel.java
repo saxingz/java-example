5afcd2d169282117ace27a24be2433a6a0c59be2
saxing <saxlh@foxmail.com>
2019-06-22 22:13:24 +0800

    netty

---
+++ a0020_java8/src/main/java/org/saxing/b_Netty/c8/BootstrapDatagramChannel.java
@@ -0,0 +1,41 @@
+package org.saxing.b_Netty.c8;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.channel.socket.oio.OioDatagramChannel;
+
+import java.net.InetSocketAddress;
+
+public class BootstrapDatagramChannel {
+
+    public void bootstrap() {
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(new OioEventLoopGroup())
+                .channel(OioDatagramChannel.class)
+                .handler(new SimpleChannelInboundHandler<DatagramPacket>() {
+                    @Override
+                    public void channelRead0(ChannelHandlerContext ctx,
+                                             DatagramPacket msg) throws Exception {
+                        // Do something with the packet
+                    }
+                });
+        ChannelFuture future = bootstrap.bind(new InetSocketAddress(0));
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("Channel bound");
+                } else {
+                    System.err.println("Bind attempt failed");
+                    future.cause().printStackTrace();
+                }
+            }
+        });
+    }
+
+}
