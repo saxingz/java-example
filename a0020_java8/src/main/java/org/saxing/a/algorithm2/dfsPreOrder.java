e535453b6a87dcf8148b52ed25ee8ab05cb66f97
saxing <saxlh@foxmail.com>
2019-06-26 10:59:11 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/dfsPreOrder.java
@@ -0,0 +1,38 @@
+package org.saxing.a.algorithm2;
+
+import org.saxing.a.algorithm.base.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * lint code 66
+ *
+ * 给出一棵二叉树，返回其节点值的前序遍历。
+ * 非递归
+ */
+public class dfsPreOrder {
+
+    private List<TreeNode> dfsPre(TreeNode root){
+
+        List<Integer> results = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode cur = root;
+        while (cur != null && !stack.empty()){
+            while (cur != null){
+                results.add(cur.val);
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if (!stack.empty()){
+                cur = stack.pop();
+                cur = cur.right;
+            }
+        }
+    }
+
+
+
+}
