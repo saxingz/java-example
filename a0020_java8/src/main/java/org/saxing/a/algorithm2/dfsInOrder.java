c493b6188d408aa080bb8afdc4a4a2e6d0d299b8
saxing <saxlh@foxmail.com>
2019-06-27 20:17:29 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/dfsInOrder.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/dfsInOrder.java
@@ -23,8 +23,30 @@ public class dfsInOrder {
         root.right = new TreeNode(5);
         root.right.left = new TreeNode(6);
 
-        List<Integer> list = new dfsInOrder().dfsInOrder1(root);
+        List<Integer> list = new dfsInOrder().dfsInOrder11(root);
         System.out.println(list);
+    }
+
+    private List<Integer> dfsInOrder11(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode cur = root;
+
+        while (cur != null || !stack.empty()){
+            while (cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            if (!stack.empty()){
+                cur = stack.pop();
+                result.add(cur.val);
+                cur = cur.right;
+            }
+        }
+
+        return result;
     }
 
     private List<Integer> dfsInOrder1(TreeNode root) {
