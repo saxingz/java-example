db25bc194362d7d85d2f5a7848c8513da1218e05
saxing <saxlh@foxmail.com>
2019-06-27 19:23:53 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/dfsPreOrder.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/dfsPreOrder.java
@@ -1,5 +1,6 @@
 package org.saxing.a.algorithm2;
 
+import org.saxing.a.algorithm.base.ListNode;
 import org.saxing.a.algorithm.base.TreeNode;
 
 import java.util.ArrayList;
@@ -24,11 +25,32 @@ public class dfsPreOrder {
         node.right = new TreeNode(5);
         node.right.left = new TreeNode(6);
 
-        List<Integer> list = new dfsPreOrder().dfsPre(node);
+        List<Integer> list = new dfsPreOrder().dfsPre11(node);
         System.out.println(list);
     }
 
-    private List<Integer> dfsPre(TreeNode root){
+    private List<Integer> dfsPre11(TreeNode root){
+        List<Integer> results = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode cur = root;
+        while (cur != null || !stack.empty()){
+            while (cur != null){
+                results.add(cur.val);
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            if (!stack.empty()){
+                cur = stack.pop();
+                cur = cur.right;
+            }
+        }
+
+        return results;
+    }
+
+    private List<Integer> dfsPre1(TreeNode root){
 
         List<Integer> results = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
