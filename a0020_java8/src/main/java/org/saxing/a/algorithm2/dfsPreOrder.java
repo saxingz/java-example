87368c037826e5ac6f0a59f8ae47990f1db8951b
saxing <saxlh@foxmail.com>
2019-06-27 22:03:20 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/dfsPreOrder.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/dfsPreOrder.java
@@ -16,16 +16,29 @@ public class dfsPreOrder {
 
 
     public static void main(String[] args) {
-        TreeNode node = new TreeNode(1);
-        node.left = new TreeNode(2);
-        node.left.left = new TreeNode(3);
-        node.left.right = new TreeNode(4);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
 
-        node.right = new TreeNode(5);
-        node.right.left = new TreeNode(6);
+        root.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
 
-        List<Integer> list = new dfsPreOrder().dfsPre11(node);
+        List<Integer> list = new dfsPreOrder().dfsPre11(root);
         System.out.println(list);
+
+        List<Integer> resultWithRecursion = new ArrayList<>();
+        new dfsPreOrder().dfsPreWithRecursion(resultWithRecursion, root);
+        System.out.println(resultWithRecursion);
+
+        System.out.println(list.equals(resultWithRecursion));
+    }
+
+    private void dfsPreWithRecursion(List<Integer> result, TreeNode root){
+        if (root == null) return;
+        result.add(root.val);
+        dfsPreWithRecursion(result, root.left);
+        dfsPreWithRecursion(result, root.right);
     }
 
     private List<Integer> dfsPre11(TreeNode root){
