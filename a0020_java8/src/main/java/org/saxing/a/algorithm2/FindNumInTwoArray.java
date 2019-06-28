bd606f9b284d128ed95c3632886548acd8748c34
saxing <saxlh@foxmail.com>
2019-06-28 14:37:25 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/FindNumInTwoArray.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/FindNumInTwoArray.java
@@ -32,6 +32,38 @@ public class FindNumInTwoArray {
         end=System.nanoTime();
         System.out.println(res2);
         System.out.println(end - start);
+
+        start=System.nanoTime();
+        boolean res3 = find3(target, getArray());
+        end=System.nanoTime();
+        System.out.println(res3);
+        System.out.println(end - start);
+    }
+
+    /**
+     * 方案三
+     *
+     * *把每一行看成有序递增的数组，
+     * *利用二分查找，
+     * *通过遍历每一行得到答案，
+     * *时间复杂度是mlogn，每行的时间复杂度是O（logn）。
+     */
+    private static boolean find3(int target, int[][] array){
+        for (int i = 0; i < array.length; i++) {
+            int low = 0;
+            int high = array[i].length - 1;
+            while (low <= high){
+                int mid = (low + high) / 2;
+                if (target < array[i][mid]){
+                    high = mid - 1;
+                }else if (target > array[i][mid]){
+                    low = mid + 1;
+                }else{
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 
