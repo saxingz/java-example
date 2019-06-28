64c3fdf4d23f424d60a81d89e1d8367ca499ddb5
saxing <saxlh@foxmail.com>
2019-06-28 11:21:55 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/SwapNodesinPairs.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/SwapNodesinPairs.java
@@ -7,15 +7,23 @@ import org.saxing.a.algorithm2.base.ListNode;
  */
 public class SwapNodesinPairs {
 
-    public static void main(String[] args) {
+    public static ListNode getNode(){
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
 
-        ListNode listNode = swapPairs(head);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = swapPairs(getNode());
         System.out.println(listNode);
+        ListNode listNode2 = swapPairs2(getNode());
+        System.out.println(listNode2);
+        ListNode listNode3 = swapPairs3(getNode());
+        System.out.println(listNode3);
     }
 
     public static ListNode swapPairs(ListNode head) {
@@ -34,6 +42,46 @@ public class SwapNodesinPairs {
                 temp.next = curr.next;
             }
         }
+        return newHead;
+    }
+
+
+    public static ListNode swapPairs3(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        ListNode newHead = head.next;
+        while (cur != null && cur.next != null){
+            ListNode temp = cur;
+            cur = cur.next;
+            temp.next = cur.next;
+            cur.next = temp;
+            cur = temp.next;
+            if (cur != null && cur.next != null){
+                temp.next = cur.next;
+            }
+
+        }
+
+        return newHead;
+    }
+
+
+    public static ListNode swapPairs2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        ListNode newHead = head.next;
+        while (cur != null && cur.next != null){
+            ListNode temp = cur;
+            cur = cur.next;
+            temp.next = cur.next;
+            cur.next = temp;
+            cur = temp.next;
+            if (cur != null && cur.next != null){
+                temp.next = cur.next;
+            }
+
+        }
+
         return newHead;
     }
 
