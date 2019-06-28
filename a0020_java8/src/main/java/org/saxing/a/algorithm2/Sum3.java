58ae38ca087861290f1e733abdc045cd8e43e4dd
saxing <saxlh@foxmail.com>
2019-06-28 19:06:57 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/Sum3.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/Sum3.java
@@ -1,5 +1,6 @@
 package org.saxing.a.algorithm2;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,7 +22,51 @@ public class Sum3 {
         List<List<Integer>> lists2 = threeSum2(getArray());
         System.out.println(lists2);
 
+
+        List<List<Integer>> lists3 = threeSum2(getArray());
+        System.out.println(lists3);
+
     }
+
+    public static List<List<Integer>> threeSum3(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < num.length - 2; i++) {
+            if (i > 0 && num[i] == num[i - 1]){
+                continue;
+            }
+            int j = i + 1, k = num.length - 1;
+            while (j < k){
+                int candidate = num[i] + num[j] + num[k];
+                if (candidate <= 0){
+                    if (candidate == 0){
+                        result.add(Arrays.asList(num[i], num[j], num[k]));
+                    }
+                    ++j;
+                    while (j < k && num[j] == num[j - 1]){
+                        ++j;
+                    }
+                }else{
+                    --k;
+                    while (j < k && num[k] == num[k-1]){
+                        --k;
+                    }
+                }
+            }
+
+        }
+        return result;
+
+    }
+
+
+
+
+
+
+
+
+
 
     public static List<List<Integer>> threeSum2(int[] num) {
         Arrays.sort(num);
@@ -39,14 +84,14 @@ public class Sum3 {
                     if (candidate == 0){
                         result.add(Arrays.asList(num[i], num[j], num[k]));
                     }
-                    j++;
+                    ++j;
                     while (j < k && num[j] == num[j - 1]){
-                        j++;
+                        ++j;
                     }
                 }else{
-                    k --;
+                    --k;
                     while (j < k && num[k] == num[k - 1]){
-                        k--;
+                        --k;
                     }
                 }
             }
@@ -65,7 +110,7 @@ public class Sum3 {
                 continue;
             }
 
-            int j = i + i, k = num.length - 1;
+            int j = i + 1, k = num.length - 1;
 
             while (j < k){
                 int candidate = num[i] + num[j] + num[k];
