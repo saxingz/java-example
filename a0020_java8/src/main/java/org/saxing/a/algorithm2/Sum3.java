a91bfe84016f6a826220c180176999b268c74927
saxing <saxlh@foxmail.com>
2019-06-28 19:00:17 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/Sum3.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/Sum3.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * leet code 15
+ */
 public class Sum3 {
 
     static int[] getArray(){
@@ -11,10 +14,47 @@ public class Sum3 {
     }
 
     public static void main(String[] args) {
+
         List<List<Integer>> lists = threeSum1(getArray());
         System.out.println(lists);
+
+        List<List<Integer>> lists2 = threeSum2(getArray());
+        System.out.println(lists2);
+
     }
 
+    public static List<List<Integer>> threeSum2(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < num.length - 2; i++) {
+            if (i > 0 && num[i] == num[i - 1]){
+                continue;
+            }
+            int j = i + 1, k = num.length - 1;
+
+            while (j < k){
+                int candidate = num[i] + num[j] + num[k];
+                if (candidate <= 0){
+                    if (candidate == 0){
+                        result.add(Arrays.asList(num[i], num[j], num[k]));
+                    }
+                    j++;
+                    while (j < k && num[j] == num[j - 1]){
+                        j++;
+                    }
+                }else{
+                    k --;
+                    while (j < k && num[k] == num[k - 1]){
+                        k--;
+                    }
+                }
+            }
+        }
+
+        return result;
+
+    }
 
     public static List<List<Integer>> threeSum1(int[] num) {
         Arrays.sort(num);
