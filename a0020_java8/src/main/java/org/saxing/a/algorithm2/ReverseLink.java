9309d836d5b97327b9a881f235c2cd2b9b386451
saxing <saxlh@foxmail.com>
2019-06-28 10:41:35 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/ReverseLink.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/ReverseLink.java
@@ -7,7 +7,18 @@ import org.saxing.a.algorithm2.base.ListNode;
  */
 public class ReverseLink {
 
-    public ListNode reverseList(ListNode head) {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode listNode = reverseList(head);
+        System.out.println(listNode);
+    }
+
+    public static ListNode reverseList(ListNode head) {
         ListNode cur = head;
         ListNode prev = null;
         while (cur != null){
