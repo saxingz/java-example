1452b12f1ce063b455ed47ef220c142cd29f2567
saxing <saxlh@foxmail.com>
2019-06-28 20:32:14 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/ValidateBinarySearchTree.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/ValidateBinarySearchTree.java
@@ -29,8 +29,35 @@ public class ValidateBinarySearchTree {
         System.out.println(isValidBST1(getTreeNode1()));
         System.out.println(isValidBST1(getTreeNode2()));
 
+
+        System.out.println(isValidBST2(getTreeNode1()));
+        System.out.println(isValidBST2(getTreeNode2()));
     }
 
+
+    /**
+     * exec 2 -------------------------------------------------
+     * @param root
+     * @return
+     */
+    public static boolean isValidBST2(TreeNode root) {
+        return isValid2(root, null, null);
+    }
+
+    public static boolean isValid2(TreeNode root, Integer min, Integer max){
+        if (root == null) return true;
+        if (min != null && root.val <= min) return false;
+        if (max != null && root.val >= max) return false;
+        return isValid2(root.left, min, root.val)
+                && isValid2(root.right, root.val, max);
+    }
+
+
+    /**
+     * exec 1----------------------------------------------------
+     * @param root
+     * @return
+     */
     public static boolean isValidBST1(TreeNode root) {
         return isValid1(root, null, null);
     }
