b540bb0a37d8a7b7cbbef086f489e7d0c0476007
saxing <saxlh@foxmail.com>
2019-06-28 11:19:46 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/SwapNodesinPairs2.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/SwapNodesinPairs2.java
@@ -36,4 +36,45 @@ public class SwapNodesinPairs2 {
         return newHead;
     }
 
+
+
+    public static ListNode swapPairs3(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        ListNode newHead = head.next;
+        while (cur != null && cur.next != null){
+            ListNode temp = cur;
+            cur = cur.next;
+            temp.next = cur.next;
+            cur.next = temp;
+            cur = temp.next;
+            if (cur != null && cur.next != null){
+                temp.next = cur.next;
+            }
+
+        }
+
+        return newHead;
+    }
+
+
+    public static ListNode swapPairs2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        ListNode newHead = head.next;
+        while (cur != null && cur.next != null){
+            ListNode temp = cur;
+            cur = cur.next;
+            temp.next = cur.next;
+            cur.next = temp;
+            cur = temp.next;
+            if (cur != null && cur.next != null){
+                temp.next = cur.next;
+            }
+
+        }
+
+        return newHead;
+    }
+
 }
