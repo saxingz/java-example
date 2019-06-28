0c2f83edb2bd998e638c7e4f9b5cfe19965be8c0
saxing <saxlh@foxmail.com>
2019-06-28 18:54:38 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/Sum3.java
@@ -0,0 +1,52 @@
+package org.saxing.a.algorithm2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sum3 {
+
+    static int[] getArray(){
+        return new int[]{-1, 0, 1, 2, -1, -4};
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> lists = threeSum1(getArray());
+        System.out.println(lists);
+    }
+
+
+    public static List<List<Integer>> threeSum1(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        for (int i = 0; i < num.length - 2; i++) {
+            if (i > 0 && num[i] == num[i - 1]){
+                continue;
+            }
+
+            int j = i + i, k = num.length - 1;
+
+            while (j < k){
+                int candidate = num[i] + num[j] + num[k];
+                if (candidate <= 0){
+                    if (candidate == 0){
+                        result.add(Arrays.asList(num[i], num[j], num[k]));
+                    }
+                    ++j;
+                    while (j < k && num[j] == num[j - 1]){
+                        ++j;
+                    }
+                } else {
+                    --k;
+                    while (j < k && num[k] == num[k - 1]){
+                        --k;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
