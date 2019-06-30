a250ff76639c1b3c147d5f767b292deec4adbcbf
saxing <saxlh@foxmail.com>
2019-06-30 11:16:55 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/TaskScheduler.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/TaskScheduler.java
@@ -29,10 +29,30 @@ public class TaskScheduler {
         int result1 = leastInterval1(getArray(), 2);
         System.out.println(result1);
 
-
-        int result2 = leastInterval1(getArray(), 2);
+        int result2 = leastInterval2(getArray(), 2);
         System.out.println(result2);
+
+        int result3 = leastInterval3(getArray(), 2);
+        System.out.println(result3);
+
+
     }
+
+// ==================================================================================================
+// ==================================================================================================
+// ==================================================================================================
+
+    public static int leastInterval3(char[] tasks, int n) {
+        int[] c = new int[26];
+        for (char t : tasks){
+            c[t - 'A']++;
+        }
+        Arrays.sort(c);
+        int i = 25;
+        while (i >= 0 && c[i] == c[25]) i--;
+        return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
+    }
+
 
 
 // ==================================================================================================
