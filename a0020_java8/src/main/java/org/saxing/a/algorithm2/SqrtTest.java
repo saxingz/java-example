05c186547ec12a540eaca8745940733c1f5b6cc3
saxing <saxlh@foxmail.com>
2019-07-02 08:07:57 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/SqrtTest.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/SqrtTest.java
@@ -6,8 +6,14 @@ package org.saxing.a.algorithm2;
 public class SqrtTest {
 
     public static void main(String[] args) {
-        int res = mySqrt(8);
+
+        int res = mySqrt1(8);
         System.out.println(res);
+
+
+        int res2 = mySqrt2(8);
+        System.out.println(res2);
+
     }
 
 
@@ -15,7 +21,28 @@ public class SqrtTest {
 //==================================================================================================
 //==================================================================================================
 
-    public static int mySqrt(int x) {
+    public static int mySqrt2(int x) {
+        if (x == 0 || x == 1) return x;
+        int l = 0, r = x, res = 1;
+        while (l <= r){
+            int mid = (l + r) / 2;
+            if (mid == x / mid){
+                return mid;
+            } else if (mid > x / mid){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+                res = l;
+            }
+        }
+        return res;
+    }
+
+//==================================================================================================
+//==================================================================================================
+//==================================================================================================
+
+    public static int mySqrt1(int x) {
         if (x == 0 || x == 1) return x;
         int l = 0, r = x, res = 1;
         while (l <= r){
