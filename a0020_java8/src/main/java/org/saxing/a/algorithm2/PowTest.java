7a0dc86a269cd56dbcfe7fbf5f4ef0375d2ad21e
saxing <saxlh@foxmail.com>
2019-07-15 22:26:02 +0800

    algorithm

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/PowTest.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/PowTest.java
@@ -11,6 +11,15 @@ public class PowTest {
 
         double v2 = myPow2(2, 4);
         System.out.println(v2);
+
+        double v3 = myPow3(2, 4);
+        System.out.println(v2);
+    }
+
+    public static double myPow3(double x, int n) {
+        if (n == 0) return 1;
+        double res = myPow3(x, n / 2);
+        return (n & 1) == 0 ? res * res : n < 0 ? res * res * (1 / x) : res * res * x;
     }
 
     public static double myPow2(double x, int n) {
