0da3a6575b4248d05edc975e63b78714e2110481
saxing <saxlh@foxmail.com>
2019-07-16 23:12:12 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/WordSearchII.java
@@ -0,0 +1,29 @@
+package org.saxing.a.algorithm2;
+
+import java.util.List;
+
+/**
+ * leetcode 212
+ */
+public class WordSearchII {
+
+    public static String[] getWords(){
+        return new String[]{"oath","pea","eat","rain"};
+    }
+
+    public static char[][] getBoard(){
+        return new char[][] {
+            {'o','a','a','n'},
+            {'e','t','a','e'},
+            {'i','h','k','r'},
+            {'i','f','l','v'}
+        };
+    };
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
