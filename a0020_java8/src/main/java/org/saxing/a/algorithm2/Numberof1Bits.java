ee6954ee85022ad99d7aba87ae6d31b651cb3b43
saxing <saxlh@foxmail.com>
2019-07-17 23:30:59 +0800

    algorithm

---
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/Numberof1Bits.java
@@ -0,0 +1,22 @@
+package org.saxing.a.algorithm2;
+
+/**
+ * leetcode 191
+ */
+public class Numberof1Bits {
+
+    public static void main(String[] args) {
+        System.out.println(new Numberof1Bits().hammingWeight(9));
+    }
+
+    public int hammingWeight(int n) {
+        if (n == 0) return 0;
+        int count = 0;
+        while (n > 0){
+            count++;
+            n =  n & (n - 1);
+        }
+        return count;
+    }
+
+}
