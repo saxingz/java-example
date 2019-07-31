0e668cdb75568d48ebc97e2f46d875c4c450599a
saxing <saxlh@foxmail.com>
2019-07-31 10:03:56 +0800

    boot2 test

---
+++ a0031_boot2/src/main/java/org/saxing/boot2/A0031Boot2Application.java
@@ -0,0 +1,13 @@
+package org.saxing.boot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0031Boot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0031Boot2Application.class, args);
+    }
+
+}
