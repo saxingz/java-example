11b5e52f405c62cae2b4847b1fc898d5669b0be6
saxing <saxlh@foxmail.com>
2019-07-31 13:48:53 +0800

    boot2 test

---
+++ a0031_boot2/src/main/java/org/saxing/boot2/TestController.java
@@ -0,0 +1,19 @@
+package org.saxing.boot2;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/geta")
+    public String getA(){
+        String res = "a  " + new Random().nextFloat();
+        System.out.println(res);
+        return res;
+    }
+
+}
