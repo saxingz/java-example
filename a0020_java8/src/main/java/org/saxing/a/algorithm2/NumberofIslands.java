09bb2f72fa49e262898cb4f63e851fd4d991d2ce
saxing <saxlh@foxmail.com>
2019-09-15 22:52:48 +0800

    algorithm number of island

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/NumberofIslands.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/NumberofIslands.java
@@ -5,6 +5,38 @@ package org.saxing.a.algorithm2;
  */
 public class NumberofIslands {
 
+    public static void main(String[] args) {
+
+    }
+
+    int[][] distance = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        UnionFind200 uf = new UnionFind200(grid);
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1'){
+                    for (int[] d : distance){
+                        int x = i + d[0];
+                        int y = j + d[1];
+                        if (x >= 0 && x < rows && y >=0 && y < cols && grid[x][y] == '1'){
+                            int id1 = i * cols + j;
+                            int id2 = x * cols + y;
+                            uf.union(id1, id2);
+                        }
+                    }
+                }
+            }
+        }
+
+        return uf.count;
+    }
+
 
     class UnionFind200{
         int[] father;
