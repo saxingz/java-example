12f8fedf4e56880984fab8f1965142d5dcdf6298
saxing <saxlh@foxmail.com>
2019-09-22 13:47:22 +0800

    test data

---
+++ a0033_xiutest/src/main/java/org/saxing/xiutest/A0033XiutestApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.xiutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0033XiutestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0033XiutestApplication.class, args);
+    }
+
+}
