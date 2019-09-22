7e28a3c59e3591c212096a98c888a3adf17e6fc8
saxing <saxlh@foxmail.com>
2019-09-22 23:23:13 +0800

    algorithm lru

--- a0020_java8/src/main/java/org/saxing/a/algorithm2/LRUCache.java
+++ a0020_java8/src/main/java/org/saxing/a/algorithm2/LRUCache.java
@@ -31,6 +31,62 @@ public class LRUCache {
         head.post = node;
     }
 
+    /**
+     * remove node
+     *
+     * @param node
+     */
+    private void removeNode(DLinkedNode node){
+        DLinkedNode pre = node.pre;
+        DLinkedNode post = node.post;
+
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    /**
+     * mode to head
+     * @param node
+     */
+    private void moveToHead(DLinkedNode node){
+        this.removeNode(node);
+        this.addNode(node);
+    }
+
+    /**
+     * pop tail
+     * @return
+     */
+    private DLinkedNode popTail(){
+        DLinkedNode res = tail.pre;
+        this.removeNode(res);
+        return res;
+    }
+
+    public LRUCache(int capacity) {
+        this.count = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        head.pre = null;
+
+        tail = new DLinkedNode();
+        tail.post = null;
+
+        head.post = tail;
+        tail.pre = head;
+    }
+
+    public int get(int key){
+        DLinkedNode node = cache.get(key);
+        if (node == null){
+            return -1;
+        }
+
+        this.moveToHead(node);
+        return node.value;
+    }
+
 
 
 
