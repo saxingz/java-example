6ec89e70919f4d91be490fd6273edb09746fa17f
saxing <saxlh@foxmail.com>
2019-10-14 16:31:31 +0800

    validator

---
+++ a0035_service-validator/aa0035_01_validate-demo/src/main/java/org/saxing/aa0035_01_validatedemo/Aa003501ValidateDemoApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.aa0035_01_validatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa003501ValidateDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aa003501ValidateDemoApplication.class, args);
+    }
+
+}
