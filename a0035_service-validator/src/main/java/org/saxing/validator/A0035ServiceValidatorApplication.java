1f58f1899b122b6f20d7df07d0165a7cdeceb044
saxing <saxlh@foxmail.com>
2019-10-14 16:20:00 +0800

    validator

---
+++ a0035_service-validator/src/main/java/org/saxing/validator/A0035ServiceValidatorApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0035ServiceValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0035ServiceValidatorApplication.class, args);
+    }
+
+}
