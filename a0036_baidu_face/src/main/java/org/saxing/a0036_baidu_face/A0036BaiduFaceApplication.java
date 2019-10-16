dc48a58fb8d7b4d0ef6e2ba4d0c4ae6163f59a3d
saxing <saxlh@foxmail.com>
2019-10-16 14:34:57 +0800

    baidu face

---
+++ a0036_baidu_face/src/main/java/org/saxing/a0036_baidu_face/A0036BaiduFaceApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0036_baidu_face;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0036BaiduFaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0036BaiduFaceApplication.class, args);
+    }
+
+}
---
+++ a0036_baidu_face/src/test/java/org/saxing/a0036_baidu_face/A0036BaiduFaceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.saxing.a0036_baidu_face;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A0036BaiduFaceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
