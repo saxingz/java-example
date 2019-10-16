fc543ed0171d95f85f50b7cbc24c1fc9342f9e8a
saxing <saxlh@foxmail.com>
2019-10-16 19:24:23 +0800

    baidu face

---
+++ a0036_baidu_face/src/main/java/org/saxing/a0036_baidu_face/BaiduFaceExample.java
@@ -0,0 +1,57 @@
+package org.saxing.a0036_baidu_face;
+
+import com.baidu.aip.face.AipFace;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+
+public class BaiduFaceExample {
+
+    //设置APPID/AK/SK
+    public static final String APP_ID = "17539254";
+    public static final String API_KEY = "zFr5OLa3PbMwN2qg9iNdq1GP";
+    public static final String SECRET_KEY = "1pQg3II3RGcr2zi4CGL4dS6BNQtHoUot";
+
+    public static void main(String[] args) throws IOException {
+
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("max_face_num", "10");
+        options.put("match_threshold", "70");
+        options.put("quality_control", "HIGH");
+        options.put("liveness_control", "NONE");
+        options.put("max_user_num", "1");
+
+
+        // 初始化一个AipFace
+        AipFace client = new AipFace(APP_ID, API_KEY, SECRET_KEY);
+
+        // 可选：设置网络连接参数
+        client.setConnectionTimeoutInMillis(2000);
+        client.setSocketTimeoutInMillis(60000);
+
+        // 可选：设置代理服务器地址, http和socket二选一，或者均不设置
+//        client.setHttpProxy("proxy_host", proxy_port);  // 设置http代理
+//        client.setSocketProxy("proxy_host", proxy_port);  // 设置socket代理
+
+
+        byte[] fileContent = FileUtils.readFileToByteArray(new File("D:\\temp\\face\\test3-half-face.jpg"));
+
+        // 调用接口
+//        String image = "取决于image_type参数，传入BASE64字符串或URL字符串或FACE_TOKEN字符串";
+        String image = Base64.getEncoder().encodeToString(fileContent);
+        String imageType = "BASE64";
+
+        // 人脸检测
+//        JSONObject res = client.detect(image, imageType, options);
+        String groupIdList = "group1";
+        JSONObject res = client.multiSearch(image, imageType, groupIdList, options);
+        System.out.println(res.toString(2));
+
+    }
+
+}
