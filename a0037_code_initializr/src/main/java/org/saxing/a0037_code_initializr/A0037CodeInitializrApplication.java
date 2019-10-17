9408a20331d6a008f88226666ef4092c8dde777a
saxing <saxlh@foxmail.com>
2019-10-17 13:17:23 +0800

    initializr

---
+++ a0037_code_initializr/src/main/java/org/saxing/a0037_code_initializr/A0037CodeInitializrApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0037_code_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0037CodeInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0037CodeInitializrApplication.class, args);
+    }
+
+}
---
+++ a0037_code_initializr/src/test/java/org/saxing/a0037_code_initializr/A0037CodeInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0037_code_initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0037CodeInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
