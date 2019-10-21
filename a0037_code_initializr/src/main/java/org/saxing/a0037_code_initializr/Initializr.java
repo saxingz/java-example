ccd4112be1bbe0b0c346cd1012a6f3ad7b68e9bf
saxing <saxlh@foxmail.com>
2019-10-21 09:33:22 +0800

    initializr

---
+++ a0037_code_initializr/src/main/java/org/saxing/a0037_code_initializr/Initializr.java
@@ -0,0 +1,89 @@
+package org.saxing.a0037_code_initializr;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Initializr {
+
+    public static final String SOURCE_PATH = "D:\\code\\au\\au-initializr";
+    public static final String DES_PATH = "D:\\code\\au\\au-initializr-destination";
+    public static final String VARIABLE_PROJECT_NAME = "\\{project-name\\}";
+    public static final String VARIABLE_ROOT_PACKAGE_NAME = "\\{root-package-name\\}";
+    public static final String VARIABLE_DESCRIPTION = "\\{project-description\\}";
+    public static final String VARIABLE_README_MD = "\\{readme.md\\}";
+    public static final String VARIABLE_APOLLO_APPID = "\\{apollo.appid\\}";
+
+    // 待输入
+    public static final String PROJECT_NAME = "file";
+    public static final String ROOT_PACKAGE_NAME = "file";
+    public static final String PROJECT_DESCRIPTION = "file";
+    public static final String PROJECT_README = "文件系统";
+    public static final String PROJECT_APOLLO_APPID = "100003";
+
+    public static void main(String[] args) {
+        getFile(SOURCE_PATH);
+    }
+
+    public static void getFile(String path) {
+        // File对象 可以是文件或者目录
+        File file = new File(path);
+        File[] files = file.listFiles();
+        if (files == null){
+            return;
+        }
+
+        for (File curr : files) {
+            if (curr.isFile()) {
+                System.out.println("^^  " + curr.getName());
+                String filePath = curr.getPath();
+                changeFile(filePath, getDestinationPath(filePath));
+            } else if (curr.isDirectory()) {
+                String directoryPath = curr.getPath();
+                File temp = new File(getDestinationPath(directoryPath));
+                boolean mkdirsRes = temp.mkdirs();
+                if (!mkdirsRes) {
+                    throw new RuntimeException("directory error");
+                }
+                getFile(curr.getPath());
+            }
+        }
+    }
+
+    public static String getDestinationPath(String originPath){
+        originPath = originPath
+                .replaceAll(VARIABLE_PROJECT_NAME, PROJECT_NAME)
+                .replaceAll(VARIABLE_ROOT_PACKAGE_NAME, ROOT_PACKAGE_NAME)
+                .replaceAll(VARIABLE_DESCRIPTION, PROJECT_DESCRIPTION)
+                .replaceAll(VARIABLE_README_MD, PROJECT_README)
+                .replaceAll(VARIABLE_APOLLO_APPID, PROJECT_APOLLO_APPID);
+        return DES_PATH + originPath.replaceAll(SOURCE_PATH.replaceAll("\\\\", "\\\\\\\\"), "");
+    }
+
+    public static void changeFile(String source, String destination) {
+        String line;
+        try (
+                BufferedReader reader = Files.newBufferedReader(Paths.get(source), StandardCharsets.UTF_8);
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get(destination), StandardCharsets.UTF_8);
+            ){
+            while ((line = reader.readLine()) != null) {
+                line = line
+                        .replaceAll(VARIABLE_PROJECT_NAME, PROJECT_NAME)
+                        .replaceAll(VARIABLE_ROOT_PACKAGE_NAME, ROOT_PACKAGE_NAME)
+                        .replaceAll(VARIABLE_DESCRIPTION, PROJECT_DESCRIPTION)
+                        .replaceAll(VARIABLE_README_MD, PROJECT_README)
+                        .replaceAll(VARIABLE_APOLLO_APPID, PROJECT_APOLLO_APPID);
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
