24891a161ad0a1163610e29fa25bc9c5310cab17
saxing <saxlh@foxmail.com>
2019-11-01 17:38:08 +0800

    baidu face test

--- a0036_baidu_face/src/main/java/org/saxing/a0036_baidu_face/BaiduFaceExample.java
+++ a0036_baidu_face/src/main/java/org/saxing/a0036_baidu_face/BaiduFaceExample.java
@@ -18,6 +18,14 @@ public class BaiduFaceExample {
 
     public static void main(String[] args) throws IOException {
 
+        for (int i = 0; i < 12; i++) {
+            byte[] fileContent = FileUtils.readFileToByteArray(new File("D:\\temp\\ceshi-face\\"+(i+1)+".jpg"));
+            System.out.println("第 " + (i + 1) + "张");
+            test(fileContent);
+        }
+    }
+
+    public static void test(byte[] fileContent){
         // 传入可选参数调用接口
         HashMap<String, String> options = new HashMap<String, String>();
         options.put("max_face_num", "10");
@@ -35,11 +43,11 @@ public class BaiduFaceExample {
         client.setSocketTimeoutInMillis(60000);
 
         // 可选：设置代理服务器地址, http和socket二选一，或者均不设置
-        client.setHttpProxy("192.168.50.2", 3128);  // 设置http代理
+//        client.setHttpProxy("192.168.50.2", 3128);  // 设置http代理
 //        client.setSocketProxy("proxy_host", proxy_port);  // 设置socket代理
 
 
-        byte[] fileContent = FileUtils.readFileToByteArray(new File("D:\\temp\\temp_1day\\face\\test1.jpg"));
+
 
         // 调用接口
 //        String image = "取决于image_type参数，传入BASE64字符串或URL字符串或FACE_TOKEN字符串";
@@ -50,8 +58,16 @@ public class BaiduFaceExample {
 //        JSONObject res = client.detect(image, imageType, options);
         String groupIdList = "group1";
         JSONObject res = client.multiSearch(image, imageType, groupIdList, options);
+        if (res.toString().contains("songsenhai")){
+            System.out.println("========== true =============");
+        }
+        if (res.toString().contains("niumingliang")){
+            System.out.println("------- niumingliang ------------");
+        }
+        if (res.toString().contains("xushuai")){
+            System.out.println("#########################");
+        }
         System.out.println(res.toString(2));
-
     }
 
 }
