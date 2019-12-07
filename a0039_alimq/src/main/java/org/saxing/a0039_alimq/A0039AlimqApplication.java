198de01381020e7f7af476b9206be5ad709b207c
saxing <saxlh@foxmail.com>
2019-12-07 13:56:23 +0800

    ali mq test

---
+++ a0039_alimq/src/main/java/org/saxing/a0039_alimq/A0039AlimqApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0039_alimq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0039AlimqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0039AlimqApplication.class, args);
+    }
+
+}
---
+++ a0039_alimq/src/test/java/org/saxing/a0039_alimq/A0039AlimqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0039_alimq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0039AlimqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
