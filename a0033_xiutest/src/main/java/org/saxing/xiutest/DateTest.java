f8e349904c85cae06df2c844784da598b654fae2
saxing <saxlh@foxmail.com>
2020-02-02 21:00:06 +0800

    test

---
+++ a0033_xiutest/src/main/java/org/saxing/xiutest/DateTest.java
@@ -0,0 +1,14 @@
+package org.saxing.xiutest;
+
+import java.util.Date;
+
+public class DateTest {
+
+    public static void main(String[] args) {
+        Date nowDate = new Date();
+        // 1578281222521
+        // 1578281215599
+        System.out.println(nowDate.getTime());
+    }
+
+}
