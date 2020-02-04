12fd82c2947cf2bd733fb4b99b5ee6d127405daf
saxing <saxlh@foxmail.com>
2020-02-04 23:31:40 +0800

    test

---
+++ a0033_xiutest/src/main/java/org/saxing/xiutest/JsonTest.java
@@ -0,0 +1,12 @@
+package org.saxing.xiutest;
+
+public class JsonTest {
+
+    public static void main(String[] args) {
+        String str = "{\"result\":{\"face_num\":1,\"face_list\":[{\"angle\":{\"roll\":-3.21,\"pitch\":14.67,\"yaw\":0.67},\"face_token\":\"b26bbcd559f6d7a587ce03ba4838e123\",\"location\":{\"top\":350.11,\"left\":107.07,\"rotation\":-4,\"width\":453,\"height\":461},\"face_probability\":1,\"age\":24}]},\"log_id\":3525556575152,\"error_msg\":\"SUCCESS\",\"cached\":0,\"error_code\":0,\"timestamp\":1578656013}";
+
+
+        System.out.println(str);
+    }
+
+}
