a9ec641fb809199a943ae86d41316e056f25ba77
saxing <saxlh@foxmail.com>
2020-02-05 20:40:06 +0800

    test

--- a0037_code_initializr/src/main/java/org/saxing/a0037_code_initializr/ProjectList.java
+++ a0037_code_initializr/src/main/java/org/saxing/a0037_code_initializr/ProjectList.java
@@ -19,6 +19,20 @@ public class ProjectList {
     public static final String A100013 = "test";
     public static final String A100014 = "moment";
     public static final String A100015 = "syncdb";
+    public static final String A100016 = "statistic";
+    // 未上apollo
+    public static final String A100017 = "coolfile";
+    public static final String A100018 = "boss";
+    public static final String A100019 = "finance";
+    public static final String A100020 = "log";
+    public static final String A100021 = "javaphpcms";
+
+
+    // MQ list group
+    public static final String GID_001_iam = "登录日志表 GID_accesslog_by_iam  登录日志消息  接收者： iam";
+    public static final String GID_002_attendance = "GID_workflow_attendance_by_attendance  审批考勤消息  接收者：考勤项目";
+    public static final String GID_003_album = "GID_workflow_attendance_by_attendance  审批考勤消息  接收者：考勤项目";
+    public static final String GID_004_kidarch = "接收者：7kid基础体系项目";
 
 
 }
