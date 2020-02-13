34cb971ad95b363ac42988f55d666fe23ca90ebb
saxing <saxlh@foxmail.com>
2020-02-13 20:41:55 +0800

    test

--- a0037_code_initializr/src/main/java/org/saxing/a0037_code_initializr/Initializr.java
+++ a0037_code_initializr/src/main/java/org/saxing/a0037_code_initializr/Initializr.java
@@ -33,12 +33,12 @@ public class Initializr {
      * see the {@link ProjectList}
      */
     // 待输入
-    public static final String PROJECT_NAME = "statistic";
-    public static final String PROJECT_ROOT_PACKAGE_NAME = "statistic";
+    public static final String PROJECT_NAME = "javaphpcms";
+    public static final String PROJECT_ROOT_PACKAGE_NAME = "javaphpcms";
     public static final String PROJECT_DATASOURCE_NAME = PROJECT_ROOT_PACKAGE_NAME;
-    public static final String PROJECT_DESCRIPTION = "statistic system";
-    public static final String PROJECT_README = "statistic";
-    public static final String PROJECT_APOLLO_APPID = "100016";
+    public static final String PROJECT_DESCRIPTION = "javaphpcms";
+    public static final String PROJECT_README = "javaphpcms";
+    public static final String PROJECT_APOLLO_APPID = "100021";
 
     public static void main(String[] args) {
         getFile(SOURCE_PATH);
