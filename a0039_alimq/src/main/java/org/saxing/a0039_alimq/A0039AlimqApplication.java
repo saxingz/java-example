3fceecdd6c243ff3a84ae9325735d836fef59a40
saxing <saxlh@foxmail.com>
2020-03-21 23:45:41 +0800

    test

--- a0039_alimq/src/main/java/org/saxing/a0039_alimq/A0039AlimqApplication.java
+++ a0039_alimq/src/main/java/org/saxing/a0039_alimq/A0039AlimqApplication.java
@@ -3,6 +3,12 @@ package org.saxing.a0039_alimq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * mq test
+ *
+ * @author saxing 2020/3/21 23:45
+ *
+ */
 @SpringBootApplication
 public class A0039AlimqApplication {
 
