e09fb8ff183be69302a3a11b9d21628d2da5777f
saxing <saxlh@foxmail.com>
2020-04-05 18:08:26 +0800

    codota test

---
+++ a0033_xiutest/src/main/java/org/saxing/xiutest/CodotaTest.java
@@ -0,0 +1,18 @@
+package org.saxing.xiutest;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+
+/**
+ * codota test
+ *
+ * @author saxing 2020/4/5 18:08
+ */
+public class CodotaTest {
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new StringReader("fff"));
+    }
+
+}
