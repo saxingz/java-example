41f57180c26c29b7c9ca3f92758b72621d3733a6
saxing <saxlh@foxmail.com>
2020-04-11 21:18:40 +0800

    feat: sensitive

---
+++ a0040_sensitive/src/main/java/org/saxing/a0040_sensitive/A0040SensitiveApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0040_sensitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0040SensitiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0040SensitiveApplication.class, args);
+    }
+
+}
---
+++ a0040_sensitive/src/test/java/org/saxing/a0040_sensitive/A0040SensitiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0040_sensitive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0040SensitiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
