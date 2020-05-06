d5f3bd2dc4b6ab3aae85b60fc92b9dcec620142c
saxing <saxlh@foxmail.com>
2020-05-06 19:08:50 +0800

    feat: nio

--- a0020_java8/src/main/java/org/saxing/d_queue/nio/TestBuffer.java
+++ a0020_java8/src/main/java/org/saxing/d_queue/nio/TestBuffer.java
@@ -9,7 +9,25 @@ import java.nio.IntBuffer;
  */
 public class TestBuffer {
 
+
     public static void main(String[] args) {
+
+        // 2 wrap方法使用
+
+        //  wrap方法会包裹一个数组: 一般这种用法不会先初始化缓存对象的长度，因为没有意义，最后还会被wrap所包裹的数组覆盖掉。
+        //  并且wrap方法修改缓冲区对象的时候，数组本身也会跟着发生变化。
+        int[] arr = new int[]{1,2,5};
+        IntBuffer buf1 = IntBuffer.wrap(arr);
+        System.out.println(buf1);
+
+        IntBuffer buf2 = IntBuffer.wrap(arr, 0 , 2);
+        //这样使用表示容量为数组arr的长度，但是可操作的元素只有实际进入缓存区的元素长度
+        System.out.println(buf2);
+
+
+    }
+
+    public static void main2(String[] args) {
         IntBuffer buf = IntBuffer.allocate(10);
 
         buf.put(13);// position位置：0 - > 1
