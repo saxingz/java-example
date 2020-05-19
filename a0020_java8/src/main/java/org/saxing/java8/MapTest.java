7667c03a989f5b18e1e3c29c9405534ae63de3b7
saxing <saxlh@foxmail.com>
2020-05-19 14:40:29 +0800

    feat： char test

--- a0020_java8/src/main/java/org/saxing/java8/MapTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/MapTest.java
@@ -1,7 +1,10 @@
 package org.saxing.java8;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author saxing 2020/5/18 20:23
@@ -9,6 +12,28 @@ import java.util.Map;
 public class MapTest {
 
     public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test2(){
+        String uuid = UUID.randomUUID().toString();
+
+        System.out.println(uuid);
+        boolean isAlphanumeric = StringUtils.isAlphanumeric(uuid);
+        System.out.println(isAlphanumeric);
+
+        uuid = uuid.replaceAll("-", "");
+        System.out.println(uuid);
+        isAlphanumeric = StringUtils.isAlphanumeric(uuid);
+        System.out.println(isAlphanumeric);
+
+        uuid += " ";
+        System.out.println(uuid);
+        isAlphanumeric = StringUtils.isAlphanumeric(uuid);
+        System.out.println(isAlphanumeric);
+    }
+
+    public static void main2(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("a", "a1");
         map.put("b", "b1");
