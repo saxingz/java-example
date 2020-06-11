9b1a1ed997b75712a2d56a34740bb2e3d5c1089a
saxing <saxlh@foxmail.com>
2020-06-11 19:43:40 +0800

    feat: test

---
+++ a0020_java8/src/main/java/org/saxing/java8/FutureTest.java
@@ -0,0 +1,24 @@
+package org.saxing.java8;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class FutureTest {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Callable<String> call = () -> {
+            Thread.sleep(3000);
+            return "abcdefg";
+        };
+        FutureTask<String> futureTask = new FutureTask<>(call);
+        new Thread(futureTask).start();
+
+        Thread.sleep(1000);
+        System.out.println("aaaa");
+
+        String s = futureTask.get();
+        System.out.println(s);
+    }
+
+}
