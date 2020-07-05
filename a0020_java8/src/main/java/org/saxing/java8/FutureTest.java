bf73464d48db6fdea97865a08ef38ce92c1c2b05
saxing <saxlh@foxmail.com>
2020-07-05 23:41:58 +0800

    feat: test

--- a0020_java8/src/main/java/org/saxing/java8/FutureTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/FutureTest.java
@@ -4,6 +4,11 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
+/**
+ * future test
+ *
+ * @author saxing 2020/7/5 23:41
+ */
 public class FutureTest {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
