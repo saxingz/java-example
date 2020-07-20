3b4f352c000c58ee016dd37d1c8fdb6e1c5ead7a
saxing <saxlh@foxmail.com>
2020-07-20 09:26:10 +0800

    feat: test

--- a0020_java8/src/main/java/org/saxing/java8/FutureTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/FutureTest.java
@@ -1,6 +1,7 @@
 package org.saxing.java8;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
@@ -11,7 +12,32 @@ import java.util.concurrent.FutureTask;
  */
 public class FutureTest {
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrentHashMap<Long, Integer> teacherVisibleNum = new ConcurrentHashMap<>();
+        Callable<Void> callable = () -> {
+            for (int i = 0; i < 1000; i++) {
+                teacherVisibleNum.put(i + 200L, i);
+                System.out.println("add ...");
+            }
+            return null;
+        };
+        FutureTask<Void> futureTask = new FutureTask<>(callable);
+        new Thread(futureTask).start();
+        Thread.sleep(500);
+
+        try {
+            futureTask.get();
+            System.out.println(teacherVisibleNum);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("success");
+
+    }
+
+    public static void main2(String[] args) throws InterruptedException, ExecutionException {
         Callable<String> call = () -> {
             Thread.sleep(3000);
             return "abcdefg";
