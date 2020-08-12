fe952a70e5ef376cdf8e3306e592bbdeb55c3606
saxing <saxlh@foxmail.com>
2020-08-12 22:53:17 +0800

    feat: test

--- a0020_java8/src/main/java/org/saxing/java8/TimeTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/TimeTest.java
@@ -1,5 +1,7 @@
 package org.saxing.java8;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Date;
 
 public class TimeTest {
@@ -7,14 +9,26 @@ public class TimeTest {
     public static void main(String[] args) {
 
         Long current = System.currentTimeMillis();
-        Long dateTime = new Date().getTime();
-
+        // 1596789092056
         System.out.println(current);
+
+
+        Long dateTime = new Date().getTime();
+        // 1596789092057
         System.out.println(dateTime);
 
         // 1592536526
+        //
         // 1596777250822
 
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        long currentSecond = localDateTime.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
+        LocalDateTime past8Minutes = localDateTime.minusMinutes(8);
+        long pastTimeSecond = past8Minutes.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
+
+        System.out.println(currentSecond);
+        System.out.println(pastTimeSecond);
     }
 
 }
