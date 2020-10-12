b28344999cab64f9a5494a7e8feedcd7fa440ec6
saxing <saxlh@foxmail.com>
2020-10-12 15:18:27 +0800

    feat: test

---
+++ a0020_java8/src/main/java/org/saxing/java8/CollectTest.java
@@ -0,0 +1,41 @@
+package org.saxing.java8;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * @author saxing 2020/10/12 15:04
+ */
+public class CollectTest {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    class Book {
+        private String name;
+        private int releaseYear;
+        private String isbn;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private void testAdd(){
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("The Fellowship of the Ring", 1954, "0395489318"));
+        bookList.add(new Book("The Two Towers", 1954, "0345339711"));
+        bookList.add(new Book("The Return of the King", 1955, "0618129111"));
+
+
+    }
+
+}
