51768bd071bfae02f8d0d087bdf0c64a274cb762
saxing <saxlh@foxmail.com>
2020-10-18 16:34:52 +0800

    feat: gen java code

--- a0041_wemedia/src/main/java/org/saxing/a0041_wemedia/A0041WemediaApplication.java
+++ a0041_wemedia/src/main/java/org/saxing/a0041_wemedia/A0041WemediaApplication.java
@@ -1,8 +1,15 @@
 package org.saxing.a0041_wemedia;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * wemedia
+ *
+ * @author saxing 2020/10/18 13:25
+ */
+@MapperScan("org.saxing.a0041_wemedia.dao")
 @SpringBootApplication
 public class A0041WemediaApplication {
 
---
+++ a0041_wemedia/src/test/java/org/saxing/a0041_wemedia/GenCode.java
@@ -0,0 +1,131 @@
+package org.saxing.a0041_wemedia;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class GenCode {
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("saxing");
+        gc.setOpen(false);
+        gc.setFileOverride(false);
+        gc.setIdType(IdType.AUTO);
+        gc.setSwagger2(true); // 实体属性 Swagger2 注解
+        gc.setDateType(DateType.ONLY_DATE);
+        gc.setEntityName("%sDO");
+        gc.setXmlName("%sMapper");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3500/wemedia?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8&useSSL=false");
+//         dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("test");
+        dsc.setPassword("test");
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName("");
+        pc.setParent("org.saxing.a0041_wemedia");
+        pc.setEntity("domain.entity");
+        pc.setService("logic");
+        pc.setServiceImpl("logic.impl");
+        pc.setController("controller");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        // 自定义输出配置
+        // 如果模板引擎是 velocity
+        String templatePath = "/templates/mapper.xml.vm";
+        List<FileOutConfig> focList = new ArrayList<>();
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        //此处设置为null，就不会再java下创建xml的文件夹了
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setLogicDeleteFieldName("is_deleted");
+        TableFill createdTime = new TableFill("created_time", FieldFill.INSERT);
+        TableFill updatedTime = new TableFill("created_time", FieldFill.INSERT_UPDATE);
+        strategy.setTableFillList(Arrays.asList(createdTime, updatedTime));
+        // 写于父类中的公共字段
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+//        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.execute();
+    }
+
+}
