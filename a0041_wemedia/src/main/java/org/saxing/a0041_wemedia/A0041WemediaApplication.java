7647e71be2473409cbd9ca392762b3d83ce42850
saxing <saxlh@foxmail.com>
2020-10-18 13:24:31 +0800

    feat: wemedia first commit

---
+++ a0041_wemedia/src/main/java/org/saxing/a0041_wemedia/A0041WemediaApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0041_wemedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0041WemediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0041WemediaApplication.class, args);
+    }
+
+}
---
+++ a0041_wemedia/src/test/java/org/saxing/a0041_wemedia/A0041WemediaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0041_wemedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0041WemediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
