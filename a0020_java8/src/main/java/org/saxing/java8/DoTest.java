6be54cd1c8e3603e2809e9ea84eb1850e88e8022
saxing <saxlh@foxmail.com>
2020-10-22 17:59:34 +0800

    test

---
+++ a0020_java8/src/main/java/org/saxing/java8/DoTest.java
@@ -0,0 +1,11 @@
+package org.saxing.java8;
+
+public class DoTest {
+
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("fffff");
+        i = 10;
+    }
+
+}
