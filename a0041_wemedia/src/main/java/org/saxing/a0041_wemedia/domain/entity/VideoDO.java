a8684795aef1d08d438bb173cf9cd74263426739
saxing <saxlh@foxmail.com>
2020-10-22 19:20:47 +0800

    feat: table fill

--- a0041_wemedia/src/main/java/org/saxing/a0041_wemedia/domain/entity/VideoDO.java
+++ a0041_wemedia/src/main/java/org/saxing/a0041_wemedia/domain/entity/VideoDO.java
@@ -74,11 +74,9 @@ public class VideoDO implements Serializable {
     private Boolean isDeleted;
 
     @ApiModelProperty(value = "创建时间")
-    @TableField(fill = FieldFill.INSERT)
     private Date createdTime;
 
     @ApiModelProperty(value = "修改时间")
-    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updatedTime;
 
 
