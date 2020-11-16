ecc7fc627d4086169bd204e44a63e86b2a4137c2
saxing <saxlh@foxmail.com>
2020-11-16 22:36:29 +0800

    feat: think in spring

--- a0042_xiaomage_lesson/java-beans-demo/src/main/java/org/saxing/BeanInfoDemo.java
+++ a0042_xiaomage_lesson/java-beans-demo/src/main/java/org/saxing/BeanInfoDemo.java
@@ -3,6 +3,7 @@ package org.saxing;
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
+import java.beans.PropertyEditorSupport;
 import java.util.stream.Stream;
 
 /**
@@ -16,8 +17,27 @@ public class BeanInfoDemo {
     public static void main(String[] args) throws IntrospectionException {
         BeanInfo beanInfo = Introspector.getBeanInfo(Person.class, Object.class);
 
-        Stream.of(beanInfo.getPropertyDescriptors()).forEach(System.out::println);
+        Stream.of(beanInfo.getPropertyDescriptors()).forEach(
+                propertyDescriptor -> {
+                    Class<?> propertyType = propertyDescriptor.getPropertyType();
+                    System.out.println(propertyType);
+                    String propertyName = propertyDescriptor.getName();
+                    if ("age".equals(propertyName)) {
+                        propertyDescriptor.setPropertyEditorClass(StringToIntegerPropertyEditor.class);
+//                        propertyDescriptor.createPropertyEditor();
+                    }
+                }
+        );
 
+    }
+
+    static class StringToIntegerPropertyEditor extends PropertyEditorSupport {
+
+        @Override
+        public void setAsText(String text) throws IllegalArgumentException {
+            Integer value = Integer.valueOf(text);
+            setValue(value);
+        }
     }
 
 }
