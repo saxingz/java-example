c04af2e2e74e7e7bd80630167c9236fbcc9c70d4
saxing <saxlh@foxmail.com>
2020-11-19 22:59:40 +0800

    feat: think in spring

--- a0042_xiaomage_lesson/think-in-spring/spring-bean/src/main/java/org/saxing/thinking/in/spring/bean/definition/BeanInstantiationDemo.java
+++ a0042_xiaomage_lesson/think-in-spring/spring-bean/src/main/java/org/saxing/thinking/in/spring/bean/definition/BeanInstantiationDemo.java
@@ -14,7 +14,11 @@ public class BeanInstantiationDemo {
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/bean-instantiation-context.xml");
         User user = beanFactory.getBean("user-by-static-method", User.class);
+        User userByInstanceMethod = beanFactory.getBean("user-by-instance-method", User.class);
+
         System.out.println(user);
+        System.out.println(userByInstanceMethod);
+        System.out.println(userByInstanceMethod == user);
     }
 
 }
