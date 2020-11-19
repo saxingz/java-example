5d8c5cd79973e6cbd47e13caa24a1a2df967d3d7
saxing <saxlh@foxmail.com>
2020-11-19 23:09:34 +0800

    feat: think in spring

---
+++ a0042_xiaomage_lesson/think-in-spring/spring-bean/src/main/java/org/saxing/thinking/in/spring/bean/definition/SpecialBeanInstantiationDemo.java
@@ -0,0 +1,33 @@
+package org.saxing.thinking.in.spring.bean.definition;
+
+import org.saxing.thinking.in.spring.bean.factory.UserFactory;
+import org.saxing.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * BeanInstantiationDemo
+ *
+ * @author saxing 2020/11/19 22:39
+ */
+public class SpecialBeanInstantiationDemo {
+
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/special-bean-instantiation-context.xml");
+
+        demoServiceLoader();
+    }
+
+    public static void demoServiceLoader() {
+        ServiceLoader<UserFactory> serviceLoader = ServiceLoader.load(UserFactory.class, Thread.currentThread().getContextClassLoader());
+        Iterator<UserFactory> iterator = serviceLoader.iterator();
+        while (iterator.hasNext()) {
+            UserFactory userFactory = iterator.next();
+            System.out.println(userFactory.createUser());
+        }
+    }
+
+}
