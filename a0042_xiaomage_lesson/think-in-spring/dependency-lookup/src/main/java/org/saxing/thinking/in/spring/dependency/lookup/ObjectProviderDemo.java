ce3f74b1d404b53a140b9046320593d539aaee84
saxing <saxlh@foxmail.com>
2020-11-22 15:28:02 +0800

    feat: think in spring

--- a0042_xiaomage_lesson/think-in-spring/dependency-lookup/src/main/java/org/saxing/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
+++ a0042_xiaomage_lesson/think-in-spring/dependency-lookup/src/main/java/org/saxing/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
@@ -1,8 +1,10 @@
 package org.saxing.thinking.in.spring.dependency.lookup;
 
+import org.saxing.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * ObjectProviderDemo
@@ -17,13 +19,36 @@ public class ObjectProviderDemo {
         
         applicationContext.refresh();
         lookupByObjectProvider(applicationContext);
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
         
         applicationContext.close();
     }
 
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+//        Iterable<String> stringIterable = objectProvider;
+//        for (String string : stringIterable) {
+//            System.out.println(string);
+//        }
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        User user = userObjectProvider.getIfAvailable(User::createUser);
+        System.out.println("当前 User 对象： " + user);
+    }
+
     @Bean
+    @Primary
     public String helloWorld() { // 默认情况下，方法名为bean名称
         return "Hello, World";
+    }
+
+    @Bean
+    public String message() {
+        return "Message";
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
