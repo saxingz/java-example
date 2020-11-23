d3ad9ba416b7dccdb8647f5d3df076b602b7951b
saxing <saxlh@foxmail.com>
2020-11-23 22:38:21 +0800

    feat: think in spring

---
+++ a0042_xiaomage_lesson/think-in-spring/dependency-injection/src/main/java/org/saxing/thinking/in/spring/ioc/dependency/injection/AwareInterfaceDependencyInjectionDemo.java
@@ -0,0 +1,43 @@
+package org.saxing.thinking.in.spring.ioc.dependency.injection;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * AwareInterfaceDependencyInjectionDemo
+ *
+ * @author saxing 2020/11/22 20:49
+ */
+public class AwareInterfaceDependencyInjectionDemo implements BeanFactoryAware, ApplicationContextAware {
+
+    // static仅演示
+    private static BeanFactory beanFactory;
+    private static ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AwareInterfaceDependencyInjectionDemo.class);
+        context.refresh();
+
+        System.out.println(beanFactory == context.getBeanFactory());
+        System.out.println(applicationContext == context);
+
+        context.close();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        AwareInterfaceDependencyInjectionDemo.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        AwareInterfaceDependencyInjectionDemo.applicationContext = applicationContext;
+    }
+}
