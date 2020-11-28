8939be7b8f328ed68cc6ee644cca2911d0871865
saxing <saxlh@foxmail.com>
2020-11-28 18:36:33 +0800

    feat: think in spring

---
+++ a0042_xiaomage_lesson/think-in-spring/dependency-injection/src/main/java/org/saxing/thinking/in/spring/ioc/dependency/injection/LazyAnnotationDependencyInjectionDemo.java
@@ -0,0 +1,51 @@
+package org.saxing.thinking.in.spring.ioc.dependency.injection;
+
+import org.saxing.thinking.in.spring.ioc.dependency.injection.annotation.UserGroup;
+import org.saxing.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * {@link org.springframework.beans.factory.ObjectProvider}
+ *
+ * @author saxing 2020/11/27 23:39
+ */
+public class LazyAnnotationDependencyInjectionDemo {
+
+    @Autowired
+    private User user;
+
+    @Autowired
+    private ObjectProvider<User> userObjectProvider;
+
+    @Autowired
+    private ObjectFactory<Set<User>> userObjectFactory;
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(LazyAnnotationDependencyInjectionDemo.class);
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(applicationContext);
+        String xmlResourcePath = "classpath:/META-INF/dependency-lookup-context.xml";
+        beanDefinitionReader.loadBeanDefinitions(xmlResourcePath);
+        applicationContext.refresh();
+
+        LazyAnnotationDependencyInjectionDemo demo = applicationContext.getBean(LazyAnnotationDependencyInjectionDemo.class);
+
+        System.out.println("demo.user = " + demo.user);
+        System.out.println("demo.userObjectProvider = " + demo.userObjectProvider.getObject());
+        System.out.println("demo.usersObjectFactory = " + demo.userObjectFactory.getObject());
+
+        demo.userObjectProvider.forEach(System.out::println);
+
+        applicationContext.close();
+    }
+
+}
