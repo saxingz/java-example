2b638a8d9c9221f6ff72316f9ccf831d05d66ca7
saxing <saxlh@foxmail.com>
2020-11-28 20:11:05 +0800

    feat: think in spring

---
+++ a0042_xiaomage_lesson/think-in-spring/dependency-injection/src/main/java/org/saxing/thinking/in/spring/ioc/dependency/injection/AnnotationDependencyInjectionResolutionDemo.java
@@ -0,0 +1,51 @@
+package org.saxing.thinking.in.spring.ioc.dependency.injection;
+
+import org.saxing.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * {@link ObjectProvider}
+ *
+ * @author saxing 2020/11/27 23:39
+ */
+public class AnnotationDependencyInjectionResolutionDemo {
+
+    @Autowired
+    @Lazy
+    private User lazyUser;
+    @Autowired
+    private User user;
+    @Autowired
+    private Map<String, User> users;
+    @Autowired
+    private Optional<User> userOptional;
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(AnnotationDependencyInjectionResolutionDemo.class);
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(applicationContext);
+        String xmlResourcePath = "classpath:/META-INF/dependency-lookup-context.xml";
+        beanDefinitionReader.loadBeanDefinitions(xmlResourcePath);
+        applicationContext.refresh();
+
+        AnnotationDependencyInjectionResolutionDemo demo = applicationContext.getBean(AnnotationDependencyInjectionResolutionDemo.class);
+
+        System.out.println("demo.user = " + demo.user);
+        System.out.println("demo.users = " + demo.users);
+        System.out.println("demo.userOptional = " + demo.userOptional);
+
+
+        applicationContext.close();
+    }
+
+}
