f49b009d8cda49b8c69e6507bcd22416fd7491c4
saxing <saxlh@foxmail.com>
2020-11-29 23:48:06 +0800

    feat: rocketmq springboot

---
+++ a0044-rocketmq-springboot/src/main/java/org/saxing/rocketmqspringboot/RocketmqSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.rocketmqspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqSpringbootApplication.class, args);
+    }
+
+}
