76fb482ed0481fb1ec44654968e7fb008befaf41
saxing <saxlh@foxmail.com>
2020-11-29 23:58:20 +0800

    feat: rocketmq springboot

---
+++ a0044-rocketmq-springboot/a0044-producer/src/main/java/org/saxing/a0044producer/A0044ProducerApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0044producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0044ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0044ProducerApplication.class, args);
+    }
+
+}
