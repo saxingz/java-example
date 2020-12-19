0941d589f6b3fd3ef4f3135cccc55554e77fe8f3
saxing <saxlh@foxmail.com>
2020-12-19 17:54:49 +0800

    feat: quartz

---
+++ a0046_quartz/src/main/java/org/saxing/a0046_quartz/A0046QuartzApplication.java
@@ -0,0 +1,13 @@
+package org.saxing.a0046_quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0046QuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0046QuartzApplication.class, args);
+    }
+
+}
