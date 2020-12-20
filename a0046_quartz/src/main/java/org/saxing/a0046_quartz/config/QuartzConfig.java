bf456f0146a717c1bd76ff2e0a23ae7355d40dd1
saxing <saxlh@foxmail.com>
2020-12-20 12:18:48 +0800

    feat: quartz persist

---
+++ a0046_quartz/src/main/java/org/saxing/a0046_quartz/config/QuartzConfig.java
@@ -0,0 +1,36 @@
+package org.saxing.a0046_quartz.config;
+
+
+import org.quartz.*;
+import org.saxing.a0046_quartz.job.MyBootJob;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * quartz config
+ *
+ * @author saxing 2020/12/20 12:06
+ */
+@Configuration
+public class QuartzConfig {
+
+
+    @Bean
+    public JobDetail myBootJobDetail() {
+        return JobBuilder.newJob(MyBootJob.class)
+                .withIdentity("mybootjob1", "group1")
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger myBootTrigger() {
+        return TriggerBuilder.newTrigger()
+                .startNow().withIdentity("trigger1", "group1")
+                .forJob(myBootJobDetail())
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(5).repeatForever())
+                .build();
+    }
+
+}
