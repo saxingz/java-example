5cf3ba8e2a272955f263c3d4b879dc711d3a28f9
saxing <saxlh@foxmail.com>
2020-12-26 17:03:41 +0800

    feat: spring data es

---
+++ a0047_spring_data/a0047-spring-data-elasticsearch/src/test/java/org/saxing/a0047springdataelasticsearch/A0047SpringDataElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0047springdataelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0047SpringDataElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
