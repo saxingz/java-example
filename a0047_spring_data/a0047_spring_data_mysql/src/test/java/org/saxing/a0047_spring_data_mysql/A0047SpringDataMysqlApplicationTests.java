c2367deca22b0eeca11ea7cd982567c11fb4252c
saxing <saxlh@foxmail.com>
2021-02-03 13:56:40 +0800

    feat: spring data mysql

---
+++ a0047_spring_data/a0047_spring_data_mysql/src/test/java/org/saxing/a0047_spring_data_mysql/A0047SpringDataMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0047_spring_data_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0047SpringDataMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
