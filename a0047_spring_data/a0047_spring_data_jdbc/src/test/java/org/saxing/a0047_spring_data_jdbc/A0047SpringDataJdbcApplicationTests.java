303e7aac509d1a0c1e4b53e6247b1cbda77f481c
saxing <saxlh@foxmail.com>
2021-02-03 14:01:16 +0800

    feat: spring data jdbc

---
+++ a0047_spring_data/a0047_spring_data_jdbc/src/test/java/org/saxing/a0047_spring_data_jdbc/A0047SpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0047_spring_data_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0047SpringDataJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
