dcf7491e5d26a50292f390ca2532296048b7d18c
saxing <saxlh@foxmail.com>
2021-05-20 10:38:29 +0800

    feat: test

--- a0020_java8/src/main/java/org/saxing/java8/TimeTest.java
+++ a0020_java8/src/main/java/org/saxing/java8/TimeTest.java
@@ -1,12 +1,47 @@
 package org.saxing.java8;
 
+import org.springframework.util.DigestUtils;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Base64;
 import java.util.Date;
 
 public class TimeTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String str = "java.security.NoSuchAlgorithmException21432534534142";
+
+
+        byte[] md5s = MessageDigest.getInstance("MD5").digest();
+        System.out.println(new String(md5s));
+
+        byte[] bytes = DigestUtils.md5Digest(str.getBytes(StandardCharsets.UTF_8));
+        System.out.println(bytes);
+
+        String base64 = Base64.getMimeEncoder().encodeToString(str.getBytes());
+        System.out.println(base64);
+
+        String base642 = Base64.getEncoder().encodeToString(str.getBytes());
+        System.out.println(base642);
+
+
+
+
+        String sha256Hex = org.apache.commons.codec.digest.DigestUtils.sha256Hex(str);
+        System.out.println(sha256Hex);
+
+
+        String resultMd5 = DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8));
+        System.out.println(resultMd5);
+
+    }
+
+    public static void main2(String[] args) {
 
         Long current = System.currentTimeMillis();
         // 1596789092056
