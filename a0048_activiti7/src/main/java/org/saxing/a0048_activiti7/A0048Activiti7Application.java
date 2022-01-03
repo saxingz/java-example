5cd44d5b7f98bef3f4ea9428d535af7091b80710
saxing <saxlh@foxmail.com>
2022-01-03 16:41:13 +0800

    feat: init commit

---
+++ a0048_activiti7/src/main/java/org/saxing/a0048_activiti7/A0048Activiti7Application.java
@@ -0,0 +1,13 @@
+package org.saxing.a0048_activiti7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0048Activiti7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A0048Activiti7Application.class, args);
+    }
+
+}
---
+++ a0048_activiti7/src/test/java/org/saxing/a0048_activiti7/A0048Activiti7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saxing.a0048_activiti7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A0048Activiti7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
